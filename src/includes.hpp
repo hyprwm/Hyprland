8973f017ac0105a071b550d1d7b7acbde6dbd388
vaxerski <43317083+vaxerski@users.noreply.github.com>
2022-04-14 21:48:12 +0200

    support linux_dmabuf_v1

--- src/includes.hpp
+++ src/includes.hpp
@@ -42,6 +42,7 @@ extern "C" {
 #include <wlr/types/wlr_data_control_v1.h>
 #include <wlr/types/wlr_data_device.h>
 #include <wlr/types/wlr_export_dmabuf_v1.h>
+#include <wlr/types/wlr_linux_dmabuf_v1.h>
 #include <wlr/types/wlr_gamma_control_v1.h>
 #include <wlr/types/wlr_idle.h>
 #include <wlr/types/wlr_input_device.h>

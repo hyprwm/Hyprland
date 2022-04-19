6719574f0cbe643bc213630ccc0d6ff3dc6faf59
Sixels <sixels@protonmail.com>
2022-04-19 13:51:53 -0300

    Add input options to config

--- src/config/ConfigManager.cpp
+++ src/config/ConfigManager.cpp
@@ -55,6 +55,9 @@ void CConfigManager::setDefaultVars() {
     configValues["input:kb_options"].strValue = "";
     configValues["input:kb_rules"].strValue = "";
     configValues["input:kb_model"].strValue = "";
+    configValues["input:repeat_rate"].intValue = 25;
+    configValues["input:repeat_delay"].intValue = 600;
+    configValues["input:touchpad:disable_while_typing"].intValue = 1;
 
     configValues["input:follow_mouse"].intValue = 1;
 

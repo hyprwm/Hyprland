857ea436946745c6077790fa62d3b7a171b58551
Tanish2002 <tanishkhare@gmail.com>
2022-05-08 21:57:40 +0530

    add flake.nix and other required files for nix build

---
+++ default.nix
@@ -0,0 +1,42 @@
+{ lib, stdenv, fetchFromGitHub, src, version, pkg-config, cmake, ninja, libdrm
+, libinput, libxcb, libxkbcommon, mesa, mount, pango, wayland, wayland-protocols
+, wayland-scanner, wlroots, xcbutilwm, xwayland, xwaylandSupport ? true }:
+
+stdenv.mkDerivation {
+  pname = "hyprland";
+  inherit src version;
+
+  nativeBuildInputs = [ cmake ninja pkg-config wayland ]
+    ++ lib.optional xwaylandSupport xwayland;
+
+  buildInputs = [
+    libdrm
+    libinput
+    libxcb
+    libxkbcommon
+    mesa
+    pango
+    wayland-protocols
+    wayland-scanner
+    wlroots
+    xcbutilwm
+  ];
+
+  dontBuild = true;
+  dontInstall = true;
+
+  patches = [ ./fix-makefile.diff ];
+
+  postPatch = ''
+    mkdir -p $out/bin
+    make install PREFIX=$out
+  '';
+
+  meta = with lib; {
+    homepage = "https://github.com/vaxerski/Hyprland";
+    description =
+      "A dynamic tiling Wayland compositor that doesn't sacrifice on its looks";
+    license = licenses.bsd3;
+    platforms = platforms.linux;
+  };
+}

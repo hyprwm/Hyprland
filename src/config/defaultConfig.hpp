b7241bceefe78ac4b0e12bc1a46b35c0a2258609
vaxerski <43317083+vaxerski@users.noreply.github.com>
2022-05-12 11:34:39 +0200

    fix and update the default and autogen cfg

--- src/config/defaultConfig.hpp
+++ src/config/defaultConfig.hpp
@@ -19,16 +19,12 @@ input {
     kb_model=
     kb_options=
     kb_rules=
-    repeat_rate=
-    repeat_delay=
 
-    touchpad {
-        disable_while_typing=
-    }
+    follow_mouse=1
 }
 
 general {
-    max_fps=60
+    max_fps=60 # deprecated, unused
     sensitivity=0.25
     main_mod=SUPER
 
@@ -37,29 +33,61 @@ general {
     border_size=2
     col.active_border=0x66ee1111
     col.inactive_border=0x66333333
+
+    damage_tracking=full # experimental, monitor is 100% fine, but full might have some minor bugs, especially with high blur settings!
 }
 
 decoration {
     rounding=10
+    blur=1
+    blur_size=3 # minimum 1
+    blur_passes=1 # minimum 1, more passes = more resource intensive.
+    # Your blur "amount" is blur_size * blur_passes, but high blur_size (over around 5-ish) will produce artifacts.
+    # if you want heavy blur, you need to up the blur_passes.
+    # the more passes, the more you can up the blur_size without noticing artifacts.
 }
 
 animations {
     enabled=1
-    speed=7
+    speed=7 # speed is measured in 100s of ms, 7 = 700ms
+    curve=default # you can customize your own bezier curves, see the wiki
+    windows_curve=default # specific curve for all window animations
     windows_speed=6 # specific speeds for components can be made with name_speed=float. 0 means use global (speed=float). If not set, will use the global value.
     windows=1
     borders=1
-    borders_speed=20
+    borders_speed=10
     fadein=1 # fade in AND out
-    fadein_speed=20
+    fadein_speed=10
+    workspaces=1
+    workspaces_speed=8
 }
 
-# basic binds
-bind=SUPER,return,exec,kitty
-bind=SUPER,T,exec,alacritty
+dwindle {
+    pseudotile=0 # enable pseudotiling on dwindle
+}
+
+# example window rules
+# for windows named/classed as abc and xyz
+#windowrule=move 69 420,abc
+#windowrule=size 420 69,abc
+#windowrule=tile,xyz
+#windowrule=float,abc
+#windowrule=pseudo,abc
+#windowrule=monitor 0,xyz
+
+# example binds
+bind=SUPER,Q,exec,kitty
 bind=SUPER,C,killactive,
 bind=SUPER,M,exec,pkill Hyprland
+bind=SUPER,E,exec,dolphin
 bind=SUPER,V,togglefloating,
+bind=SUPER,R,exec,wofi --show drun -o DP-3
+bind=SUPER,P,pseudo,
+
+bind=SUPER,left,movefocus,l
+bind=SUPER,right,movefocus,r
+bind=SUPER,up,movefocus,u
+bind=SUPER,down,movefocus,d
 
 bind=SUPER,1,workspace,1
 bind=SUPER,2,workspace,2
@@ -71,4 +99,15 @@ bind=SUPER,7,workspace,7
 bind=SUPER,8,workspace,8
 bind=SUPER,9,workspace,9
 bind=SUPER,0,workspace,10
+
+bind=ALT,1,movetoworkspace,1
+bind=ALT,2,movetoworkspace,2
+bind=ALT,3,movetoworkspace,3
+bind=ALT,4,movetoworkspace,4
+bind=ALT,5,movetoworkspace,5
+bind=ALT,6,movetoworkspace,6
+bind=ALT,7,movetoworkspace,7
+bind=ALT,8,movetoworkspace,8
+bind=ALT,9,movetoworkspace,9
+bind=ALT,0,movetoworkspace,10
 )#";

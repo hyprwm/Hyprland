3e8dc178f9ae9df0f535510f313116fd4dbdd2e1
Four_Griffins <four.griffins.dev@gmail.com>
2022-05-16 23:52:48 +0200

    added opacity setting for fullscreen windows

--- src/config/ConfigManager.cpp
+++ src/config/ConfigManager.cpp
@@ -42,6 +42,7 @@ void CConfigManager::setDefaultVars() {
     configValues["decoration:blur_passes"].intValue = 1;
     configValues["decoration:active_opacity"].floatValue = 1;
     configValues["decoration:inactive_opacity"].floatValue = 1;
+    configValues["decoration:fullscreen_opacity"].floatValue = 1;
 
     configValues["dwindle:pseudotile"].intValue = 0;
     configValues["dwindle:col.group_border"].intValue = 0x66777700;

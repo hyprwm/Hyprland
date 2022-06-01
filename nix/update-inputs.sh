7451890fd71bd8e5a0abe4c22e3d7d6e1b0b2fce
Mihai Fufezan <fufexan@protonmail.com>
2022-06-02 00:42:05 +0300

    nix: replace paths dynamically

--- nix/update-inputs.sh
+++ nix/update-inputs.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env -S nix shell nixpkgs#gawk nixpkgs#git nixpkgs#moreutils nixpkgs#jq -c bash
 
-# update nixpkgs to latest version
-nix flake lock --update-input nixpkgs
-
 # get wlroots revision from submodule
 SUB_REV=$(git submodule status | awk '{ print substr($1,2)}')
 # and from lockfile
 CRT_REV=$(jq < flake.lock '.nodes.wlroots.locked.rev' -r)
 
 if [ $SUB_REV != $CRT_REV ]; then
+  # update nixpkgs to latest version
+  nix flake lock --update-input nixpkgs
+
   # update wlroots to submodule revision
   nix flake lock --override-input wlroots "gitlab:wlroots/wlroots/$SUB_REV?host=gitlab.freedesktop.org"
 

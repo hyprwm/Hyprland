6a16f11d63cead0b8526f442c18727a6f3dc76ea
vaxerski <43317083+vaxerski@users.noreply.github.com>
2022-07-06 21:57:35 +0200

    add namespace to hyprctl layers

--- src/debug/HyprCtl.cpp
+++ src/debug/HyprCtl.cpp
@@ -64,7 +64,7 @@ std::string layersRequest() {
             result += getFormat("\tLayer level %i:\n", layerLevel);
 
             for (auto& layer : level) {
-                result += getFormat("\t\tLayer %x: xywh: %i %i %i %i\n", layer, layer->geometry.x, layer->geometry.y, layer->geometry.width, layer->geometry.height);
+                result += getFormat("\t\tLayer %x: xywh: %i %i %i %i, namespace: %s\n", layer, layer->geometry.x, layer->geometry.y, layer->geometry.width, layer->geometry.height, layer->szNamespace.c_str());
             }
 
             layerLevel++;

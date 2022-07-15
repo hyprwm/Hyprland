144185681125df1567ada00c301e9ce242909f27
vaxerski <43317083+vaxerski@users.noreply.github.com>
2022-07-15 19:21:13 +0200

    fix blur on oversized, default to enabled

--- src/config/ConfigManager.cpp
+++ src/config/ConfigManager.cpp
@@ -63,7 +63,7 @@ void CConfigManager::setDefaultVars() {
     configValues["decoration:inactive_opacity"].floatValue = 1;
     configValues["decoration:fullscreen_opacity"].floatValue = 1;
     configValues["decoration:multisample_edges"].intValue = 1;
-    configValues["decoration:no_blur_on_oversized"].intValue = 1;
+    configValues["decoration:no_blur_on_oversized"].intValue = 0;
     configValues["decoration:drop_shadow"].intValue = 1;
     configValues["decoration:shadow_range"].intValue = 4;
     configValues["decoration:shadow_render_power"].intValue = 3;

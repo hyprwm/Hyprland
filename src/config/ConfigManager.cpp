925b72958c80b74f7728377f91a2a692d519d19b
vaxerski <43317083+vaxerski@users.noreply.github.com>
2022-07-21 20:30:48 +0200

    reload input after exec once

--- src/config/ConfigManager.cpp
+++ src/config/ConfigManager.cpp
@@ -1153,6 +1153,10 @@ void CConfigManager::dispatchExecOnce() {
     }
 
     firstExecRequests.clear(); // free some kb of memory :P
+
+    // set input, fixes some certain issues
+    g_pInputManager->setKeyboardLayout();
+    g_pInputManager->setMouseConfigs();
 }
 
 void CConfigManager::performMonitorReload() {

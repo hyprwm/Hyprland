910774a243949b97390454ac3a94f8fe60a584d9
vaxerski <43317083+vaxerski@users.noreply.github.com>
2022-07-26 22:01:55 +0200

    remove commit event cuz i forgot

--- src/events/Layers.cpp
+++ src/events/Layers.cpp
@@ -80,6 +80,7 @@ void Events::listener_destroyLayerSurface(void* owner, void* data) {
 
     layersurface->noProcess = true;
 
+    layersurface->hyprListener_commitLayerSurface.removeCallback();
     layersurface->hyprListener_destroyLayerSurface.removeCallback();
     layersurface->hyprListener_mapLayerSurface.removeCallback();
     layersurface->hyprListener_unmapLayerSurface.removeCallback();

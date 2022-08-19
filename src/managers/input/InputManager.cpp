290f813144889dd4b3366da8273ef1625effb7b7
vaxerski <vaxry@vaxry.net>
2022-08-19 23:22:53 +0200

    fix crash

--- src/managers/input/InputManager.cpp
+++ src/managers/input/InputManager.cpp
@@ -185,9 +185,10 @@ void CInputManager::mouseMoveUnified(uint32_t time, bool refocus) {
         } else {
             pFoundWindow = g_pCompositor->vectorToWindowIdeal(mouseCoords);
 
-            if (refocus && !pFoundWindow) {
-                pFoundWindow = g_pCompositor->getFirstWindowOnWorkspace(PMONITOR->activeWorkspace);
-            }
+            // TODO: this causes crashes, sometimes. ???
+           // if (refocus && !pFoundWindow) {
+           //     pFoundWindow = g_pCompositor->getFirstWindowOnWorkspace(PMONITOR->activeWorkspace);
+           // }
         }
 
         if (pFoundWindow) {

9f16aea10ba7de3a24d86b87a3801c587a7e6b60
sioodmy <sioodmy@tuta.io>
2022-08-21 21:05:28 +0200

    fix: update version string

--- flake.nix
+++ flake.nix
@@ -1,5 +1,6 @@
 {
-  description = "Hyprland is a dynamic tiling Wayland compositor that doesn't sacrifice on its looks";
+  description =
+    "Hyprland is a dynamic tiling Wayland compositor that doesn't sacrifice on its looks";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -9,61 +10,62 @@
     };
   };
 
-  outputs = inputs @ {
-    self,
-    nixpkgs,
-    ...
-  }: let
-    inherit (nixpkgs) lib;
-    genSystems = lib.genAttrs [
-      # Add more systems if they are supported
-      "aarch64-linux"
-      "x86_64-linux"
-    ];
-    pkgsFor = nixpkgs.legacyPackages;
-    mkDate = longDate: (lib.concatStringsSep "-" [
-      (__substring 0 4 longDate)
-      (__substring 4 2 longDate)
-      (__substring 6 2 longDate)
-    ]);
-  in {
-    overlays.default = _: prev: rec {
-      wlroots-hyprland = prev.wlroots.overrideAttrs (__: {
-        version = mkDate (inputs.wlroots.lastModifiedDate or "19700101");
-        src = inputs.wlroots;
-      });
-      hyprland = prev.callPackage ./nix/default.nix {
-        stdenv = prev.gcc12Stdenv;
-        version = "0.10.0beta" + "+date=" + (mkDate (self.lastModifiedDate or "19700101"));
-        wlroots = wlroots-hyprland;
+  outputs = inputs@{ self, nixpkgs, ... }:
+    let
+      inherit (nixpkgs) lib;
+      genSystems = lib.genAttrs [
+        # Add more systems if they are supported
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+      pkgsFor = nixpkgs.legacyPackages;
+      mkDate = longDate:
+        (lib.concatStringsSep "-" [
+          (__substring 0 4 longDate)
+          (__substring 4 2 longDate)
+          (__substring 6 2 longDate)
+        ]);
+    in {
+      overlays.default = _: prev: rec {
+        wlroots-hyprland = prev.wlroots.overrideAttrs (__: {
+          version = mkDate (inputs.wlroots.lastModifiedDate or "19700101");
+          src = inputs.wlroots;
+        });
+        hyprland = prev.callPackage ./nix/default.nix {
+          stdenv = prev.gcc12Stdenv;
+          version = "0.10.3beta" + "+date="
+            + (mkDate (self.lastModifiedDate or "19700101"));
+          wlroots = wlroots-hyprland;
+        };
+        hyprland-debug = hyprland.override { debug = true; };
+        waybar-hyprland = prev.waybar.overrideAttrs (oldAttrs: {
+          mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
+        });
       };
-      hyprland-debug = hyprland.override {debug = true;};
-      waybar-hyprland = prev.waybar.overrideAttrs (oldAttrs: {
-        mesonFlags = oldAttrs.mesonFlags ++ ["-Dexperimental=true"];
+
+      packages = genSystems (system:
+        (self.overlays.default null pkgsFor.${system}) // {
+          default = self.packages.${system}.hyprland;
+        });
+
+      devShells = genSystems (system: {
+        default = pkgsFor.${system}.mkShell.override {
+          stdenv = pkgsFor.${system}.gcc12Stdenv;
+        } {
+          name = "hyprland-shell";
+          inputsFrom = [
+            self.packages.${system}.wlroots-hyprland
+            self.packages.${system}.hyprland
+          ];
+        };
       });
+
+      formatter = genSystems (system: pkgsFor.${system}.alejandra);
+
+      nixosModules.default = import ./nix/module.nix self;
+      homeManagerModules.default = import ./nix/hm-module.nix self;
+
+      overlay = throw
+        "Hyprland: .overlay output is deprecated, please use the .overlays.default output";
     };
-
-    packages = genSystems (system:
-      (self.overlays.default null pkgsFor.${system})
-      // {
-        default = self.packages.${system}.hyprland;
-      });
-
-    devShells = genSystems (system: {
-      default = pkgsFor.${system}.mkShell.override {stdenv = pkgsFor.${system}.gcc12Stdenv;} {
-        name = "hyprland-shell";
-        inputsFrom = [
-          self.packages.${system}.wlroots-hyprland
-          self.packages.${system}.hyprland
-        ];
-      };
-    });
-
-    formatter = genSystems (system: pkgsFor.${system}.alejandra);
-
-    nixosModules.default = import ./nix/module.nix self;
-    homeManagerModules.default = import ./nix/hm-module.nix self;
-
-    overlay = throw "Hyprland: .overlay output is deprecated, please use the .overlays.default output";
-  };
 }

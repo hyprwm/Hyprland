ac75422b08708f91cc7824bba3a0a0d8a3311f42
Akane <96370213+akane6704@users.noreply.github.com>
2022-09-20 22:05:54 +0200

    Better naming

--- src/helpers/Color.hpp
+++ src/helpers/Color.hpp
@@ -10,21 +10,21 @@ public:
 
     float r = 0, g = 0, b = 0, a = 255;
 
-    uint64_t    getAsHex();
+    uint64_t getAsHex();
 
-    CColor operator- (const CColor& c2) const {
+    CColor operatorMinus(const CColor& c2) const {
         return CColor(r - c2.r, g - c2.g, b - c2.b, a - c2.a);
     }
 
-    CColor operator+ (const CColor& c2) const {
+    CColor operatorPlus(const CColor& c2) const {
         return CColor(r + c2.r, g + c2.g, b + c2.b, a + c2.a);
     }
 
-    CColor operator* (const float& v) const {
+    CColor operatorMultiply(const float& v) const {
         return CColor(r * v, g * v, b * v, a * v);
     }
     
-    bool operator==(const CColor& c2) const {
+    bool operatorEq(const CColor& c2) const {
         return r == c2.r && g == c2.g && b == c2.b && a == c2.a;
     }
 };

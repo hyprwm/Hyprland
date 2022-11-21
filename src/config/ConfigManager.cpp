52c4c83018e6d55411d9c8d470fbc0d823ad61da
vaxerski <43317083+vaxerski@users.noreply.github.com>
2022-11-21 23:26:18 +0000

    add scroll_button libinput opt

--- src/config/ConfigManager.cpp
+++ src/config/ConfigManager.cpp
@@ -142,6 +142,7 @@ void CConfigManager::setDefaultVars() {
     configValues["input:float_switch_override_focus"].intValue = 1;
     configValues["input:left_handed"].intValue = 0;
     configValues["input:scroll_method"].strValue = STRVAL_EMPTY;
+    configValues["input:scroll_button"].intValue = 0;
     configValues["input:touchpad:natural_scroll"].intValue = 0;
     configValues["input:touchpad:disable_while_typing"].intValue = 1;
     configValues["input:touchpad:clickfinger_behavior"].intValue = 0;
@@ -193,6 +194,7 @@ void CConfigManager::setDeviceDefaultVars(const std::string& dev) {
     cfgValues["drag_lock"].intValue = 0;
     cfgValues["left_handed"].intValue = 0;
     cfgValues["scroll_method"].strValue = STRVAL_EMPTY;
+    cfgValues["scroll_button"].intValue = 0;
     cfgValues["touch_transform"].intValue = 0;
     cfgValues["touch_output"].strValue = STRVAL_EMPTY;
     cfgValues["enabled"].intValue = 1; // only for mice / touchpads

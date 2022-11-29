55776df685340ae798aace1c74381d3f6d350313
vaxerski <43317083+vaxerski@users.noreply.github.com>
2022-11-29 11:10:19 +0000

    fix double free

--- src/render/OpenGL.cpp
+++ src/render/OpenGL.cpp
@@ -787,7 +787,6 @@ void CHyprOpenGLImpl::renderTextureWithBlur(const CTexture& tex, wlr_box* pBox, 
             return;
         }
     }
-    pixman_region32_fini(&inverseOpaque);
 
     //                                                                        vvv TODO: layered blur fbs?
     const bool USENEWOPTIMIZE = (*PBLURNEWOPTIMIZE && m_pCurrentWindow && !m_pCurrentWindow->m_bIsFloating && m_RenderData.pCurrentMonData->blurFB.m_cTex.m_iTexID && !g_pCompositor->isWorkspaceSpecial(m_pCurrentWindow->m_iWorkspaceID));

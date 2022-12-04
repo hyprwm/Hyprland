3b1b3f9c9758a8282f4a097befe01181e378cfdc
Flafy <flafyarazi@gmail.com>
2022-12-04 22:57:41 +0200

    blur xray for layers (#1158) * feat: apply blur_xray to non window surfaces * don't blur optimize bottom and background layers

--- src/render/OpenGL.hpp
+++ src/render/OpenGL.hpp
@@ -82,7 +82,7 @@ public:
     void    renderRectWithDamage(wlr_box*, const CColor&, pixman_region32_t* damage, int round = 0);
     void    renderTexture(wlr_texture*, wlr_box*, float a, int round = 0, bool allowCustomUV = false);
     void    renderTexture(const CTexture&, wlr_box*, float a, int round = 0, bool discardOpaque = false, bool allowCustomUV = false);
-    void    renderTextureWithBlur(const CTexture&, wlr_box*, float a, wlr_surface* pSurface, int round = 0);
+    void    renderTextureWithBlur(const CTexture&, wlr_box*, float a, wlr_surface* pSurface, int round = 0, bool blockBlurOptimization = false);
     void    renderRoundedShadow(wlr_box*, int round, int range, float a = 1.0);
     void    renderBorder(wlr_box*, const CGradientValueData&, int round, float a = 1.0);
 

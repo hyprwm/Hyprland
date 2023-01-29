1eb70cf4bf50cd9e974bacaf120a7aeed2023330
Jan Beich <jbeich@FreeBSD.org>
2023-01-29 01:25:26 +0000

    Allow to disable X11 backend separately from Xwayland after 5a750b485aca
    $ meson setup -Dxwayland=enabled -Dwlroots:backends="drm,libinput" _build
    $ meson compile -C _build
    [...]
    /usr/bin/ld: src/Hyprland.p/debug_HyprCtl.cpp.o: in function `createOutputIter(wlr_backend*, void*)':
    _build/../src/debug/HyprCtl.cpp:1015: undefined reference to `wlr_backend_is_x11'
    /usr/bin/ld: _build/../src/debug/HyprCtl.cpp:1016: undefined reference to `wlr_x11_output_create'
    /usr/bin/ld: _build/../src/debug/HyprCtl.cpp:1026: undefined reference to `wlr_backend_is_x11'
    collect2: error: ld returned 1 exit status
    ninja: build stopped: subcommand failed.

--- src/includes.hpp
+++ src/includes.hpp
@@ -105,8 +105,11 @@ extern "C" {
 
 #include <drm_fourcc.h>
 
-#ifndef NO_XWAYLAND
+#if WLR_HAS_X11_BACKEND
 #include <wlr/backend/x11.h>
+#endif
+
+#ifndef NO_XWAYLAND
 #include <wlr/xwayland.h>
 #endif
 }
@@ -125,6 +128,10 @@ extern "C" {
 #define GLES32
 #include <GLES3/gl32.h>
 #include <GLES3/gl3ext.h>
+#endif
+
+#if !WLR_HAS_X11_BACKEND
+#include "helpers/X11Stubs.hpp"
 #endif
 
 #ifdef NO_XWAYLAND

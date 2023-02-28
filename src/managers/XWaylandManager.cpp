6aca28a5b42bd26b966b3da0bdc0221ab7b96e5c
vaxerski <43317083+vaxerski@users.noreply.github.com>
2023-02-28 19:36:36 +0000

    Added decoration reserved area

--- src/managers/XWaylandManager.cpp
+++ src/managers/XWaylandManager.cpp
@@ -136,7 +136,11 @@ void CHyprXWaylandManager::sendCloseWindow(CWindow* pWindow) {
     }
 }
 
-void CHyprXWaylandManager::setWindowSize(CWindow* pWindow, const Vector2D& size, bool force) {
+void CHyprXWaylandManager::setWindowSize(CWindow* pWindow, Vector2D size, bool force) {
+
+    const auto RESERVED = pWindow->getFullWindowReservedArea();
+
+    size = size - RESERVED.topLeft - RESERVED.bottomRight;
 
     if (!force &&
         ((pWindow->m_vReportedSize == size && pWindow->m_vRealPosition.vec() == pWindow->m_vReportedPosition) || (pWindow->m_vReportedSize == size && !pWindow->m_bIsX11)))

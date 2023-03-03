c386c52cf9fbd442e3c6efec4664d9907fd2cfb1
Mihai Fufezan <fufexan@protonmail.com>
2023-03-04 01:41:37 +0200

    Nix: fix hidpi patch

--- nix/wlroots.nix
+++ nix/wlroots.nix
@@ -9,7 +9,7 @@
   hwdata,
   libliftoff,
   libdisplay-info,
-  hidpiXWayland ? false,
+  hidpiXWayland ? true,
   enableXWayland ? true,
   nvidiaPatches ? false,
 }:
@@ -35,10 +35,8 @@ assert (lib.assertMsg (hidpiXWayland -> enableXWayland) ''
       patches =
         (old.patches or [])
         ++ (lib.optionals (enableXWayland && hidpiXWayland) [
-          (fetchpatch {
-            url = "https://gitlab.freedesktop.org/lilydjwg/wlroots/-/commit/6c5ffcd1fee9e44780a6a8792f74ecfbe24a1ca7.diff";
-            sha256 = "sha256-Eo1pTa/PIiJsRZwIUnHGTIFFIedzODVf0ZeuXb0a3TQ=";
-          })
+          # adapted from https://gitlab.freedesktop.org/lilydjwg/wlroots/-/commit/6c5ffcd1fee9e44780a6a8792f74ecfbe24a1ca7
+          ./wlroots-hidpi.patch
           (fetchpatch {
             url = "https://gitlab.freedesktop.org/wlroots/wlroots/-/commit/18595000f3a21502fd60bf213122859cc348f9af.diff";
             sha256 = "sha256-jvfkAMh3gzkfuoRhB4E9T5X1Hu62wgUjj4tZkJm0mrI=";

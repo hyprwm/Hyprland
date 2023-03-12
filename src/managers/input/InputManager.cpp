146d231ec5ea9059a64e34caeba06e790991e81a
vaxerski <43317083+vaxerski@users.noreply.github.com>
2023-03-12 13:46:38 +0000

    config: add key_press_enables_dpms

--- src/managers/input/InputManager.cpp
+++ src/managers/input/InputManager.cpp
@@ -963,6 +963,12 @@ void CInputManager::onKeyboardKey(wlr_keyboard_key_event* e, SKeyboard* pKeyboar
     if (!pKeyboard->enabled)
         return;
 
+    static auto* const PDPMS = &g_pConfigManager->getConfigValuePtr("key_press_enables_dpms")->intValue;
+    if (*PDPMS && g_pCompositor->m_bDPMSStateON) {
+        // enable dpms
+        g_pKeybindManager->dpms("on");
+    }
+
     bool passEvent = g_pKeybindManager->onKeyEvent(e, pKeyboard);
 
     wlr_idle_notify_activity(g_pCompositor->m_sWLRIdle, g_pCompositor->m_sSeat.seat);

7faead75bd57b5d52572a6a46c9c38336b282fda
Mihai Fufezan <fufexan@protonmail.com>
2023-04-10 14:53:50 +0300

    nix: update xdph
    workflows/nix-update: update all inputs

--- nix/update-inputs.sh
+++ nix/update-inputs.sh
@@ -5,17 +5,17 @@ set -ex
 # get wlroots revision from submodule
 SUB_REV=$(git submodule status | rg wlroots | awk '{ print substr($1,2)}')
 # and from lockfile
-CRT_REV=$(jq < flake.lock '.nodes.wlroots.locked.rev' -r)
+CRT_REV=$(jq <flake.lock '.nodes.wlroots.locked.rev' -r)
 
-if [ $SUB_REV != $CRT_REV ]; then
-  # update nixpkgs to latest version
-  nix flake lock --update-input nixpkgs
+if [ "$SUB_REV" != "$CRT_REV" ]; then
+  # update inputs to latest versions
+  nix flake update
 
   # update wlroots to submodule revision
   nix flake lock --override-input wlroots "gitlab:wlroots/wlroots/$SUB_REV?host=gitlab.freedesktop.org"
 
   # remove "dirty" mark from lockfile
-  jq < flake.lock 'del(.nodes.wlroots.original.rev)' | sponge flake.lock
+  jq <flake.lock 'del(.nodes.wlroots.original.rev)' | sponge flake.lock
 else
   echo "wlroots is up to date!"
 fi

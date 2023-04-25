2057859b353d63baf8b6369e8bb00a0ee1785250
outfoxxed <outfoxxed@outfoxxed.me>
2023-04-25 15:00:34 +0000

    Add follow mouse mode to avoid refocusing under cursor (#2135) * Add follow mouse mode to avoid refocusing under cursor

--- src/managers/input/InputManager.hpp
+++ src/managers/input/InputManager.hpp
@@ -175,6 +175,9 @@ class CInputManager {
     // for hiding cursor on touch
     bool m_bLastInputTouch = false;
 
+    // for tracking mouse refocus
+    CWindow* m_pLastMouseFocus = nullptr;
+
   private:
     bool                 m_bCursorImageOverridden = false;
     eBorderIconDirection m_eBorderIconDirection   = BORDERICON_NONE;

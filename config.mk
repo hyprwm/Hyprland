a2c165b504373f1715daef09696b0f79bd711426
Ching Pei Yang <59727193+horriblename@users.noreply.github.com>
2023-04-26 23:59:16 +0200

    Plugin header overhaul (#2087) * meson: install headers * Meson/CMake: add pkg-config file for headers * makefile: install headers and pkgconfig * CMake: move protocols to cmake Co-authored-by: Ching Pei Yang <badnam3o.0@gmail.com> --------- Co-authored-by: Mihai Fufezan <fufexan@protonmail.com> Co-authored-by: vaxerski <43317083+vaxerski@users.noreply.github.com>

--- config.mk
+++
@@ -1,4 +0,0 @@
-PREFIX = /usr/local
-CFLAGS ?= -g -Wall -Wextra -Werror -Wno-unused-parameter -Wno-sign-compare -Wno-unused-function -Wno-unused-variable -Wno-unused-result -Wdeclaration-after-statement
-CFLAGS += -DXWAYLAND
-

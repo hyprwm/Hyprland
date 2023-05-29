b3a86952cf14b6f556159eb898eaa7b6ceac4335
Jeremy Huang <66577496+jchhuang-tech@users.noreply.github.com>
2023-05-29 00:52:36 -0700

    focus: fix #1675 window not scrollable after movefocus (#2390)

--- src/managers/KeybindManager.cpp
+++ src/managers/KeybindManager.cpp
@@ -981,6 +981,7 @@ void CKeybindManager::moveFocusTo(std::string args) {
             g_pCompositor->focusWindow(PWINDOWTOCHANGETO);
             Vector2D middle = PWINDOWTOCHANGETO->m_vRealPosition.goalv() + PWINDOWTOCHANGETO->m_vRealSize.goalv() / 2.f;
             g_pCompositor->warpCursorTo(middle);
+            g_pInputManager->simulateMouseMovement();
 
             if (PLASTWINDOW->m_iMonitorID != PWINDOWTOCHANGETO->m_iMonitorID) {
                 // event

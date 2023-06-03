845bb026c851222f38f4a7ba1a7992e09f4149ed
vaxerski <vaxry@vaxry.net>
2023-06-03 13:01:27 +0200

    noxwl: add missing stubs

--- src/helpers/XWaylandStubs.hpp
+++ src/helpers/XWaylandStubs.hpp
@@ -28,8 +28,7 @@ typedef struct {
 } xcb_size_hints_t;
 typedef unsigned int xcb_window_t;
 
-typedef enum xcb_stack_mode_t
-{
+typedef enum xcb_stack_mode_t {
     XCB_STACK_MODE_ABOVE     = 0,
     XCB_STACK_MODE_BELOW     = 1,
     XCB_STACK_MODE_TOP_IF    = 2,
@@ -112,6 +111,8 @@ struct wlr_xwayland_surface {
 
         struct wl_signal map;
         struct wl_signal unmap;
+        struct wl_signal associate;
+        struct wl_signal dissociate;
         struct wl_signal set_title;
         struct wl_signal set_class;
         struct wl_signal set_role;

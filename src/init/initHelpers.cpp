891a58eabb5b39e674700786373ed3da7105b503
Mily <milyth@duck.com>
2023-07-13 10:04:49 -0300

    init: make gainRealTime more compatible with other OSes

--- src/init/initHelpers.cpp
+++ src/init/initHelpers.cpp
@@ -5,11 +5,22 @@ bool Init::isSudo() {
 }
 
 void Init::gainRealTime() {
-#ifdef SCHED_RESET_ON_FORK
-    const int   minPrio = sched_get_priority_min(SCHED_RR);
-    sched_param param   = {.sched_priority = minPrio};
+    const int                minPrio = sched_get_priority_min(SCHED_RR);
+    const struct sched_param param   = {.sched_priority = minPrio};
 
+#ifdef SCHED_RESET_ON_FORK
     if (pthread_setschedparam(pthread_self(), SCHED_RR | SCHED_RESET_ON_FORK, &param))
         Debug::log(WARN, "Failed to change process scheduling strategy");
+#else
+    if (pthread_setschedparam(pthread_self(), SCHED_RR, &param)) {
+        Debug::log(WARN, "Failed to change process scheduling strategy");
+        return;
+    }
+
+    pthread_atfork(NULL, NULL, []() {
+        const struct sched_param param = {.sched_priority = 0};
+        if (pthread_setschedparam(pthread_self(), SCHED_OTHER, &param))
+            Debug::log(WARN, "Failed to reset process scheduling strategy");
+    });
 #endif
 }

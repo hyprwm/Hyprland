352110ea26e1219306a9ee5569dfc300a3c69106
Mily <milyth@duck.com>
2023-07-12 22:04:38 -0300

    init: checks if host supports reseting scheduler on fork

--- src/init/initHelpers.cpp
+++ src/init/initHelpers.cpp
@@ -5,9 +5,11 @@ bool Init::isSudo() {
 }
 
 void Init::gainRealTime() {
+#ifdef SCHED_RESET_ON_FORK
     const int   minPrio = sched_get_priority_min(SCHED_RR);
     sched_param param   = {.sched_priority = minPrio};
 
     if (pthread_setschedparam(pthread_self(), SCHED_RR | SCHED_RESET_ON_FORK, &param))
         Debug::log(WARN, "Failed to change process scheduling strategy");
+#endif
 }

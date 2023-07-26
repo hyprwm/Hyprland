397199a1f7fa38347b29682e83d84a96d6853785
ferrreo <harderthanfire@gmail.com>
2023-07-26 10:23:46 +0100

    Fix for issue #2809

--- src/init/initHelpers.cpp
+++ src/init/initHelpers.cpp
@@ -1,20 +1,18 @@
 #include "initHelpers.hpp"
+#include <sys/resource.h>
 
 bool Init::isSudo() {
     return getuid() != geteuid() || !geteuid();
 }
 
 void Init::gainRealTime() {
-    const int                minPrio = sched_get_priority_min(SCHED_RR);
-    int old_policy;
-    struct sched_param param;
-
-    if (pthread_getschedparam(pthread_self(), &old_policy, &param)) {
-        Debug::log(WARN, "Failed to get old pthread scheduling priority");
+    // If something else is adjusting priority, don't interfere
+    if (getpriority(PRIO_PROCESS, 0) != 0) {
         return;
     }
 
-    param.sched_priority = minPrio;
+    const int                minPrio = sched_get_priority_min(SCHED_RR);
+    const struct sched_param param   = {.sched_priority = minPrio};
 
     if (pthread_setschedparam(pthread_self(), SCHED_RR, &param)) {
         Debug::log(WARN, "Failed to change process scheduling strategy");

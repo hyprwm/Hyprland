eb6e8fc1c77dda2450308690f4535cf0c37d92d8
romanstingler <roman.stingler@gmail.com>
2023-08-03 21:40:56 +0200

    xwayland: drop wlr_xwayland_surface.events.set_pid (#2884) Co-authored-by: Roman Stingler <roman.stingler@waterdrop.com>

--- src/helpers/XWaylandStubs.hpp
+++ src/helpers/XWaylandStubs.hpp
@@ -118,7 +118,6 @@ struct wlr_xwayland_surface {
         struct wl_signal set_class;
         struct wl_signal set_role;
         struct wl_signal set_parent;
-        struct wl_signal set_pid;
         struct wl_signal set_startup_id;
         struct wl_signal set_window_type;
         struct wl_signal set_hints;

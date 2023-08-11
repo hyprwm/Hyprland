60b548296d9465c77a3f96f01305df5b83084903
vaxerski <vaxry@vaxry.net>
2023-08-11 17:22:16 +0200

    internal: move backtrace to specific func

--- src/helpers/MiscFunctions.hpp
+++ src/helpers/MiscFunctions.hpp
@@ -4,22 +4,29 @@
 #include <wayland-server.h>
 #include <wlr/util/box.h>
 #include "Vector2D.hpp"
+#include <vector>
 
-std::string absolutePath(const std::string&, const std::string&);
-void        addWLSignal(wl_signal*, wl_listener*, void* pOwner, const std::string& ownerString);
-std::string getFormat(const char* fmt, ...); // Basically Debug::log to a string
-std::string escapeJSONStrings(const std::string& str);
-void        scaleBox(wlr_box*, float);
-std::string removeBeginEndSpacesTabs(std::string);
-bool        isNumber(const std::string&, bool allowfloat = false);
-bool        isDirection(const std::string&);
-int         getWorkspaceIDFromString(const std::string&, std::string&);
-float       vecToRectDistanceSquared(const Vector2D& vec, const Vector2D& p1, const Vector2D& p2);
-void        logSystemInfo();
-std::string execAndGet(const char*);
-int64_t     getPPIDof(int64_t pid);
-int64_t     configStringToInt(const std::string&);
-float       getPlusMinusKeywordResult(std::string in, float relative);
-void        matrixProjection(float mat[9], int w, int h, wl_output_transform tr);
-double      normalizeAngleRad(double ang);
-std::string replaceInString(std::string subject, const std::string& search, const std::string& replace);
+struct SCallstackFrameInfo {
+    void*       adr = nullptr;
+    std::string desc;
+};
+
+std::string                      absolutePath(const std::string&, const std::string&);
+void                             addWLSignal(wl_signal*, wl_listener*, void* pOwner, const std::string& ownerString);
+std::string                      getFormat(const char* fmt, ...); // Basically Debug::log to a string
+std::string                      escapeJSONStrings(const std::string& str);
+void                             scaleBox(wlr_box*, float);
+std::string                      removeBeginEndSpacesTabs(std::string);
+bool                             isNumber(const std::string&, bool allowfloat = false);
+bool                             isDirection(const std::string&);
+int                              getWorkspaceIDFromString(const std::string&, std::string&);
+float                            vecToRectDistanceSquared(const Vector2D& vec, const Vector2D& p1, const Vector2D& p2);
+void                             logSystemInfo();
+std::string                      execAndGet(const char*);
+int64_t                          getPPIDof(int64_t pid);
+int64_t                          configStringToInt(const std::string&);
+float                            getPlusMinusKeywordResult(std::string in, float relative);
+void                             matrixProjection(float mat[9], int w, int h, wl_output_transform tr);
+double                           normalizeAngleRad(double ang);
+std::string                      replaceInString(std::string subject, const std::string& search, const std::string& replace);
+std::vector<SCallstackFrameInfo> getBacktrace();

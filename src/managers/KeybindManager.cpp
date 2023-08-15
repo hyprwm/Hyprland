4b6241478cd2805e63299029cca06345fd468dbe
memchr <memchr@proton.me>
2023-08-15 22:57:38 +0000

    fix: ioctl VT_GETACTIVE fd

--- src/managers/KeybindManager.cpp
+++ src/managers/KeybindManager.cpp
@@ -519,19 +519,19 @@ bool CKeybindManager::handleVT(xkb_keysym_t keysym) {
 
         // vtnr is bugged for some reason.
         unsigned int ttynum = 0;
-#if defined(VT_GETSTATE)
-        int            fd;
-        struct vt_stat st;
+        int          fd;
         if ((fd = open("/dev/tty", O_RDONLY | O_NOCTTY)) >= 0) {
+#if defined(VT_GETSTATE)
+            struct vt_stat st;
             if (!ioctl(fd, VT_GETSTATE, &st))
                 ttynum = st.v_active;
+#elif defined(VT_GETACTIVE)
+            int vt;
+            if (!ioctl(0, VT_GETACTIVE, &vt))
+                ttynum = vt;
+#endif
             close(fd);
         }
-#elif defined(VT_GETACTIVE)
-        int vt;
-        if (!ioctl(0, VT_GETACTIVE, &vt))
-            ttynum = vt;
-#endif
 
         if (ttynum == TTY)
             return true;

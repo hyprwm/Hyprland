596315058478d512b925ed4c12aafb3349e41479
vaxerski <vaxry@vaxry.net>
2023-09-11 10:22:11 +0100

    constify vars

--- src/managers/KeybindManager.cpp
+++ src/managers/KeybindManager.cpp
@@ -1159,17 +1159,17 @@ void CKeybindManager::moveActiveTo(std::string args) {
         return;
 
     if (PLASTWINDOW->m_bIsFloating) {
-        auto vPos       = PLASTWINDOW->m_vRealPosition.goalv();
-        auto PMONITOR   = g_pCompositor->getMonitorFromID(PLASTWINDOW->m_iMonitorID);
-        int  borderSize = PLASTWINDOW->getRealBorderSize();
+        auto       vPos       = PLASTWINDOW->m_vRealPosition.goalv();
+        const auto PMONITOR   = g_pCompositor->getMonitorFromID(PLASTWINDOW->m_iMonitorID);
+        const auto BORDERSIZE = PLASTWINDOW->getRealBorderSize();
 
         switch (arg) {
-            case 'l': vPos.x = PMONITOR->vecReservedTopLeft.x + borderSize; break;
-            case 'r': vPos.x = PMONITOR->vecSize.x - PMONITOR->vecReservedBottomRight.x - PLASTWINDOW->m_vRealSize.goalv().x - borderSize; break;
+            case 'l': vPos.x = PMONITOR->vecReservedTopLeft.x + BORDERSIZE; break;
+            case 'r': vPos.x = PMONITOR->vecSize.x - PMONITOR->vecReservedBottomRight.x - PLASTWINDOW->m_vRealSize.goalv().x - BORDERSIZE; break;
             case 't':
-            case 'u': vPos.y = PMONITOR->vecReservedTopLeft.y + borderSize; break;
+            case 'u': vPos.y = PMONITOR->vecReservedTopLeft.y + BORDERSIZE; break;
             case 'b':
-            case 'd': vPos.y = PMONITOR->vecSize.y - PMONITOR->vecReservedBottomRight.y - PLASTWINDOW->m_vRealSize.goalv().y - borderSize; break;
+            case 'd': vPos.y = PMONITOR->vecSize.y - PMONITOR->vecReservedBottomRight.y - PLASTWINDOW->m_vRealSize.goalv().y - BORDERSIZE; break;
         }
 
         PLASTWINDOW->m_vRealPosition = vPos + PMONITOR->vecPosition;

c55470eee76ad8c4897fcb1b3b2062b1b11dc68d
Jeremy Huang <66577496+thejch@users.noreply.github.com>
2023-09-15 04:03:05 -0700

    master: Add mfact dispatcher (#3298) * add mfact dispatcher * limit mfact to between 0.05 and 0.95 * add try catch block for stof * change log to err

--- src/layout/MasterLayout.cpp
+++ src/layout/MasterLayout.cpp
@@ -1227,6 +1227,20 @@ std::any CHyprMasterLayout::layoutMessage(SLayoutMessageHeader header, std::stri
         runOrientationCycle(header, nullptr, -1);
     } else if (command == "orientationcycle") {
         runOrientationCycle(header, &vars, 1);
+    } else if (command == "mfact") {
+        if (vars.size() >= 2) {
+            float newMfact = 0;
+            try {
+                newMfact = std::stof(vars[1]);
+            } catch (std::exception& e) {
+                Debug::log(ERR, "Argument is invalid: {}", e.what());
+                return 0;
+            }
+            for (auto& nd : m_lMasterNodesData) {
+                if (nd.isMaster)
+                    nd.percMaster = std::clamp(newMfact, 0.05f, 0.95f);
+            }
+        }
     }
 
     return 0;

4daa51570018900f12ede73f8bf73f6964a381bc
vaxerski <43317083+vaxerski@users.noreply.github.com>
2023-09-29 20:05:43 +0100

    watchdog: don't hang on exit

--- src/helpers/Watchdog.cpp
+++ src/helpers/Watchdog.cpp
@@ -3,6 +3,9 @@
 #include "config/ConfigManager.hpp"
 
 CWatchdog::~CWatchdog() {
+    m_bExitThread = true;
+    m_bNotified   = true;
+    m_cvWatchdogCondition.notify_all();
     m_pWatchdog.reset();
 }
 
@@ -21,6 +24,9 @@ CWatchdog::CWatchdog() {
                 if (m_cvWatchdogCondition.wait_for(lk, std::chrono::milliseconds((int)(*PTIMEOUT * 1000.0)), [this] { return m_bNotified; }) == false)
                     pthread_kill(m_iMainThreadPID, SIGUSR1);
             }
+
+            if (m_bExitThread)
+                break;
 
             m_bWatching = false;
             m_bNotified = false;

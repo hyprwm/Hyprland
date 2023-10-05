07385e2c4b81a9a3f5d384e6af4782348dbf3f28
ItsDrike <itsdrike@protonmail.com>
2023-10-05 13:57:13 +0200

    Remove starting newline in demangled func name

--- src/plugins/PluginAPI.cpp
+++ src/plugins/PluginAPI.cpp
@@ -301,7 +301,7 @@ APICALL std::vector<SFunctionMatch> HyprlandAPI::findFunctionsByName(HANDLE hand
             count++;
         }
 
-        return SYMBOLSDEMANGLED.substr(pos, SYMBOLSDEMANGLED.find('\n', pos + 1) - pos);
+        return SYMBOLSDEMANGLED.substr(pos + 1, SYMBOLSDEMANGLED.find('\n', pos + 1) - pos - 1);
     };
 
     if (SYMBOLS.empty()) {

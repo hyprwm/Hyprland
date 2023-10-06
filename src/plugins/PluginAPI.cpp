1afb00a01b7cab3e68f5af3ca6a7d7d86b8f913e
ItsDrike <itsdrike@protonmail.com>
2023-10-06 13:01:40 +0000

    pluginapi: remove starting newline in demangled func name (#3502)

--- src/plugins/PluginAPI.cpp
+++ src/plugins/PluginAPI.cpp
@@ -301,7 +301,11 @@ APICALL std::vector<SFunctionMatch> HyprlandAPI::findFunctionsByName(HANDLE hand
             count++;
         }
 
-        return SYMBOLSDEMANGLED.substr(pos, SYMBOLSDEMANGLED.find('\n', pos + 1) - pos);
+        // Skip the newline char itself
+        if (pos != 0)
+            pos++;
+
+        return SYMBOLSDEMANGLED.substr(pos, SYMBOLSDEMANGLED.find('\n', pos) - pos);
     };
 
     if (SYMBOLS.empty()) {

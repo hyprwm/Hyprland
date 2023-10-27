2cb73d603c659dabd4195e28d50ef5235c01d18e
VDawg <vdawg@tuta.io>
2023-10-27 22:12:59 +0200

    improve hsl conversion in blur1

--- src/render/shaders/Textures.hpp
+++ src/render/shaders/Textures.hpp
@@ -207,49 +207,77 @@ float doubleCircleSigmoid(float x, float a) {
     return y;
 }
 
-// https://www.shadertoy.com/view/wt23Rt
-vec3 saturate(vec3 v) {
-    return clamp(v, 0., 1.);
-}
-vec3 hue2rgb(float hue) {
-    hue = fract(hue);
-    return saturate(vec3(abs(hue * 6. - 3.) - 1., 2. - abs(hue * 6. - 2.), 2. - abs(hue * 6. - 4.)));
-}
-vec3 rgb2hsl(vec3 c) {
-    float cMin = min(min(c.r, c.g), c.b), cMax = max(max(c.r, c.g), c.b), delta = cMax - cMin;
-    vec3  hsl = vec3(0., 0., (cMax + cMin) / 2.);
-    if (delta != 0.0) { //If it has chroma and isn't gray.
-        if (hsl.z < .5) {
-            hsl.y = delta / (cMax + cMin); //Saturation.
-        } else {
-            hsl.y = delta / (2. - cMax - cMin); //Saturation.
-        }
-        float deltaR = (((cMax - c.r) / 6.) + (delta / 2.)) / delta, deltaG = (((cMax - c.g) / 6.) + (delta / 2.)) / delta, deltaB = (((cMax - c.b) / 6.) + (delta / 2.)) / delta;
-        //Hue.
-        if (c.r == cMax) {
-            hsl.x = deltaB - deltaG;
-        } else if (c.g == cMax) {
-            hsl.x = (1. / 3.) + deltaR - deltaB;
-        } else { //if(c.b==cMax){
-            hsl.x = (2. / 3.) + deltaG - deltaR;
-        }
-        hsl.x = fract(hsl.x);
+vec3 rgb2hsl(vec3 col) {
+    float red   = col.r;
+    float green = col.g;
+    float blue  = col.b;
+
+    float minc  = min(col.r, min(col.g, col.b));
+    float maxc  = max(col.r, max(col.g, col.b));
+    float delta = maxc - minc;
+
+    float lum = (minc + maxc) * 0.5;
+    float sat = 0.0;
+    float hue = 0.0;
+
+    if (lum > 0.0 && lum < 1.0) {
+        float mul = (lum < 0.5) ? (lum) : (1.0 - lum);
+        sat       = delta / (mul * 2.0);
     }
-    return hsl;
+
+    vec3  masks = vec3((maxc == red && maxc != green) ? 1.0 : 0.0, (maxc == green && maxc != blue) ? 1.0 : 0.0, (maxc == blue && maxc != red) ? 1.0 : 0.0);
+
+    vec3  adds = vec3(((green - blue) / delta), 2.0 + ((blue - red) / delta), 4.0 + ((red - green) / delta));
+
+    float deltaGtz = (delta > 0.0) ? 1.0 : 0.0;
+
+    hue += dot(adds, masks);
+    hue *= deltaGtz;
+    hue /= 6.0;
+
+    if (hue < 0.0)
+        hue += 1.0;
+
+    return vec3(hue, sat, lum);
 }
-vec3 hsl2rgb(vec3 hsl) {
-    if (hsl.y == 0.) {
-        return vec3(hsl.z); //Luminance.
-    } else {
-        float b;
-        if (hsl.z < .5) {
-            b = hsl.z * (1. + hsl.y);
-        } else {
-            b = hsl.z + hsl.y - hsl.y * hsl.z;
-        }
-        float a = 2. * hsl.z - b;
-        return a + hue2rgb(hsl.x) * (b - a);
+vec3 hsl2rgb(vec3 col) {
+    const float onethird = 1.0 / 3.0;
+    const float twothird = 2.0 / 3.0;
+    const float rcpsixth = 6.0;
+
+    float       hue = col.x;
+    float       sat = col.y;
+    float       lum = col.z;
+
+    vec3        xt = vec3(rcpsixth * (hue - twothird), 0.0, rcpsixth * (1.0 - hue));
+
+    if (hue < twothird) {
+        xt.r = 0.0;
+        xt.g = rcpsixth * (twothird - hue);
+        xt.b = rcpsixth * (hue - onethird);
     }
+
+    if (hue < onethird) {
+        xt.r = rcpsixth * (onethird - hue);
+        xt.g = rcpsixth * hue;
+        xt.b = 0.0;
+    }
+
+    xt = min(xt, 1.0);
+
+    float sat2   = 2.0 * sat;
+    float satinv = 1.0 - sat;
+    float luminv = 1.0 - lum;
+    float lum2m1 = (2.0 * lum) - 1.0;
+    vec3  ct     = (sat2 * xt) + satinv;
+
+    vec3  rgb;
+    if (lum >= 0.5)
+        rgb = (luminv * ct) + lum2m1;
+    else
+        rgb = lum * ct;
+
+    return rgb;
 }
 
 void main() {
@@ -284,7 +312,6 @@ void main() {
         gl_FragColor = vec4(newColor, color[3]);
     }
 }
-
 )#";
 
 inline const std::string FRAGBLUR2 = R"#(

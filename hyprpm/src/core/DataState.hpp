d36055054651442db19df046cbdd0697b805c541
Vaxry <43317083+vaxerski@users.noreply.github.com>
2023-12-07 10:41:09 +0000

    hyprpm: Add hyprpm, a Hyprland Plugin Manager (#4072)

---
+++ hyprpm/src/core/DataState.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <string>
+#include <vector>
+#include "Plugin.hpp"
+
+struct SGlobalState {
+    std::string headersHashCompiled = "";
+    bool        dontWarnInstall     = false;
+};
+
+namespace DataState {
+    std::string                    getDataStatePath();
+    void                           ensureStateStoreExists();
+    void                           addNewPluginRepo(const SPluginRepository& repo);
+    void                           removePluginRepo(const std::string& urlOrName);
+    bool                           pluginRepoExists(const std::string& urlOrName);
+    void                           updateGlobalState(const SGlobalState& state);
+    SGlobalState                   getGlobalState();
+    bool                           setPluginEnabled(const std::string& name, bool enabled);
+    std::vector<SPluginRepository> getAllRepositories();
+};
---
+++ hyprpm/src/core/Manifest.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+enum eManifestType {
+    MANIFEST_HYPRLOAD,
+    MANIFEST_HYPRPM
+};
+
+class CManifest {
+  public:
+    CManifest(const eManifestType type, const std::string& path);
+
+    struct SManifestPlugin {
+        std::string              name;
+        std::string              description;
+        std::string              version;
+        std::vector<std::string> authors;
+        std::vector<std::string> buildSteps;
+        std::string              output;
+    };
+
+    struct {
+        std::string                                      name;
+        std::vector<std::string>                         authors;
+        std::vector<std::pair<std::string, std::string>> commitPins;
+    } m_sRepository;
+
+    std::vector<SManifestPlugin> m_vPlugins;
+    bool                         m_bGood = true;
+};

e1a7051b944b717cb07305110f9a435bfce18c74
vaxerski <vaxry@vaxry.net>
2023-12-25 18:04:00 +0100

    renderer: add option to blur popups closes #2134

--- src/helpers/WLClasses.hpp
+++ src/helpers/WLClasses.hpp
@@ -97,6 +97,8 @@ struct SRenderData {
 
     // for calculating UV
     CWindow* pWindow = nullptr;
+
+    bool     popup = false;
 };
 
 struct SExtensionFindingData {
@@ -180,7 +182,8 @@ struct SConstraint {
     Vector2D getLogicConstraintPos();
     Vector2D getLogicConstraintSize();
 
-    bool     operator==(const SConstraint& b) const {
+    //
+    bool operator==(const SConstraint& b) const {
         return constraint == b.constraint;
     }
 };

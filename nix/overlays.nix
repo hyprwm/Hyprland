1bd29f76943ced36007862e7fafe1a8fd02e9683
GrizzlT <13691001+GrizzlT@users.noreply.github.com>
2024-01-29 07:51:18 +0100

    nix: explicit prev overlay references

--- nix/overlays.nix
+++ nix/overlays.nix
@@ -28,10 +28,12 @@ in {
     (final: prev: let
       date = mkDate (self.lastModifiedDate or "19700101");
     in {
-      hyprland = prev.callPackage ./default.nix {
+      hyprland = final.callPackage ./default.nix {
         stdenv = prev.gcc13Stdenv;
         version = "${props.version}+date=${date}_${self.shortRev or "dirty"}";
+        wlroots = prev.wlroots-hyprland;
         commit = self.rev or "";
+        inherit (prev) udis86 hyprland-protocols;
         inherit date;
       };
       hyprland-unwrapped = final.hyprland.override {wrapRuntimeDeps = false;};

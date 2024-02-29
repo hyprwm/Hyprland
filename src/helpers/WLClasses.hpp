756e0bab30d2061f53090cc0fea951cc0d133207
Anton Samokhvalov <pg83@yandex.ru>
2024-02-29 18:01:56 +0300

    build: fix libc++/clang build (#4886)

--- src/helpers/WLClasses.hpp
+++ src/helpers/WLClasses.hpp
@@ -391,7 +391,7 @@ struct STearingController {
     DYNLISTENER(set);
     DYNLISTENER(destroy);
 
-    bool operator==(const STearingController& other) {
+    bool operator==(const STearingController& other) const {
         return pWlrHint == other.pWlrHint;
     }
 };
@@ -401,7 +401,7 @@ struct SShortcutInhibitor {
 
     DYNLISTENER(destroy);
 
-    bool operator==(const SShortcutInhibitor& other) {
+    bool operator==(const SShortcutInhibitor& other) const {
         return pWlrInhibitor == other.pWlrInhibitor;
     }
 };

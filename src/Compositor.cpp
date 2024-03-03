8c3c1abce26660ca4dd1def43e7d9c24e8701682
Tom Benham <tom.benham@quadrille.fr>
2024-03-03 22:44:05 +0100

    Removed redundant boolean condition

--- src/Compositor.cpp
+++ src/Compositor.cpp
@@ -2150,7 +2150,7 @@ void CCompositor::moveWorkspaceToMonitor(CWorkspace* pWorkspace, CMonitor* pMoni
 
     // fix old mon
     int nextWorkspaceOnMonitorID = -1;
-    if (!SWITCHINGISACTIVE || !POLDMON)
+    if (!SWITCHINGISACTIVE)
         nextWorkspaceOnMonitorID = pWorkspace->m_iID;
     else {
         for (auto& w : m_vWorkspaces) {

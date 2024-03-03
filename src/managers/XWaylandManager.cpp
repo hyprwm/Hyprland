acf0b536a643bd5c87cd6097f5c94b4041797c15
Epilepsy Gatherings <73647246+phonetic112@users.noreply.github.com>
2024-03-02 19:18:53 -0500

    xwayland: disable initial focus for xwayland dialogs (#4936)

--- src/managers/XWaylandManager.cpp
+++ src/managers/XWaylandManager.cpp
@@ -222,9 +222,7 @@ bool CHyprXWaylandManager::shouldBeFloated(CWindow* pWindow, bool pending) {
                     pWindow->m_uSurface.xwayland->window_type[i] == HYPRATOMS["_NET_WM_WINDOW_TYPE_MENU"])
                     pWindow->m_bX11ShouldntFocus = true;
 
-                if (pWindow->m_uSurface.xwayland->window_type[i] != HYPRATOMS["_NET_WM_WINDOW_TYPE_DIALOG"])
-                    pWindow->m_bNoInitialFocus = true;
-
+                pWindow->m_bNoInitialFocus = true;
                 return true;
             }
 

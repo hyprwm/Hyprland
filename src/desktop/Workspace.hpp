847f2047d77a47823b0b4fc7ea1d58d26a65108c
Vaxry <vaxry@vaxry.net>
2024-04-05 19:32:05 +0100

    core: simplify sanityCheckWorkspaces

--- src/desktop/Workspace.hpp
+++ src/desktop/Workspace.hpp
@@ -61,6 +61,8 @@ class CWorkspace {
     // Whether the user configured command for on-created-empty has been executed, if any
     bool m_bOnCreatedEmptyExecuted = false;
 
+    bool m_bPersistent = false;
+
     // Inert: destroyed and invalid. If this is true, release the ptr you have.
     bool        inert();
 

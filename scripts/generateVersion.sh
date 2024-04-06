6cea710ac8e966366763b9609895dcef7fdb5bcb
Vaxry <vaxry@vaxry.net>
2024-04-06 15:40:06 +0100

    scripts: switch to branch --show-current for branch in generateVersion

--- scripts/generateVersion.sh
+++ scripts/generateVersion.sh
@@ -2,7 +2,7 @@
 cp -fr ./src/version.h.in ./src/version.h
 
 HASH=$(git rev-parse HEAD)
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
+BRANCH=$(git branch --show-current)
 MESSAGE=$(git show ${GIT_COMMIT_HASH} | head -n 5 | tail -n 1 | sed -e 's/#//g' -e 's/\"//g')
 DATE=$(git show ${GIT_COMMIT_HASH} --no-patch --format=%cd --date=local)
 DIRTY=$(git diff-index --quiet HEAD -- || echo dirty)

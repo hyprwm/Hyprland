fc63d2960999071410cf9a84eaf89d39c9dfdc2a
Vaxry <vaxry@vaxry.net>
2024-04-20 19:34:30 +0100

    wayland-protocol: remove unused CWaylandResource

--- src/protocols/WaylandProtocol.hpp
+++ src/protocols/WaylandProtocol.hpp
@@ -14,34 +14,6 @@
 
 #define PROTO NProtocols
 
-class CWaylandResource {
-  public:
-    CWaylandResource(wl_client* client, const wl_interface* wlInterface, uint32_t version, uint32_t id);
-    ~CWaylandResource();
-
-    bool         good();
-    wl_resource* resource();
-    uint32_t     version();
-
-    void         setImplementation(const void* impl, wl_resource_destroy_func_t df);
-
-    wl_listener  m_liResourceDestroy; // private but has to be public
-    void         markDefunct();
-
-    void*        data();
-    void         setData(void* data);
-
-    void         setOnDestroyHandler(std::function<void(CWaylandResource* res)> fn);
-
-  private:
-    bool                                       m_bImplementationSet = false;
-    bool                                       m_bDefunct           = false; // m_liResourceDestroy fired
-    wl_client*                                 m_pWLClient          = nullptr;
-    wl_resource*                               m_pWLResource        = nullptr;
-    void*                                      m_pData              = nullptr;
-    std::function<void(CWaylandResource* res)> m_fOnDestroyHandler;
-};
-
 class IWaylandProtocol {
   public:
     IWaylandProtocol(const wl_interface* iface, const int& ver, const std::string& name);

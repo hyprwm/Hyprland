2dad411516eaec06234ff4aa1bc55ef48b70712b
Gabriel Ford <gabe@fordltc.net>
2024-04-22 20:09:42 -0400

    Run clang-format.

--- src/config/ConfigManager.cpp
+++ src/config/ConfigManager.cpp
@@ -1641,7 +1641,7 @@ std::optional<std::string> CConfigManager::handleMonitor(const std::string& comm
 
     if (ARGS[2].starts_with("auto")) {
         newrule.offset = Vector2D(-INT32_MAX, -INT32_MAX);
-		// If this is the first monitor rule needs to be on the right.
+        // If this is the first monitor rule needs to be on the right.
         if (ARGS[2] == "auto-right" || ARGS[2] == "auto" || m_dMonitorRules.empty())
             newrule.autoDir = eAutoDirs::DIR_AUTO_RIGHT;
         else if (ARGS[2] == "auto-left")

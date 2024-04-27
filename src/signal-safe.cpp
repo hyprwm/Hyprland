3765778bfaa91e031ceb3e072c8b12812e22f0c1
virchau13 <virchau13@hexular.net>
2024-04-28 00:38:48 +0800

    CrashReporter: fix deadlocks by making it mostly async-signal-safe (#5771) `CrashReporter::createAndSaveCrash()` is not async-signal-safe, resulting in random deadlocks/double-crashes during Hyprland crashes. This changes the function to be (mostly) async-signal-safe.

---
+++ src/signal-safe.cpp
@@ -0,0 +1,30 @@
+#include "signal-safe.hpp"
+
+#ifndef __GLIBC__
+#include <signal.h>
+#endif
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+extern char** environ;
+
+char const*   sig_getenv(char const* name) {
+    int len = strlen(name);
+    for (char** var = environ; *var != NULL; var++) {
+        if (strncmp(*var, name, len) == 0 && (*var)[len] == '=') {
+            return (*var) + len + 1;
+        }
+    }
+    return NULL;
+}
+
+char const* sig_strsignal(int sig) {
+#ifdef __GLIBC__
+    return sigabbrev_np(sig);
+#elif defined(__DragonFly__) || defined(__FreeBSD__)
+    return sys_signame[sig];
+#else
+    return "unknown";
+#endif
+}

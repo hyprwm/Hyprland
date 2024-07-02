8fa4716936e62e4dd0a4c6413d80044cba427e53
Yusuf Duran <firatyusuf.d@gmail.com>
2024-07-03 00:13:36 +0200

    format hyprctl.hpp

--- src/debug/HyprCtl.hpp
+++ src/debug/HyprCtl.hpp
@@ -29,8 +29,8 @@ class CHyprCtl {
     wl_event_source*                 m_eventSource = nullptr;
 };
 
-std::string getWindowData(PHLWINDOW w, eHyprCtlOutputFormat format);
-std::string getWorkspaceData(PHLWORKSPACE w, eHyprCtlOutputFormat format);
-std::string getMonitorData(Hyprutils::Memory::CSharedPointer<CMonitor> m, eHyprCtlOutputFormat format);
+std::string                      getWindowData(PHLWINDOW w, eHyprCtlOutputFormat format);
+std::string                      getWorkspaceData(PHLWORKSPACE w, eHyprCtlOutputFormat format);
+std::string                      getMonitorData(Hyprutils::Memory::CSharedPointer<CMonitor> m, eHyprCtlOutputFormat format);
 
 inline std::unique_ptr<CHyprCtl> g_pHyprCtl;

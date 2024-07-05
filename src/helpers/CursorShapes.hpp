b27879725cad4eae14a97bc4345fa9c4d52c0a37
Vaxry <vaxry@vaxry.net>
2024-07-05 23:22:49 +0200

    missed a file

---
+++ src/helpers/CursorShapes.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <array>
+
+// clang-format off
+constexpr std::array<const char*, 35> CURSOR_SHAPE_NAMES = {
+    "invalid",
+    "default",
+    "context-menu",
+    "help",
+    "pointer",
+    "progress",
+    "wait",
+    "cell",
+    "crosshair",
+    "text",
+    "vertical-text",
+    "alias",
+    "copy",
+    "move",
+    "no-drop",
+    "not-allowed",
+    "grab",
+    "grabbing",
+    "e-resize",
+    "n-resize",
+    "ne-resize",
+    "nw-resize",
+    "s-resize",
+    "se-resize",
+    "sw-resize",
+    "w-resize",
+    "ew-resize",
+    "ns-resize",
+    "nesw-resize",
+    "nwse-resize",
+    "col-resize",
+    "row-resize",
+    "all-scroll",
+    "zoom-in",
+    "zoom-out",
+};
+// clang-format on

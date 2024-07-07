bd25149daf01c03712a2ef3ba3ebc3623067eb11
Mihai Fufezan <mihai@fufexan.net>
2024-07-07 17:10:57 +0300

    Nix: add xcursor dep

--- nix/default.nix
+++ nix/default.nix
@@ -138,6 +138,7 @@ assert lib.assertMsg (!hidpiXWayland) "The option `hidpiXWayland` has been remov
       (lib.optionals stdenv.hostPlatform.isMusl [libexecinfo])
       (lib.optionals enableXWayland [
         xorg.libxcb
+        xorg.libXcursor
         xorg.libXdmcp
         xorg.xcbutil
         xorg.xcbutilerrors

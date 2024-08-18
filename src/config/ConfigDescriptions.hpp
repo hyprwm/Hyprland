bed0bd93e59049fff16d0d9f88ba54fc691cec11
ParaN3xus <paran3xus007@gmail.com>
2024-08-18 08:00:00 +0800

    feat: add `focus_on_close` to ConfigDescriptions.hpp

--- src/config/ConfigDescriptions.hpp
+++ src/config/ConfigDescriptions.hpp
@@ -454,6 +454,12 @@ inline static const std::vector<SConfigOptionDescription> CONFIG_OPTIONS = {
         .data        = SConfigOptionDescription::SRangeData{1, 0, 3},
     },
     SConfigOptionDescription{
+        .value       = "input:focus_on_close",
+        .description = "Controls the window focus behavior when a window is closed. When set to 0, focus will shift to the next window candidate. When set to 1, focus will shift to the window under the cursor. [0/1]",
+        .type        = CONFIG_OPTION_INT,
+        .data        = SConfigOptionDescription::SRangeData{0, 0, 1},
+    },
+    SConfigOptionDescription{
         .value       = "input:mouse_refocus",
         .description = "if disabled, mouse focus won't switch to the hovered window unless the mouse crosses a window boundary when follow_mouse=1.",
         .type        = CONFIG_OPTION_BOOL,

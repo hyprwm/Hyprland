8ad608fd36cafaa57e3697e11f0b90bdfea1c9f9
ParaN3xus <paran3xus007@gmail.com>
2024-08-21 07:59:00 +0800

    styl: format `ConfigDescriptions.hpp`

--- src/config/ConfigDescriptions.hpp
+++ src/config/ConfigDescriptions.hpp
@@ -455,7 +455,8 @@ inline static const std::vector<SConfigOptionDescription> CONFIG_OPTIONS = {
     },
     SConfigOptionDescription{
         .value       = "input:focus_on_close",
-        .description = "Controls the window focus behavior when a window is closed. When set to 0, focus will shift to the next window candidate. When set to 1, focus will shift to the window under the cursor.",
+        .description = "Controls the window focus behavior when a window is closed. When set to 0, focus will shift to the next window candidate. When set to 1, focus will shift "
+                       "to the window under the cursor.",
         .type        = CONFIG_OPTION_CHOICE,
         .data        = SConfigOptionDescription::SChoiceData{0, "next,cursor"},
     },

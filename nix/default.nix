b45b9f109626b9e7d8e4890958b163a990c7afb5
Mihai Fufezan <mihai@fufexan.net>
2024-08-22 14:30:10 +0300

    Nix: exclude wayland-scanner until next staging merge

--- nix/default.nix
+++ nix/default.nix
@@ -97,7 +97,8 @@ assert lib.assertMsg (!hidpiXWayland) "The option `hidpiXWayland` has been remov
       ninja
       pkg-config
       python3 # for udis86
-      wayland-scanner
+      # re-add after https://github.com/NixOS/nixpkgs/pull/214906 hits nixos-unstable
+      # wayland-scanner
     ];
 
     outputs = [

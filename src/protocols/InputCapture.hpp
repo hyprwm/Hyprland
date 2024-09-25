32245e8593f4ee3f3a58bb07dbbfc468c45298d0
Gwilherm Folliot <gwilherm55fo@gmail.com>
2024-09-25 18:03:12 +0200

    input-capture: impl keyboard, mouse button & mouse wheel

--- src/protocols/InputCapture.hpp
+++ src/protocols/InputCapture.hpp
@@ -9,12 +9,20 @@ class CInputCaptureProtocol : public IWaylandProtocol {
     CInputCaptureProtocol(const wl_interface* iface, const int& ver, const std::string& name);
     virtual void bindManager(wl_client* client, void* data, uint32_t ver, uint32_t id);
     void         sendAbsoluteMotion(const Vector2D& absolutePosition, const Vector2D& delta);
+    void         sendKey(uint32_t keyCode, hyprlandInputCaptureManagerV1KeyState state);
+    void         sendButton(uint32_t button, hyprlandInputCaptureManagerV1ButtonState state);
+    void         sendAxis(hyprlandInputCaptureManagerV1Axis axis, double value);
+    void         sendAxisValue120(hyprlandInputCaptureManagerV1Axis axis, int32_t value120);
+    void         sendAxisStop(hyprlandInputCaptureManagerV1Axis axis);
+
+    void         sendFrame();
 
   private:
     void onManagerResourceDestroy(wl_resource* res);
     void onCapture(CHyprlandInputCaptureManagerV1* pMgr);
     void onRelease(CHyprlandInputCaptureManagerV1* pMgr);
 
+    //
     std::vector<UP<CHyprlandInputCaptureManagerV1>> m_vManagers;
 };
 

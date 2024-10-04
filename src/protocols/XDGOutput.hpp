bfeb13f62048d8d614fb2b1cf24bbe5d533c97e7
Vaxry <vaxry@vaxry.net>
2024-10-05 00:43:59 +0100

    xdg-output: minor cleanups

--- src/protocols/XDGOutput.hpp
+++ src/protocols/XDGOutput.hpp
@@ -9,12 +9,12 @@ class CXDGOutputProtocol;
 
 class CXDGOutput {
   public:
-    CXDGOutput(SP<CZxdgOutputV1> resource, CMonitor* monitor_);
+    CXDGOutput(SP<CZxdgOutputV1> resource, SP<CMonitor> monitor_);
 
     void sendDetails();
 
   private:
-    CMonitor*               monitor = nullptr;
+    WP<CMonitor>            monitor;
     SP<CZxdgOutputV1>       resource;
 
     std::optional<Vector2D> overridePosition;
@@ -30,12 +30,12 @@ class CXDGOutputProtocol : public IWaylandProtocol {
     CXDGOutputProtocol(const wl_interface* iface, const int& ver, const std::string& name);
 
     virtual void bindManager(wl_client* client, void* data, uint32_t ver, uint32_t id);
+    void         updateAllOutputs();
 
   private:
     void onManagerResourceDestroy(wl_resource* res);
     void onOutputResourceDestroy(wl_resource* res);
     void onManagerGetXDGOutput(CZxdgOutputManagerV1* mgr, uint32_t id, wl_resource* outputResource);
-    void updateAllOutputs();
 
     //
     std::vector<UP<CZxdgOutputManagerV1>> m_vManagerResources;

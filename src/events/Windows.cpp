d1fd8456e4d51e74326b6e139dbe34a1a4a743d7
Vaxry <vaxry@vaxry.net>
2024-10-19 16:24:03 +0100

    window: guard PMONITOR in commit listener ref #8170

--- src/events/Windows.cpp
+++ src/events/Windows.cpp
@@ -775,7 +775,9 @@ void Events::listener_commitWindow(void* owner, void* data) {
 
     const auto PMONITOR = g_pCompositor->getMonitorFromID(PWINDOW->m_iMonitorID);
 
-    PMONITOR->debugLastPresentation(g_pSeatManager->isPointerFrameCommit ? "listener_commitWindow skip" : "listener_commitWindow");
+    if (PMONITOR)
+        PMONITOR->debugLastPresentation(g_pSeatManager->isPointerFrameCommit ? "listener_commitWindow skip" : "listener_commitWindow");
+
     if (g_pSeatManager->isPointerFrameCommit) {
         g_pSeatManager->isPointerFrameSkipped = false;
         g_pSeatManager->isPointerFrameCommit  = false;

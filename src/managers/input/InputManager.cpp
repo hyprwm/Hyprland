59181bd6819142ffcdd0cb713b35a7bde9b80cf3
Gwilherm Folliot <gwilherm55fo@gmail.com>
2024-12-17 19:20:52 +0100

    input-capture: send modifiers (highly experimental)

--- src/managers/input/InputManager.cpp
+++ src/managers/input/InputManager.cpp
@@ -1513,6 +1513,10 @@ void CInputManager::onKeyboardMod(SP<IKeyboard> pKeyboard) {
     const auto ALLMODS = shareModsFromAllKBs(MODS.depressed);
     MODS.depressed     = ALLMODS;
 
+    PROTO::inputCapture->sendModifiers(MODS.depressed,MODS.latched, MODS.locked, MODS.group);
+
+    if (PROTO::inputCapture->isCaptured())
+        return;
     const auto IME = m_relay.m_inputMethod.lock();
 
     if (IME && IME->hasGrab() && !DISALLOWACTION) {

df06cb4d71a291171c02467c384471f90832d23b
Jan Beich <jbeich@FreeBSD.org>
2024-12-18 19:15:44 +0000

    core: Unbreak build on FreeBSD (#8762)
    * CrashReporter: restore explicit environ(7) after 8bbeee11734d
    
    BSD systems don't declare environ(7) in any header like POSIX suggests.
    
    src/signal-safe.cpp:12:23: error: use of undeclared identifier 'environ'
       12 |     for (char** var = environ; *var != nullptr; var++) {
          |                       ^
    
    * fix
    
    ---------
    
    Co-authored-by: Vaxry <vaxry@vaxry.net>

--- src/signal-safe.cpp
+++ src/signal-safe.cpp
@@ -7,6 +7,10 @@
 #include <unistd.h>
 #include <cstring>
 
+// NOLINTNEXTLINE
+extern "C" char** environ;
+
+//
 char const* sigGetenv(char const* name) {
     const size_t len = strlen(name);
     for (char** var = environ; *var != nullptr; var++) {

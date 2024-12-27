43ca66779b7d267cc6c52bdea75d3ff8eb60132f
Tuur Vanhoutte <4633209+zjeffer@users.noreply.github.com>
2024-12-27 15:40:46 +0100

    hyprpm: use glaze to parse hyprctl plugin list (#8812)
    * Use std::filesystem::path in hyprpm DataState to avoid concatenating strings with (folder + "/" + file)
    * Added getPluginStates helper method in DataState
    * Small clang-tidy improvements

--- hyprpm/src/core/DataState.hpp
+++ hyprpm/src/core/DataState.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <filesystem>
 #include <string>
 #include <vector>
 #include "Plugin.hpp"
@@ -9,14 +10,15 @@ struct SGlobalState {
 };
 
 namespace DataState {
-    std::string                    getDataStatePath();
-    std::string                    getHeadersPath();
-    void                           ensureStateStoreExists();
-    void                           addNewPluginRepo(const SPluginRepository& repo);
-    void                           removePluginRepo(const std::string& urlOrName);
-    bool                           pluginRepoExists(const std::string& urlOrName);
-    void                           updateGlobalState(const SGlobalState& state);
-    SGlobalState                   getGlobalState();
-    bool                           setPluginEnabled(const std::string& name, bool enabled);
-    std::vector<SPluginRepository> getAllRepositories();
+    std::filesystem::path              getDataStatePath();
+    std::string                        getHeadersPath();
+    std::vector<std::filesystem::path> getPluginStates();
+    void                               ensureStateStoreExists();
+    void                               addNewPluginRepo(const SPluginRepository& repo);
+    void                               removePluginRepo(const std::string& urlOrName);
+    bool                               pluginRepoExists(const std::string& urlOrName);
+    void                               updateGlobalState(const SGlobalState& state);
+    SGlobalState                       getGlobalState();
+    bool                               setPluginEnabled(const std::string& name, bool enabled);
+    std::vector<SPluginRepository>     getAllRepositories();
 };

d2f9ba3d67164f84df698ffe961832a7526e7a6e
Maximilian Seidler <paideia_dilemma@losfuzzys.net>
2025-01-02 18:00:17 +0100

    readd equal operator for enabled int

--- src/config/ConfigManager.cpp
+++ src/config/ConfigManager.cpp
@@ -2038,7 +2038,7 @@ std::optional<std::string> CConfigManager::handleAnimation(const std::string& co
         return "no such animation";
 
     // This helper casts strings like "1", "true", "off", "yes"... to int.
-    int64_t enabledInt = configStringToInt(ARGS[1]).value_or(0);
+    int64_t enabledInt = configStringToInt(ARGS[1]).value_or(0) == 1;
 
     // Checking that the int is 1 or 0 because the helper can return integers out of range.
     if (enabledInt != 0 && enabledInt != 1)

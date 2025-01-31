8c59f73770775c7b1850929cdebed5c28199c020
Gwilherm Folliot <gwilherm55fo@gmail.com>
2025-01-31 15:15:41 +0100

    input-capture: fix build

--- src/protocols/InputCapture.cpp
+++ src/protocols/InputCapture.cpp
@@ -51,7 +51,7 @@ void CInputCaptureProtocol::sendKeymap(SP<IKeyboard> keyboard, const UP<CHyprlan
     uint32_t                                  size;
     if (keyboard) {
         format = HYPRLAND_INPUT_CAPTURE_MANAGER_V1_KEYMAP_FORMAT_XKB_V1;
-        fd     = keyboard->xkbKeymapFD;
+        fd     = keyboard->xkbKeymapFD.get();
         size   = keyboard->xkbKeymapString.length() + 1;
     } else {
         format = HYPRLAND_INPUT_CAPTURE_MANAGER_V1_KEYMAP_FORMAT_NO_KEYMAP;

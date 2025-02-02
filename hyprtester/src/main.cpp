7b4c52b0726ffc844a9b907ab754d8a2cfc829ce
Vaxry <vaxry@vaxry.net>
2025-02-02 18:43:01 +0000

    add a super basic test suite

---
+++ hyprtester/src/main.cpp
@@ -0,0 +1,84 @@
+
+// This is a tester for Hyprland. It will launch the built binary in ./build/Hyprland
+// in headless mode and test various things.
+// for now it's quite basic and limited, but will be expanded in the future.
+
+// NOTE: This tester has to be ran from its directory!!
+
+// Some TODO:
+// - Add a plugin built alongside so that we can do more detailed tests (e.g. simulating keystrokes)
+// - test coverage
+// - maybe figure out a way to do some visual tests too?
+
+// Required runtime deps for checks:
+// - kitty
+
+#include "shared.hpp"
+#include "hyprctlCompat.hpp"
+#include "tests/window/window.hpp"
+
+#include <filesystem>
+#include <hyprutils/os/Process.hpp>
+#include <csignal>
+#include <cerrno>
+#include <chrono>
+#include <thread>
+#include <print>
+
+using namespace Hyprutils::OS;
+
+static int                     ret = 0;
+static Hyprutils::OS::CProcess hyprlandProc("", {});
+static const std::string       cwd = std::filesystem::current_path().string();
+
+//
+static bool launchHyprland() {
+    std::error_code ec;
+    if (!std::filesystem::exists(cwd + "/../build/Hyprland", ec) || ec) {
+        std::println("{}No Hyprland binary", Colors::RED);
+        return false;
+    }
+
+    hyprlandProc = CProcess{cwd + "/../build/Hyprland", {"--config", cwd + "/test.conf"}};
+    hyprlandProc.addEnv("HYPRLAND_HEADLESS_ONLY", "1");
+
+    return hyprlandProc.runAsync();
+}
+
+static bool hyprlandAlive() {
+    kill(hyprlandProc.pid(), 0);
+    return errno != ESRCH;
+}
+
+int main(int argc, char** argv, char** envp) {
+
+    if (!launchHyprland())
+        return 1;
+
+    // hyprland has launched, let's check if it's alive after 2s
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    if (!hyprlandAlive()) {
+        std::println("{}Hyprland failed to launch", Colors::RED);
+        return 1;
+    }
+
+    // wonderful, we are in. Let's get the instance signature.
+    const auto INSTANCES = instances();
+    if (INSTANCES.empty()) {
+        std::println("{}Hyprland failed to launch (2)", Colors::RED);
+        return 1;
+    }
+
+    HIS       = INSTANCES.back().id;
+    WLDISPLAY = INSTANCES.back().wlSocket;
+
+    getFromSocket("/output create headless");
+
+    // now we can start issuing stuff.
+    EXPECT(testWindows(), true);
+
+    // kill hyprland
+    getFromSocket("/dispatch exit");
+
+    return ret;
+}
---
+++ hyprtester/src/shared.hpp
@@ -0,0 +1,37 @@
+// Stolen from hyprutils
+
+#pragma once
+#include <iostream>
+
+inline std::string HIS       = "";
+inline std::string WLDISPLAY = "";
+
+namespace Colors {
+    constexpr const char* RED     = "\x1b[31m";
+    constexpr const char* GREEN   = "\x1b[32m";
+    constexpr const char* YELLOW  = "\x1b[33m";
+    constexpr const char* BLUE    = "\x1b[34m";
+    constexpr const char* MAGENTA = "\x1b[35m";
+    constexpr const char* CYAN    = "\x1b[36m";
+    constexpr const char* RESET   = "\x1b[0m";
+};
+
+#define EXPECT(expr, val)                                                                                                                                                          \
+    if (const auto RESULT = expr; RESULT != (val)) {                                                                                                                               \
+        std::cout << Colors::RED << "Failed: " << Colors::RESET << #expr << ", expected " << val << " but got " << RESULT << "\n";                                                 \
+        ret = 1;                                                                                                                                                                   \
+    } else {                                                                                                                                                                       \
+        std::cout << Colors::GREEN << "Passed " << Colors::RESET << #expr << ". Got " << val << "\n";                                                                              \
+    }
+#define EXPECT_VECTOR2D(expr, val)                                                                                                                                                 \
+    do {                                                                                                                                                                           \
+        const auto& RESULT   = expr;                                                                                                                                               \
+        const auto& EXPECTED = val;                                                                                                                                                \
+        if (!(std::abs(RESULT.x - EXPECTED.x) < 1e-6 && std::abs(RESULT.y - EXPECTED.y) < 1e-6)) {                                                                                 \
+            std::cout << Colors::RED << "Failed: " << Colors::RESET << #expr << ", expected (" << EXPECTED.x << ", " << EXPECTED.y << ") but got (" << RESULT.x << ", "            \
+                      << RESULT.y << ")\n";                                                                                                                                        \
+            ret = 1;                                                                                                                                                               \
+        } else {                                                                                                                                                                   \
+            std::cout << Colors::GREEN << "Passed " << Colors::RESET << #expr << ". Got (" << RESULT.x << ", " << RESULT.y << ")\n";                                               \
+        }                                                                                                                                                                          \
+    } while (0)

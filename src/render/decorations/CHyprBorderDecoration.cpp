6b7c8a01e3071c5b50902dc058f0b44df01bee43
Maximilian Seidler <78690852+PaideiaDilemma@users.noreply.github.com>
2025-02-13 11:08:03 +0000

    animations: sync inactive/active border angles when using borderangle animations (#9401)

--- src/render/decorations/CHyprBorderDecoration.cpp
+++ src/render/decorations/CHyprBorderDecoration.cpp
@@ -63,6 +63,11 @@ void CHyprBorderDecoration::draw(PHLMONITOR pMonitor, float const& a) {
     if (m_pWindow->m_fBorderAngleAnimationProgress->enabled()) {
         grad.m_fAngle += m_pWindow->m_fBorderAngleAnimationProgress->value() * M_PI * 2;
         grad.m_fAngle = normalizeAngleRad(grad.m_fAngle);
+
+        // When borderangle is animated, it is counterintuitive to fade between inactive/active gradient angles.
+        // Instead we sync the angles to avoid fading between them and additionally rotating the border angle.
+        if (ANIMATED)
+            m_pWindow->m_cRealBorderColorPrevious.m_fAngle = grad.m_fAngle;
     }
 
     int                             borderSize    = m_pWindow->getRealBorderSize();

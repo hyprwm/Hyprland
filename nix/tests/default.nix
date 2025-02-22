625671613751abb3311ef239c5db3a3fc0c2353b
Mihai Fufezan <mihai@fufexan.net>
2025-02-22 18:43:28 +0200

    Nix/tests: run hyprtester
    XWayland fails

--- nix/tests/default.nix
+++ nix/tests/default.nix
@@ -7,6 +7,7 @@ in {
     nodes.machine = {pkgs, ...}: {
       environment.systemPackages = with pkgs; [
         flake.hyprtester
+        gdb
 
         # Programs needed for tests
         kitty
@@ -19,45 +20,47 @@ in {
       environment.variables = {
         "AQ_TRACE" = "1";
         "HYPRLAND_TRACE" = "1";
-        "LIBGL_ALWAYS_SOFTWARE" = "1";
+        # Doesn't make a difference
+        # "LIBGL_ALWAYS_SOFTWARE" = "1";
         "XDG_RUNTIME_DIR" = "/tmp";
         "XDG_CACHE_HOME" = "/tmp";
       };
 
-      # Automatically configure and start Hyprland when logging in on tty1:
-      programs.bash.loginShellInit = ''
-        if [ "$(tty)" = "/dev/tty1" ] && [ ! -f /tmp/hyprland-exit ]; then
-          set -e
-
-          # hyprtester -b ${flake.hyprland}/bin/Hyprland -c ${flake.hyprtester}/share/hypr/test.conf; touch /tmp/hyprland-exit
-          hyprtester -b ${flake.hyprland}/bin/Hyprland -c /etc/test.conf; touch /tmp/hyprland-exit
-        fi
-      '';
+      # Automatically configure and start hyprtester when logging in on tty1:
+      # programs.bash.loginShellInit = ''
+      #   if [ "$(tty)" = "/dev/tty1" ] && [ ! -f /tmp/shell-nologin ]; then
+      #     set -e
+      #     # prevent this if from running again
+      #     touch /tmp/shell-nologin
+      # 
+      #     hyprtester -b ${flake.hyprland-debug}/bin/Hyprland -c ${flake.hyprtester}/share/hypr/test2.conf; touch /tmp/hyprland-exit
+      #   fi
+      # '';
 
       programs.hyprland = {
         enable = true;
-        package = flake.hyprland;
+        package = flake.hyprland-debug;
         # We don't need portals in this test, so we don't set portalPackage
       };
 
-      environment.etc."test.conf".source = ./test.conf;
+      # Original conf
+      environment.etc."test.conf".source = "${flake.hyprtester}/share/hypr/test.conf";
+      # New conf without HEADLESS outputs, which don't work
+      environment.etc."test2.conf".source = ./test.conf;
 
       # Disable portals
       xdg.portal.enable = pkgs.lib.mkForce false;
 
-      users.users.alice = {
-        isNormalUser = true;
-        extraGroups = ["wheel"];
-        initialPassword = "123";
-      };
-
-      # Autologin alice into tty
-      services.getty.autologinUser = "alice";
+      # Autologin root into tty
+      services.getty.autologinUser = "root";
 
       system.stateVersion = "24.11";
 
       # Might crash with less
-      virtualisation.memorySize = 2048;
+      virtualisation.memorySize = 8192;
+
+      # Doesn't seem to do much, thought it would fix XWayland crashing
+      virtualisation.qemu.options = [ "-vga none -device virtio-gpu-pci" ];
     };
 
     testScript = ''
@@ -65,9 +68,7 @@ in {
       machine.wait_for_unit("multi-user.target")
 
       # Run hyprtester testing framework/suite
-      # machine.succeed("su -- alice -c 'hyprtester -b ${flake.hyprland}/bin/Hyprland -c /etc/test.conf'")
-      # 
-      machine.wait_for_file("/tmp/hyprland-exit")
+      machine.execute("hyprtester -b ${flake.hyprland-debug}/bin/Hyprland -c /etc/test2.conf")
 
       # Finally - shutdown
       machine.shutdown()

4ee1b6e996119aaea0ab2f1ed0a5c40216bbd643
dawsers <dawser@gmx.com>
2025-03-04 10:24:44 +0100

    dispatchers: scaleactive allow threshold if multiplier not standard

--- src/managers/KeybindManager.cpp
+++ src/managers/KeybindManager.cpp
@@ -2174,8 +2174,8 @@ SDispatchResult CKeybindManager::scaleActive(std::string args) {
         scale += 1.0 / PLASTWINDOW->m_sWindowData.contentScale.valueOr(1.0f);
 
     if (scale > 0.0f) {
-        if (scale >= 0.25f)
-            PLASTWINDOW->m_sWindowData.contentScale = CWindowOverridableVar(1.0f / scale, PRIORITY_SET_PROP);
+        scale = std::max(scale, 0.25f);
+        PLASTWINDOW->m_sWindowData.contentScale = CWindowOverridableVar(1.0f / scale, PRIORITY_SET_PROP);
     } else
         PLASTWINDOW->m_sWindowData.contentScale.unset(PRIORITY_SET_PROP);
 

ea258888767163f29e77485dcce86efa88e46f6f
UjinT34 <ujint34@mail.ru>
2025-03-22 15:08:40 +0300

    fix shader dir

--- scripts/generateShaderIncludes.sh
+++ scripts/generateShaderIncludes.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
+echo "-- Generating shader includes"
+
+if [ ! -d ./src/render/shaders ]; then
+	mkdir ./src/render/shaders
+fi
+
 echo '#pragma once' > ./src/render/shaders/Shaders.hpp
 echo '#include <map>' >> ./src/render/shaders/Shaders.hpp
 echo 'static const std::map<std::string, std::string> SHADERS = {' >> ./src/render/shaders/Shaders.hpp
 
 for filename in `ls ./assets/shaders/`; do
+	echo "--	${filename}"
+	
 	{ echo 'R"#('; cat ./assets/shaders/${filename}; echo ')#"'; } > ./src/render/shaders/${filename}.inc
 	echo "{\"${filename}\"," >> ./src/render/shaders/Shaders.hpp
 	echo "#include \"./${filename}.inc\"" >> ./src/render/shaders/Shaders.hpp

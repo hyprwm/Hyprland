17668914216b5abf9951177af8e6b8baad370e36
UjinT34 <ujint34@mail.ru>
2025-03-24 22:22:55 +0300

    move shaders to src/render/shaders/glsl

--- scripts/generateShaderIncludes.sh
+++ scripts/generateShaderIncludes.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+SHADERS_SRC="./src/render/shaders/glsl"
+
 echo "-- Generating shader includes"
 
 if [ ! -d ./src/render/shaders ]; then
@@ -10,10 +12,10 @@ echo '#pragma once' > ./src/render/shaders/Shaders.hpp
 echo '#include <map>' >> ./src/render/shaders/Shaders.hpp
 echo 'static const std::map<std::string, std::string> SHADERS = {' >> ./src/render/shaders/Shaders.hpp
 
-for filename in `ls ./assets/shaders/`; do
+for filename in `ls ${SHADERS_SRC}`; do
 	echo "--	${filename}"
 	
-	{ echo 'R"#('; cat ./assets/shaders/${filename}; echo ')#"'; } > ./src/render/shaders/${filename}.inc
+	{ echo 'R"#('; cat ${SHADERS_SRC}/${filename}; echo ')#"'; } > ./src/render/shaders/${filename}.inc
 	echo "{\"${filename}\"," >> ./src/render/shaders/Shaders.hpp
 	echo "#include \"./${filename}.inc\"" >> ./src/render/shaders/Shaders.hpp
 	echo "}," >> ./src/render/shaders/Shaders.hpp

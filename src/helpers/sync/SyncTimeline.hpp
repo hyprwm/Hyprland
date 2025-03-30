571090ff43297afda63dae336a1ab31b1e98a9dd
Ikalco <73481042+ikalco@users.noreply.github.com>
2025-03-30 12:11:47 -0500

    wait on dmabuf buffers to be readable

--- src/helpers/sync/SyncTimeline.hpp
+++ src/helpers/sync/SyncTimeline.hpp
@@ -20,21 +20,12 @@ class CSyncTimeline {
     static SP<CSyncTimeline> create(int drmFD_, Hyprutils::OS::CFileDescriptor&& drmSyncobjFD);
     ~CSyncTimeline();
 
-    struct SWaiter {
-        std::function<void()>          fn;
-        wl_event_source*               source = nullptr;
-        WP<CSyncTimeline>              timeline;
-        Hyprutils::OS::CFileDescriptor eventFd;
-    };
-
     // check if the timeline point has been signaled
     // flags: DRM_SYNCOBJ_WAIT_FLAGS_WAIT_FOR_SUBMIT or DRM_SYNCOBJ_WAIT_FLAGS_WAIT_AVAILABLE
     // std::nullopt on fail
     std::optional<bool>            check(uint64_t point, uint32_t flags);
 
     bool                           addWaiter(const std::function<void()>& waiter, uint64_t point, uint32_t flags);
-    void                           removeWaiter(SWaiter*);
-    void                           removeAllWaiters();
     Hyprutils::OS::CFileDescriptor exportAsSyncFileFD(uint64_t src);
     bool                           importFromSyncFileFD(uint64_t dst, Hyprutils::OS::CFileDescriptor& fd);
     bool                           transfer(SP<CSyncTimeline> from, uint64_t fromPoint, uint64_t toPoint);
@@ -47,6 +38,4 @@ class CSyncTimeline {
 
   private:
     CSyncTimeline() = default;
-
-    std::vector<SP<SWaiter>> waiters;
 };

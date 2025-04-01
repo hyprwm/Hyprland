f95d4652e5e3d9b9391651fc69aca9fe4513ac4e
UjinT34 <ujint34@mail.ru>
2025-04-01 15:42:14 +0300

    store a map of uniq image descriptions

--- src/protocols/types/ColorManagement.cpp
+++ src/protocols/types/ColorManagement.cpp
@@ -1,7 +1,11 @@
 #include "ColorManagement.hpp"
+#include <map>
 
 namespace NColorManagement {
-    const SPCPRimaries& getPrimaries(ePrimaries name) {
+    static uint32_t                              lastImageID = 0;
+    static std::map<SImageDescription, uint32_t> knownDescriptionIds; // expected to be small1
+
+    const SPCPRimaries&                          getPrimaries(ePrimaries name) {
         switch (name) {
             case CM_PRIMARIES_SRGB: return NColorPrimaries::BT709;
             case CM_PRIMARIES_BT2020: return NColorPrimaries::BT2020;
@@ -17,4 +21,23 @@ namespace NColorManagement {
         }
     }
 
+    uint32_t SImageDescription::findId() const {
+        const auto known = knownDescriptionIds.find(*this);
+        if (known != knownDescriptionIds.end())
+            return known->second;
+        else {
+            const auto newId = ++lastImageID;
+            knownDescriptionIds.insert(std::make_pair(*this, newId));
+            return newId;
+        }
+    }
+
+    uint32_t SImageDescription::getId() const {
+        return id > 0 ? id : findId();
+    }
+
+    uint32_t SImageDescription::updateId() {
+        id = findId();
+        return id;
+    }
 }

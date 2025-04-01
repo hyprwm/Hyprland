5e03d3535b593c67c089296b0276c7bd6b33bcb9
X2E4VXpZKv <92685821+X2E4VXpZKv@users.noreply.github.com>
2025-04-01 16:20:38 +0200

    internal: Don't force default cursor on config reload/monitor reconfigure (#9815)

--- src/managers/CursorManager.cpp
+++ src/managers/CursorManager.cpp
@@ -302,8 +302,6 @@ void CCursorManager::updateTheme() {
             m_pHyprcursor->loadThemeStyle(m_sCurrentStyleInfo);
     }
 
-    setCursorFromName("left_ptr");
-
     for (auto const& m : g_pCompositor->m_vMonitors) {
         m->forceFullFrames = 5;
         g_pCompositor->scheduleFrameForMonitor(m, Aquamarine::IOutput::AQ_SCHEDULE_CURSOR_SHAPE);

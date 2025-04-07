767d93bcc2aeb3eb2f36b41150f61b39978f1e63
Ikalco <73481042+ikalco@users.noreply.github.com>
2025-04-06 21:30:22 -0500

    wait on dmabuf buffers to be readable

--- src/protocols/types/DMABuffer.cpp
+++ src/protocols/types/DMABuffer.cpp
@@ -4,6 +4,14 @@
 #include "../../render/Renderer.hpp"
 #include "../../helpers/Format.hpp"
 
+#if defined(__linux__)
+#include <linux/dma-buf.h>
+#include <linux/sync_file.h>
+#include <sys/ioctl.h>
+#endif
+
+using namespace Hyprutils::OS;
+
 CDMABuffer::CDMABuffer(uint32_t id, wl_client* client, Aquamarine::SDMABUFAttrs const& attrs_) : attrs(attrs_) {
     g_pHyprRenderer->makeEGLCurrent();
 
@@ -83,4 +91,63 @@ void CDMABuffer::closeFDs() {
         attrs.fds[i] = -1;
     }
     attrs.planes = 0;
+}
+
+static int doIoctl(int fd, unsigned long request, void* arg) {
+    int ret;
+
+    do {
+        ret = ioctl(fd, request, arg);
+    } while (ret == -1 && (errno == EINTR || errno == EAGAIN));
+    return ret;
+}
+
+// https://www.kernel.org/doc/html/latest/driver-api/dma-buf.html#c.dma_buf_export_sync_file
+// returns a sync file that will be signalled when dmabuf is ready to be read
+CFileDescriptor CDMABuffer::exportSyncFile() {
+    if (!good())
+        return {};
+
+#if !defined(__linux__)
+    return {};
+#else
+    std::vector<CFileDescriptor> syncFds(attrs.fds.size());
+    for (const auto& fd : attrs.fds) {
+        if (fd == -1)
+            continue;
+
+        dma_buf_export_sync_file request{
+            .flags = DMA_BUF_SYNC_READ,
+            .fd    = -1,
+        };
+
+        if (doIoctl(fd, DMA_BUF_IOCTL_EXPORT_SYNC_FILE, &request) == 0)
+            syncFds.emplace_back(request.fd);
+    }
+
+    if (syncFds.empty())
+        return {};
+
+    CFileDescriptor syncFd;
+    for (auto& fd : syncFds) {
+        if (!syncFd.isValid()) {
+            syncFd = std::move(fd);
+            continue;
+        }
+
+        struct sync_merge_data data{
+            .name  = "merged release fence",
+            .fd2   = fd.get(),
+            .fence = -1,
+        };
+
+        if (doIoctl(syncFd.get(), SYNC_IOC_MERGE, &data) == 0) {
+            syncFd = CFileDescriptor(data.fence);
+        } else {
+            syncFd = {};
+        }
+    }
+
+    return syncFd;
+#endif
 }

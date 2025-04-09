60da7f17946a5470bcb92e85e634a76aafc8b980
Ikalco <73481042+ikalco@users.noreply.github.com>
2025-04-09 18:52:37 -0500

    allow tearing while direct scanout happens

--- src/helpers/Monitor.hpp
+++ src/helpers/Monitor.hpp
@@ -198,6 +198,7 @@ class CMonitor {
     WORKSPACEID                         activeSpecialWorkspaceID();
     CBox                                logicalBox();
     void                                scheduleDone();
+    void                                shouldDoTearing();
     bool                                attemptDirectScanout();
     void                                setCTM(const Mat3x3& ctm);
     void                                onCursorMovedOnMonitor();

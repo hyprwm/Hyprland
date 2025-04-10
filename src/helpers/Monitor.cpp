e4c8b070f8be8bce1c33f4ec96c3c683ae24db60
Nathan Ollerenshaw <chrome@stupendous.net>
2025-04-09 17:48:13 -0700

    set noFocus to false on restore

--- src/helpers/Monitor.cpp
+++ src/helpers/Monitor.cpp
@@ -252,7 +252,7 @@ void CMonitor::onConnect(bool noRule) {
         auto ws = g_pCompositor->getWorkspaceByID(workspaceID);
         if (ws) {
             g_pCompositor->moveWorkspaceToMonitor(ws, THISMONITOR);
-            this->changeWorkspace(ws, true, true, true);
+            this->changeWorkspace(ws, true, true, false);
         }
     } else
         Debug::log(LOG, "Monitor {} was not on any workspace", szName);

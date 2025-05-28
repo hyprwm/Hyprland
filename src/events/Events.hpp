7629b0607b056a427f7d99fc647acdfbbb800a76
jsst <105111211+jisstro@users.noreply.github.com>
2025-05-28 21:08:15 +0800

    deleted declaration in Events.hpp

--- src/events/Events.hpp
+++ src/events/Events.hpp
@@ -4,7 +4,6 @@
 // NOLINTNEXTLINE(readability-identifier-naming)
 namespace Events {
     // Window events
-    DYNLISTENFUNC(initializeWindow);
     DYNLISTENFUNC(commitWindow);
     DYNLISTENFUNC(mapWindow);
     DYNLISTENFUNC(unmapWindow);

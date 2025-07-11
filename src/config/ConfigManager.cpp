d7c0408f42aa67c959a1014d0af48f84eba04c3e
Tom Englund <tomenglund26@gmail.com>
2025-07-11 14:30:37 +0200

    config: remove render_ahead* config options
    remove render_ahead* config options and descriptions. they are unusued.

--- src/config/ConfigManager.cpp
+++ src/config/ConfigManager.cpp
@@ -483,8 +483,6 @@ CConfigManager::CConfigManager() {
     registerConfigVar("misc:swallow_exception_regex", {STRVAL_EMPTY});
     registerConfigVar("misc:focus_on_activate", Hyprlang::INT{0});
     registerConfigVar("misc:mouse_move_focuses_monitor", Hyprlang::INT{1});
-    registerConfigVar("misc:render_ahead_of_time", Hyprlang::INT{0});
-    registerConfigVar("misc:render_ahead_safezone", Hyprlang::INT{1});
     registerConfigVar("misc:allow_session_lock_restore", Hyprlang::INT{0});
     registerConfigVar("misc:close_special_on_empty", Hyprlang::INT{1});
     registerConfigVar("misc:background_color", Hyprlang::INT{0xff111111});

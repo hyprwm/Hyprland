2b4a2de1613426ff6a709177acc670f8da10464f
boundlessvoid <boundlessvoid0@gmail.com>
2025-07-15 19:52:16 +0200

    tester: format code and remove unused macros
    Signed-off-by: boundlessvoid <boundlessvoid0@gmail.com>

--- hyprtester/src/tests/main/animations.cpp
+++ hyprtester/src/tests/main/animations.cpp
@@ -10,15 +10,12 @@ static int ret = 0;
 using namespace Hyprutils::OS;
 using namespace Hyprutils::Memory;
 
-#define UP CUniquePointer
-#define SP CSharedPointer
-
 static bool test() {
     NLog::log("{}Testing animations", Colors::GREEN);
 
     auto str = getFromSocket("/animations");
     NLog::log("{}Testing bezier curve output from `hyprctl animations`", Colors::YELLOW);
-    { EXPECT_CONTAINS(str, std::format("beziers:\n\n\tname: quick\n\t\tX0: 0.15\n\t\tY0: 0.00\n\t\tX1: 0.10\n\t\tY1: 1.00")) }
+    {EXPECT_CONTAINS(str, std::format("beziers:\n\n\tname: quick\n\t\tX0: 0.15\n\t\tY0: 0.00\n\t\tX1: 0.10\n\t\tY1: 1.00"))};
     return !ret;
 }
 

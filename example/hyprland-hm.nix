38d77338d6bf08dca1df98e3cc12daa72c5673eb
Manuel Transfeld <auipga@users.noreply.github.com>
2025-07-19 16:13:11 +0200

    fix 'col.

--- example/hyprland-hm.nix
+++ example/hyprland-hm.nix
@@ -96,10 +96,8 @@
 
       border_size = 2;
 
-      col = {
-        active_border = "rgba(33ccffee) rgba(00ff99ee) 45deg";
-        inactive_border = "rgba(595959aa)";
-      };
+      "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+      "col.inactive_border" = "rgba(595959aa)";
 
       # Set to true enable resizing windows by clicking and dragging on borders and gaps
       resize_on_border = false;

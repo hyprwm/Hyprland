48627cb20971eba295ab7e3ce484b8e09d38b894
Florian "sp1rit"​ <sp1rit@disroot.org>
2025-09-11 16:26:24 +0200

    render/OpenGL: fix compilation for 32bit systems

--- src/render/OpenGL.cpp
+++ src/render/OpenGL.cpp
@@ -2876,7 +2876,7 @@ void CHyprOpenGLImpl::ensureBackgroundTexturePresence() {
     static auto PNOWALLPAPER    = CConfigValue<Hyprlang::INT>("misc:disable_hyprland_logo");
     static auto PFORCEWALLPAPER = CConfigValue<Hyprlang::INT>("misc:force_default_wallpaper");
 
-    const auto  FORCEWALLPAPER = std::clamp(*PFORCEWALLPAPER, -1L, 2L);
+    const auto  FORCEWALLPAPER = std::clamp(*PFORCEWALLPAPER, sc<int64_t>(-1), sc<int64_t>(2));
 
     if (*PNOWALLPAPER)
         m_backgroundTexture.reset();

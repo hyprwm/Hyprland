a76366e5312855d22183e23dd743b3132e9fda92
Ikalco <ikalco@ikalco.com>
2025-10-04 12:09:10 -0500

    add screenshare manager
    keep track if a shared window's monitor changed
    
    fix crash for managed sessions
    
    fix no screen share
    
    fix window share on transformed monitors
    
    move away from m_box and to m_bufferSize with renderMonitor handling the region capture box
    
    misc fixes
    
    fix region share

---
+++ src/managers/screenshare/ScreenshareManager.cpp
@@ -0,0 +1,189 @@
+#include "ScreenshareManager.hpp"
+#include "../../render/Renderer.hpp"
+#include "../../Compositor.hpp"
+#include "../../desktop/view/Window.hpp"
+#include "../HookSystemManager.hpp"
+#include "../EventManager.hpp"
+#include "../eventLoop/EventLoopManager.hpp"
+
+CScreenshareManager::CScreenshareManager() {
+    m_tickTimer = makeShared<CEventLoopTimer>(std::chrono::microseconds(500), [this](SP<CEventLoopTimer> self, void* data) { onTick(); }, nullptr);
+    if (g_pEventLoopManager) // null in --verify-config mode
+        g_pEventLoopManager->addTimer(m_tickTimer);
+}
+
+void CScreenshareManager::onOutputCommit(PHLMONITOR monitor) {
+    std::erase_if(m_sessions, [&](const WP<CScreenshareSession>& session) { return session.expired(); });
+
+    if (m_frames.empty()) {
+        for (const auto& session : m_sessions) {
+            if (session->m_framesInLastHalfSecond > 0)
+                return;
+        }
+
+        g_pHyprRenderer->m_directScanoutBlocked = false;
+        return; // nothing to share
+    }
+
+    std::ranges::for_each(m_frames, [&](WP<CScreenshareFrame>& frame) {
+        if (frame.expired() || !frame->m_shared || frame->done())
+            return;
+
+        if (frame->m_session->monitor() != monitor)
+            return;
+
+        if (frame->m_session->m_type == SHARE_WINDOW) {
+            CBox geometry = {frame->m_session->m_window->m_realPosition->value(), frame->m_session->m_window->m_realSize->value()};
+            if (geometry.intersection({monitor->m_position, monitor->m_size}).empty())
+                return;
+        }
+
+        frame->copy();
+        frame->m_session->m_lastFrame.reset();
+        frame->m_session->m_frameCounter++;
+    });
+
+    std::erase_if(m_frames, [&](const WP<CScreenshareFrame>& frame) { return frame.expired(); });
+}
+
+void CScreenshareManager::onTick() {
+    m_tickTimer->updateTimeout(std::chrono::microseconds(500));
+
+    std::ranges::for_each(m_managedSessions, [this](auto& session) {
+        if (session->m_lastMeasure.getMillis() < 500)
+            return;
+
+        session->m_session->m_framesInLastHalfSecond = session->m_session->m_frameCounter;
+        session->m_session->m_frameCounter           = 0;
+        session->m_lastMeasure.reset();
+
+        const auto LASTFRAMEDELTA = session->m_session->m_lastFrame.getMillis() / 1000.0;
+        const bool FRAMEAWAITING = std::ranges::any_of(m_frames, [&](const auto& frame) { return !frame.expired() && frame->m_session->m_client == session->m_session->m_client; });
+
+        if (session->m_session->m_framesInLastHalfSecond > 3 && !session->m_sentScreencast) {
+            screenshareEvents(session->m_session, true);
+            session->m_sentScreencast = true;
+        } else if (session->m_session->m_framesInLastHalfSecond < 4 && session->m_sentScreencast && LASTFRAMEDELTA > 1.0 && !FRAMEAWAITING) {
+            screenshareEvents(session->m_session, false);
+            session->m_sentScreencast = false;
+        }
+    });
+}
+
+void CScreenshareManager::screenshareEvents(WP<CScreenshareSession> session, bool started) {
+    if (session.expired()) {
+        LOGM(Log::ERR, "screenshareEvents: FAILED");
+        return;
+    }
+
+    if (started) {
+        EMIT_HOOK_EVENT("screencastv2", (std::vector<std::any>{1, session->m_type, session->m_name}));
+        g_pEventManager->postEvent(SHyprIPCEvent{.event = "screencastv2", .data = std::format("1,{},{}", session->m_type, session->m_name)});
+        LOGM(Log::INFO, "New screenshare session for ({}): {}", session->m_type, session->m_name);
+    } else {
+        EMIT_HOOK_EVENT("screencastv2", (std::vector<std::any>{0, session->m_type, session->m_name}));
+        g_pEventManager->postEvent(SHyprIPCEvent{.event = "screencastv2", .data = std::format("0,{},{}", session->m_type, session->m_name)});
+        LOGM(Log::INFO, "Stopped screenshare session for ({}): {}", session->m_type, session->m_name);
+    }
+}
+
+UP<CScreenshareSession> CScreenshareManager::newSession(wl_client* client, PHLMONITOR monitor) {
+    if UNLIKELY (!monitor || !g_pCompositor->monitorExists(monitor)) {
+        LOGM(Log::ERR, "Client requested sharing of a monitor that is gone");
+        return nullptr;
+    }
+
+    UP<CScreenshareSession> session = UP<CScreenshareSession>(new CScreenshareSession(monitor, client, false));
+
+    session->m_self = session;
+    m_sessions.emplace_back(session);
+
+    return session;
+}
+
+UP<CScreenshareSession> CScreenshareManager::newSession(wl_client* client, PHLMONITOR monitor, CBox captureRegion) {
+    if UNLIKELY (!monitor || !g_pCompositor->monitorExists(monitor)) {
+        LOGM(Log::ERR, "Client requested sharing of a monitor that is gone");
+        return nullptr;
+    }
+
+    UP<CScreenshareSession> session = UP<CScreenshareSession>(new CScreenshareSession(monitor, captureRegion, client, false));
+
+    session->m_self = session;
+    m_sessions.emplace_back(session);
+
+    return session;
+}
+
+UP<CScreenshareSession> CScreenshareManager::newSession(wl_client* client, PHLWINDOW window) {
+    if UNLIKELY (!window || !window->m_isMapped) {
+        LOGM(Log::ERR, "Client requested sharing of window that is gone or not shareable!");
+        return nullptr;
+    }
+
+    UP<CScreenshareSession> session = UP<CScreenshareSession>(new CScreenshareSession(window, client, false));
+
+    session->m_self = session;
+    m_sessions.emplace_back(session);
+
+    return session;
+}
+
+WP<CScreenshareSession> CScreenshareManager::getManagedSession(wl_client* client, PHLMONITOR monitor) {
+    return getManagedSession(SHARE_MONITOR, client, monitor, nullptr, {});
+}
+
+WP<CScreenshareSession> CScreenshareManager::getManagedSession(wl_client* client, PHLMONITOR monitor, CBox captureBox) {
+
+    return getManagedSession(SHARE_REGION, client, monitor, nullptr, captureBox);
+}
+
+WP<CScreenshareSession> CScreenshareManager::getManagedSession(wl_client* client, PHLWINDOW window) {
+    return getManagedSession(SHARE_WINDOW, client, nullptr, window, {});
+}
+
+WP<CScreenshareSession> CScreenshareManager::getManagedSession(eScreenshareType type, wl_client* client, PHLMONITOR monitor, PHLWINDOW window, CBox captureBox) {
+    auto it = std::ranges::find_if(m_managedSessions, [&](const auto& session) {
+        if (session->m_session->m_client != client || session->m_session->m_type != type)
+            return false;
+
+        switch (type) {
+            case SHARE_MONITOR: return session->m_session->m_monitor == monitor;
+            case SHARE_WINDOW: return session->m_session->m_window == window;
+            case SHARE_REGION: return session->m_session->m_monitor == monitor && session->m_session->m_captureBox == captureBox;
+        }
+
+        return false;
+    });
+
+    if (it == m_managedSessions.end()) {
+        UP<CScreenshareSession> session;
+        switch (type) {
+            case SHARE_MONITOR: session = UP<CScreenshareSession>(new CScreenshareSession(monitor, client, true)); break;
+            case SHARE_WINDOW: session = UP<CScreenshareSession>(new CScreenshareSession(window, client, true)); break;
+            case SHARE_REGION: session = UP<CScreenshareSession>(new CScreenshareSession(monitor, captureBox, client, true)); break;
+        }
+
+        session->m_self = session;
+        m_sessions.emplace_back(session);
+
+        it = m_managedSessions.emplace(m_managedSessions.end(), makeUnique<SManagedSession>(std::move(session)));
+    }
+
+    auto& session = *it;
+
+    session->stoppedListener = session->m_session->m_events.stopped.listen([session = WP<SManagedSession>(session)]() {
+        std::erase_if(g_pScreenshareManager->m_managedSessions, [&](const auto& s) { return !s || session.expired() || s->m_session == session->m_session; });
+    });
+
+    return session->m_session;
+}
+
+void CScreenshareManager::destroyClientSessions(wl_client* client) {
+    LOGM(Log::TRACE, "Destroy client sessions for {:x}", (uintptr_t)client);
+    std::erase_if(m_managedSessions, [&](const auto& session) { return !session || session->m_session->m_client == client; });
+}
+
+CScreenshareManager::SManagedSession::SManagedSession(UP<CScreenshareSession> session) : m_session(std::move(session)) {
+    m_lastMeasure.reset();
+}
---
+++ src/managers/screenshare/ScreenshareManager.hpp
@@ -0,0 +1,191 @@
+#pragma once
+
+#include <vector>
+#include "../../helpers/memory/Memory.hpp"
+#include "../../protocols/types/Buffer.hpp"
+#include "../../render/Framebuffer.hpp"
+#include "../../helpers/time/Timer.hpp"
+#include "../eventLoop/EventLoopTimer.hpp"
+
+// TODO: integrate into screencopy, toplevel export, and image copy capture
+// TODO: do screenshare damage
+
+enum eScreenshareType : uint8_t {
+    SHARE_MONITOR,
+    SHARE_WINDOW,
+    SHARE_REGION
+};
+
+enum eScreenshareError : uint8_t {
+    ERROR_NONE,
+    ERROR_UNKNOWN,
+    ERROR_STOPPED,
+    ERROR_NO_BUFFER,
+    ERROR_BUFFER_SIZE,
+    ERROR_BUFFER_FORMAT
+};
+
+enum eScreenshareResult : uint8_t {
+    RESULT_COPIED,
+    RESULT_NOT_COPIED,
+    RESULT_TIMESTAMP,
+};
+
+template <>
+struct std::formatter<eScreenshareType> : std::formatter<std::string> {
+    auto format(const eScreenshareType& res, std::format_context& ctx) const {
+        switch (res) {
+            case SHARE_MONITOR: return formatter<string>::format("monitor", ctx);
+            case SHARE_WINDOW: return formatter<string>::format("window", ctx);
+            case SHARE_REGION: return formatter<string>::format("region", ctx);
+        }
+        return formatter<string>::format("error", ctx);
+    }
+};
+
+using FScreenshareCallback = std::function<void(eScreenshareResult result)>;
+
+class CScreenshareFrame;
+
+class CScreenshareSession {
+  public:
+    CScreenshareSession(const CScreenshareSession&) = delete;
+    CScreenshareSession(CScreenshareSession&&)      = delete;
+    ~CScreenshareSession();
+
+    UP<CScreenshareFrame> nextFrame(bool overlayCursor);
+    void                  stop();
+
+    // constraints
+    const std::vector<DRMFormat>& allowedFormats() const;
+    Vector2D                      bufferSize() const;
+    PHLMONITOR                    monitor() const; // this will return the correct monitor based on type
+
+    struct {
+        CSignalT<> stopped;
+        CSignalT<> constraintsChanged;
+    } m_events;
+
+  private:
+    CScreenshareSession(PHLMONITOR monitor, wl_client* client, bool managed);
+    CScreenshareSession(PHLMONITOR monitor, CBox captureRegion, wl_client* client, bool managed);
+    CScreenshareSession(PHLWINDOW window, wl_client* client, bool managed);
+
+    WP<CScreenshareSession> m_self;
+    bool                    m_stopped = false;
+    bool                    m_managed = false;
+
+    eScreenshareType        m_type;
+    PHLMONITORREF           m_monitor;
+    PHLWINDOWREF            m_window;
+    CBox                    m_captureBox; // given capture area in logical coordinates (see xdg_output)
+
+    wl_client*              m_client;
+    std::string             m_name;
+
+    std::vector<DRMFormat>  m_formats;
+    Vector2D                m_bufferSize;
+
+    CFramebuffer            m_tempFB;
+
+    CTimer                  m_lastFrame;
+    int                     m_frameCounter           = 0;
+    int                     m_framesInLastHalfSecond = 0;
+
+    struct {
+        CHyprSignalListener monitorDestroyed;
+        CHyprSignalListener monitorModeChanged;
+        CHyprSignalListener windowDestroyed;
+        CHyprSignalListener windowSizeChanged;
+        CHyprSignalListener windowMonitorChanged;
+    } m_listeners;
+
+    void calculateConstraints();
+    void init();
+
+    friend class CScreenshareFrame;
+    friend class CScreenshareManager;
+};
+
+class CScreenshareFrame {
+  public:
+    CScreenshareFrame(const CScreenshareFrame&) = delete;
+    CScreenshareFrame(CScreenshareFrame&&)      = delete;
+    CScreenshareFrame(WP<CScreenshareSession> session, bool overlayCursor, bool isFirst);
+    ~CScreenshareFrame();
+
+    bool                done() const;
+    eScreenshareError   share(SP<IHLBuffer> buffer, const CRegion& damage, FScreenshareCallback callback);
+
+    Vector2D            bufferSize() const;
+    wl_output_transform transform() const; // returns the transform applied by compositor on the buffer
+    const CRegion&      damage() const;
+
+  private:
+    WP<CScreenshareFrame>   m_self;
+    WP<CScreenshareSession> m_session;
+    FScreenshareCallback    m_callback;
+    SP<IHLBuffer>           m_buffer;
+    Vector2D                m_bufferSize;
+    CRegion                 m_damage; // damage in buffer coords
+    bool                    m_shared = false, m_copied = false, m_failed = false;
+    bool                    m_overlayCursor;
+    bool                    m_isFirst = false;
+
+    //
+    void copy();
+    bool copyDmabuf();
+    bool copyShm();
+
+    void render();
+    void renderMonitor();
+    void renderMonitorRegion();
+    void renderWindow();
+
+    void storeTempFB();
+
+    friend class CScreenshareManager;
+    friend class CScreenshareSession;
+};
+
+class CScreenshareManager {
+  public:
+    CScreenshareManager();
+
+    UP<CScreenshareSession> newSession(wl_client* client, PHLMONITOR monitor);
+    UP<CScreenshareSession> newSession(wl_client* client, PHLMONITOR monitor, CBox captureRegion);
+    UP<CScreenshareSession> newSession(wl_client* client, PHLWINDOW window);
+
+    WP<CScreenshareSession> getManagedSession(wl_client* client, PHLMONITOR monitor);
+    WP<CScreenshareSession> getManagedSession(wl_client* client, PHLMONITOR monitor, CBox captureBox);
+    WP<CScreenshareSession> getManagedSession(wl_client* client, PHLWINDOW window);
+
+    void                    destroyClientSessions(wl_client* client);
+
+    void                    onOutputCommit(PHLMONITOR monitor);
+
+  private:
+    std::vector<WP<CScreenshareSession>> m_sessions;
+    std::vector<WP<CScreenshareFrame>>   m_frames;
+
+    struct SManagedSession {
+        SManagedSession(UP<CScreenshareSession> session);
+
+        UP<CScreenshareSession> m_session;
+        CHyprSignalListener     stoppedListener;
+        CTimer                  m_lastMeasure;
+        bool                    m_sentScreencast = false;
+    };
+
+    std::vector<UP<SManagedSession>> m_managedSessions;
+    WP<CScreenshareSession>          getManagedSession(eScreenshareType type, wl_client* client, PHLMONITOR monitor, PHLWINDOW window, CBox captureBox);
+
+    SP<CEventLoopTimer>              m_tickTimer;
+    void                             onTick();
+
+    void                             screenshareEvents(WP<CScreenshareSession> session, bool started);
+
+    friend class CScreenshareSession;
+};
+
+inline UP<CScreenshareManager> g_pScreenshareManager;

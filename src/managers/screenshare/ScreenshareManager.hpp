9f6abae1db37378eefe026ed8cea17bd1ad501b6
Ikalco <ikalco@ikalco.com>
2025-11-10 20:19:42 -0600

    implement image copy capture cursor session
    squashed from
    * implement 1st half of image copy capture cursor session
    * add cursor pos permission for image copy capture cursor session
    * add cursor share session
    * implement cursor copy capture session

--- src/managers/screenshare/ScreenshareManager.hpp
+++ src/managers/screenshare/ScreenshareManager.hpp
@@ -45,6 +45,7 @@ struct std::formatter<eScreenshareType> : std::formatter<std::string> {
 using FScreenshareCallback = std::function<void(eScreenshareResult result)>;
 
 class CScreenshareFrame;
+class CWLPointerResource;
 
 class CScreenshareSession {
   public:
@@ -105,6 +106,51 @@ class CScreenshareSession {
     friend class CScreenshareManager;
 };
 
+class CCursorshareSession {
+  public:
+    CCursorshareSession(const CCursorshareSession&) = delete;
+    CCursorshareSession(CCursorshareSession&&)      = delete;
+    ~CCursorshareSession();
+
+    eScreenshareError share(PHLMONITOR monitor, SP<IHLBuffer> buffer, FScreenshareCallback callback);
+    void              stop();
+
+    // constraints
+    DRMFormat format() const;
+    Vector2D  bufferSize() const;
+    Vector2D  hotspot() const;
+
+    struct {
+        CSignalT<> stopped;
+        CSignalT<> constraintsChanged;
+    } m_events;
+
+  private:
+    CCursorshareSession(wl_client* client, WP<CWLPointerResource> pointer);
+
+    WP<CCursorshareSession> m_self;
+    bool                    m_stopped = false;
+
+    wl_client*              m_client;
+    WP<CWLPointerResource>  m_pointer;
+
+    // constraints
+    DRMFormat m_format;
+    Vector2D  m_hotspot;
+    Vector2D  m_bufferSize;
+
+    struct {
+        CHyprSignalListener pointerDestroyed;
+        CHyprSignalListener cursorChanged;
+    } m_listeners;
+
+    bool copy(PHLMONITOR monitor, SP<IHLBuffer> buffer, FScreenshareCallback callback);
+    void calculateConstraints();
+
+    friend class CScreenshareFrame;
+    friend class CScreenshareManager;
+};
+
 class CScreenshareFrame {
   public:
     CScreenshareFrame(const CScreenshareFrame&) = delete;
@@ -158,12 +204,15 @@ class CScreenshareManager {
     WP<CScreenshareSession> getManagedSession(wl_client* client, PHLMONITOR monitor, CBox captureBox);
     WP<CScreenshareSession> getManagedSession(wl_client* client, PHLWINDOW window);
 
+    UP<CCursorshareSession> newCursorSession(wl_client* client, WP<CWLPointerResource> pointer);
+
     void                    destroyClientSessions(wl_client* client);
 
     void                    onOutputCommit(PHLMONITOR monitor);
 
   private:
     std::vector<WP<CScreenshareSession>> m_sessions;
+    std::vector<WP<CCursorshareSession>> m_cursorSessions;
     std::vector<WP<CScreenshareFrame>>   m_frames;
 
     struct SManagedSession {

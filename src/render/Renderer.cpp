f35f5b24b5ec4b9a4670f62a996b099e6ae92c7b
Tom Englund <tomenglund26@gmail.com>
2025-11-13 04:25:12 +0100

    renderer: make new_render_scheduler not drop buffers wrong
    make new_render_scheduler not drop buffers to early when third frame is
    pending, by adding a parameter to commitPendingAndDoExplicitSync when to
    drop buffers. also reuse monitor inFence to reduce flushing.

--- src/render/Renderer.cpp
+++ src/render/Renderer.cpp
@@ -1258,7 +1258,7 @@ void CHyprRenderer::calculateUVForSurface(PHLWINDOW pWindow, SP<CWLSurfaceResour
     }
 }
 
-void CHyprRenderer::renderMonitor(PHLMONITOR pMonitor, bool commit) {
+void CHyprRenderer::renderMonitor(PHLMONITOR pMonitor, bool commit, bool dropBuffers) {
     static std::chrono::high_resolution_clock::time_point renderStart        = std::chrono::high_resolution_clock::now();
     static std::chrono::high_resolution_clock::time_point renderStartOverlay = std::chrono::high_resolution_clock::now();
     static std::chrono::high_resolution_clock::time_point endRenderOverlay   = std::chrono::high_resolution_clock::now();
@@ -1486,7 +1486,7 @@ void CHyprRenderer::renderMonitor(PHLMONITOR pMonitor, bool commit) {
                                                                 Aquamarine::eOutputPresentationMode::AQ_OUTPUT_PRESENTATION_VSYNC);
 
     if (commit)
-        commitPendingAndDoExplicitSync(pMonitor);
+        commitPendingAndDoExplicitSync(pMonitor, dropBuffers);
 
     if (shouldTear)
         pMonitor->m_tearingState.busy = true;
@@ -1553,7 +1553,7 @@ static hdr_output_metadata       createHDRMetadata(SImageDescription settings, S
     };
 }
 
-bool CHyprRenderer::commitPendingAndDoExplicitSync(PHLMONITOR pMonitor) {
+bool CHyprRenderer::commitPendingAndDoExplicitSync(PHLMONITOR pMonitor, bool dropBuffers) {
     static auto PCT        = CConfigValue<Hyprlang::INT>("render:send_content_type");
     static auto PPASS      = CConfigValue<Hyprlang::INT>("render:cm_fs_passthrough");
     static auto PAUTOHDR   = CConfigValue<Hyprlang::INT>("render:cm_auto_hdr");
@@ -1691,7 +1691,8 @@ bool CHyprRenderer::commitPendingAndDoExplicitSync(PHLMONITOR pMonitor) {
         }
     }
 
-    g_pBufferReleaseManager->dropBuffers(pMonitor);
+    if (dropBuffers)
+        g_pBufferReleaseManager->dropBuffers(pMonitor);
 
     bool ok = pMonitor->m_state.commit();
     if (!ok) {

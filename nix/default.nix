305f770bb8b1b7d6896edf02ac16ef89aaa11c68
Mihai Fufezan <mihai@fufexan.net>
2025-11-15 23:16:26 +0200

    nix: add muparser dep

--- nix/default.nix
+++ nix/default.nix
@@ -26,6 +26,7 @@
   libxkbcommon,
   libuuid,
   libgbm,
+  muparser,
   pango,
   pciutils,
   re2,
@@ -148,6 +149,7 @@ in
           libuuid
           libxkbcommon
           libgbm
+          muparser
           pango
           pciutils
           re2

303c5eaf8838144ac7c30ec83d6b442c525f0020
Katherine Raye <alicelegare@outlook.com>
2025-11-30 20:27:38 -0500

    forgot to format

--- src/render/Renderer.hpp
+++ src/render/Renderer.hpp
@@ -155,7 +155,7 @@ class CHyprRenderer {
 
     struct {
         bool hiddenOnTouch    = false;
-        bool hiddenOnPen   = false;
+        bool hiddenOnPen      = false;
         bool hiddenOnTimeout  = false;
         bool hiddenOnKeyboard = false;
     } m_cursorHiddenConditions;

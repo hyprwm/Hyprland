e94934402c2e5cabe21ce6e7e3d353f3b877a032
Vaxry <vaxry@vaxry.net>
2025-12-18 15:53:50 +0000

    work

---
+++ src/desktop/history/WindowHistoryTracker.cpp
@@ -0,0 +1,43 @@
+#include "WindowHistoryTracker.hpp"
+
+#include "../../managers/HookSystemManager.hpp"
+#include "../view/Window.hpp"
+
+using namespace Desktop;
+using namespace Desktop::History;
+
+CWindowHistoryTracker::CWindowHistoryTracker() {
+    static auto P2 = g_pHookSystem->hookDynamic("activeWindow", [this](void* self, SCallbackInfo& info, std::any data) {
+        auto window = std::any_cast<PHLWINDOW>(data);
+
+        track(window);
+    });
+}
+
+void CWindowHistoryTracker::track(PHLWINDOW w) {
+    std::erase(m_history, w);
+    m_history.emplace_back(w);
+}
+
+const std::vector<PHLWINDOWREF>& CWindowHistoryTracker::fullHistory() {
+    gc();
+    return m_history;
+}
+
+std::vector<PHLWINDOWREF> CWindowHistoryTracker::historyForWorkspace(PHLWORKSPACE ws) {
+    gc();
+    std::vector<PHLWINDOWREF> windows;
+
+    for (const auto& w : m_history) {
+        if (w->m_workspace != ws)
+            continue;
+
+        windows.emplace_back(w);
+    }
+
+    return windows;
+}
+
+void CWindowHistoryTracker::gc() {
+    std::erase_if(m_history, [](const auto& e) { return !e; });
+}

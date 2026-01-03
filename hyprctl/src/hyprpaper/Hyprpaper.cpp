1912ba87e9a491e72c8ef735dcbaf473c6e1ba93
Vaxry <vaxry@vaxry.net>
2026-01-03 21:48:19 +0100

    hyprctl: bump hyprpaper protocol to rev 2

--- hyprctl/src/hyprpaper/Hyprpaper.cpp
+++ hyprctl/src/hyprpaper/Hyprpaper.cpp
@@ -4,6 +4,7 @@
 #include <optional>
 #include <format>
 #include <filesystem>
+#include <print>
 
 #include <hyprpaper_core-client.hpp>
 
@@ -15,7 +16,7 @@ using namespace std::string_literals;
 constexpr const char*          SOCKET_NAME = ".hyprpaper.sock";
 static SP<CCHyprpaperCoreImpl> g_coreImpl;
 
-constexpr const uint32_t       PROTOCOL_VERSION_SUPPORTED = 1;
+constexpr const uint32_t       PROTOCOL_VERSION_SUPPORTED = 2;
 
 //
 static hyprpaperCoreWallpaperFitMode fitFromString(const std::string_view& sv) {
@@ -53,21 +54,7 @@ static std::expected<std::string, std::string> getFullPath(const std::string_vie
     return resolvePath(sv);
 }
 
-std::expected<void, std::string> Hyprpaper::makeHyprpaperRequest(const std::string_view& rq) {
-    if (!rq.contains(' '))
-        return std::unexpected("Invalid request");
-
-    if (!rq.starts_with("/hyprpaper "))
-        return std::unexpected("Invalid request");
-
-    std::string_view LHS, RHS;
-    auto             spacePos = rq.find(' ', 12);
-    LHS                       = rq.substr(11, spacePos - 11);
-    RHS                       = rq.substr(spacePos + 1);
-
-    if (LHS != "wallpaper")
-        return std::unexpected("Unknown hyprpaper request");
-
+static std::expected<void, std::string> doWallpaper(const std::string_view& RHS) {
     CVarList2         args(std::string{RHS}, 0, ',');
 
     const std::string MONITOR  = std::string{args[0]};
@@ -99,7 +86,7 @@ std::expected<void, std::string> Hyprpaper::makeHyprpaperRequest(const std::stri
     if (!socket)
         return std::unexpected("can't send: failed to connect to hyprpaper (is it running?)");
 
-    g_coreImpl = makeShared<CCHyprpaperCoreImpl>(1);
+    g_coreImpl = makeShared<CCHyprpaperCoreImpl>(PROTOCOL_VERSION_SUPPORTED);
 
     socket->addImplementation(g_coreImpl);
 
@@ -111,7 +98,7 @@ std::expected<void, std::string> Hyprpaper::makeHyprpaperRequest(const std::stri
     if (!spec)
         return std::unexpected("can't send: hyprpaper doesn't have the spec?!");
 
-    auto manager = makeShared<CCHyprpaperCoreManagerObject>(socket->bindProtocol(g_coreImpl->protocol(), PROTOCOL_VERSION_SUPPORTED));
+    auto manager = makeShared<CCHyprpaperCoreManagerObject>(socket->bindProtocol(g_coreImpl->protocol(), std::min(PROTOCOL_VERSION_SUPPORTED, spec->specVer())));
 
     if (!manager)
         return std::unexpected("wire error: couldn't create manager");
@@ -147,6 +134,75 @@ std::expected<void, std::string> Hyprpaper::makeHyprpaperRequest(const std::stri
 
     if (err)
         return std::unexpected(*err);
+
+    return {};
+}
+
+static std::expected<void, std::string> doListActive() {
+    const auto RTDIR = getenv("XDG_RUNTIME_DIR");
+
+    if (!RTDIR || RTDIR[0] == '\0')
+        return std::unexpected("can't send: no XDG_RUNTIME_DIR");
+
+    const auto HIS = getenv("HYPRLAND_INSTANCE_SIGNATURE");
+
+    if (!HIS || HIS[0] == '\0')
+        return std::unexpected("can't send: no HYPRLAND_INSTANCE_SIGNATURE (not running under hyprland)");
+
+    auto socketPath = RTDIR + "/hypr/"s + HIS + "/"s + SOCKET_NAME;
+
+    auto socket = Hyprwire::IClientSocket::open(socketPath);
+
+    if (!socket)
+        return std::unexpected("can't send: failed to connect to hyprpaper (is it running?)");
+
+    g_coreImpl = makeShared<CCHyprpaperCoreImpl>(PROTOCOL_VERSION_SUPPORTED);
+
+    socket->addImplementation(g_coreImpl);
+
+    if (!socket->waitForHandshake())
+        return std::unexpected("can't send: wire handshake failed");
+
+    auto spec = socket->getSpec(g_coreImpl->protocol()->specName());
+
+    if (!spec)
+        return std::unexpected("can't send: hyprpaper doesn't have the spec?!");
+
+    if (spec->specVer() < 2)
+        return std::unexpected("can't send: hyprpaper protocol version too low (hyprpaper too old)");
+
+    auto manager = makeShared<CCHyprpaperCoreManagerObject>(socket->bindProtocol(g_coreImpl->protocol(), std::min(PROTOCOL_VERSION_SUPPORTED, spec->specVer())));
+
+    if (!manager)
+        return std::unexpected("wire error: couldn't create manager");
+
+    auto status = makeShared<CCHyprpaperStatusObject>(manager->sendGetStatusObject());
+
+    status->setActiveWallpaper([](const char* mon, const char* wp) { std::println("{}: {}", mon, wp); });
+
+    socket->roundtrip();
+
+    return {};
+}
+
+std::expected<void, std::string> Hyprpaper::makeHyprpaperRequest(const std::string_view& rq) {
+    if (!rq.contains(' '))
+        return std::unexpected("Invalid request");
+
+    if (!rq.starts_with("/hyprpaper "))
+        return std::unexpected("Invalid request");
+
+    std::string_view LHS, RHS;
+    auto             spacePos = rq.find(' ', 12);
+    LHS                       = rq.substr(11, spacePos - 11);
+    RHS                       = rq.substr(spacePos + 1);
+
+    if (LHS == "wallpaper")
+        return doWallpaper(RHS);
+    else if (LHS == "listactive")
+        return doListActive();
+    else
+        return std::unexpected("invalid hyprpaper request");
 
     return {};
 }

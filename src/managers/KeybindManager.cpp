a34f9a025aceeeebfa4238c2d0a50958dd44f277
Gwilherm Folliot <gwilherm55fo@gmail.com>
2026-01-04 15:30:23 +0100

    input-capture: add releaseinputcapture dispatcher

--- src/managers/KeybindManager.cpp
+++ src/managers/KeybindManager.cpp
@@ -157,6 +157,7 @@ CKeybindManager::CKeybindManager() {
     m_dispatchers["global"]                         = global;
     m_dispatchers["setprop"]                        = setProp;
     m_dispatchers["forceidle"]                      = forceIdle;
+	m_dispatchers["releaseinputcapture"]			= releaseInputCapture;
 
     m_scrollTimer.reset();
 

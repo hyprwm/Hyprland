584f0d5cee2f47544dfccd5a5c3ca4070b15ce4b
Tom Englund <tomenglund26@gmail.com>
2026-02-17 17:29:09 +0100

    multigpu: move sameGpu to DRM namespace
    move sameGpu out of MiscFunctions to DRM namespace.

--- src/helpers/MiscFunctions.cpp
+++ src/helpers/MiscFunctions.cpp
@@ -21,7 +21,6 @@
 #include <iomanip>
 #include <sstream>
 #include <fstream>
-#include <xf86drm.h>
 #ifdef HAS_EXECINFO
 #include <execinfo.h>
 #endif
@@ -981,22 +980,4 @@ bool truthy(const std::string& str) {
     std::ranges::transform(cpy, cpy.begin(), ::tolower);
 
     return cpy.starts_with("true") || cpy.starts_with("yes") || cpy.starts_with("on");
-}
-
-bool sameGpu(int fd1, int fd2) {
-    drmDevice* devA = nullptr;
-    drmDevice* devB = nullptr;
-
-    if (drmGetDevice2(fd1, 0, &devA) != 0)
-        return false;
-    if (drmGetDevice2(fd2, 0, &devB) != 0) {
-        drmFreeDevice(&devA);
-        return false;
-    }
-
-    bool same = drmDevicesEqual(devA, devB);
-
-    drmFreeDevice(&devA);
-    drmFreeDevice(&devB);
-    return same;
 }

8239467d64eb64fc66749e6fffe448f100b577d3
Tom Englund <tomenglund26@gmail.com>
2026-02-27 06:58:50 +0100

    fences: move to libdrm functions
    no need to reinvent the wheel, use libdrm sync_accumulate and move the
    helpers to drm namespace.

--- src/helpers/Drm.cpp
+++ src/helpers/Drm.cpp
@@ -1,5 +1,12 @@
 #include <xf86drm.h>
+#include <libsync.h>
+
+#if defined(__linux__)
+#include <linux/dma-buf.h>
+#endif
+
 #include "Drm.hpp"
+using namespace Hyprutils::OS;
 
 bool DRM::sameGpu(int fd1, int fd2) {
     drmDevice* devA = nullptr;
@@ -17,4 +24,48 @@ bool DRM::sameGpu(int fd1, int fd2) {
     drmFreeDevice(&devA);
     drmFreeDevice(&devB);
     return same;
+}
+
+int DRM::doIoctl(int fd, unsigned long request, void* arg) {
+    int ret;
+
+    do {
+        ret = ioctl(fd, request, arg);
+    } while (ret == -1 && (errno == EINTR || errno == EAGAIN));
+    return ret;
+}
+
+// https://www.kernel.org/doc/html/latest/driver-api/dma-buf.html#c.dma_buf_export_sync_file
+// returns a sync file that will be signalled when dmabuf is ready to be read
+CFileDescriptor DRM::exportFence(int fd) {
+#ifndef __linux__
+    return {};
+#endif
+
+#ifdef DMA_BUF_IOCTL_EXPORT_SYNC_FILE
+    if (fd < 0)
+        return {};
+
+    dma_buf_export_sync_file request{
+        .flags = DMA_BUF_SYNC_READ,
+        .fd    = -1,
+    };
+
+    if (doIoctl(fd, DMA_BUF_IOCTL_EXPORT_SYNC_FILE, &request) == 0)
+        return CFileDescriptor{request.fd};
+#endif
+    return {};
+}
+
+CFileDescriptor DRM::mergeFence(int fence1, int fence2) {
+#ifdef __linux__
+    sync_accumulate("merged release fence", &fence1, fence2);
+
+    if (fence2 >= 0)
+        close(fence2);
+
+    return CFileDescriptor{fence1};
+#else
+    return {};
+#endif
 }
--- src/helpers/Drm.hpp
+++ src/helpers/Drm.hpp
@@ -1,5 +1,9 @@
 #pragma once
+#include <hyprutils/os/FileDescriptor.hpp>
 
 namespace DRM {
-    bool sameGpu(int fd1, int fd2);
+    bool                           sameGpu(int fd1, int fd2);
+    int                            doIoctl(int fd, unsigned long request, void* arg);
+    Hyprutils::OS::CFileDescriptor exportFence(int fd);
+    Hyprutils::OS::CFileDescriptor mergeFence(int fence1, int fence2);
 }

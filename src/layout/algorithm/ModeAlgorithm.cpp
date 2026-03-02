75a815fbf28b73f3b9f9b9246ed3eb1fbd9b2a58
Vaxry <43317083+vaxerski@users.noreply.github.com>
2026-03-02 21:10:21 +0000

    algo/dwindle: use focal point correctly for x-ws moves (#13514)

--- src/layout/algorithm/ModeAlgorithm.cpp
+++ src/layout/algorithm/ModeAlgorithm.cpp
@@ -1,5 +1,10 @@
 #include "ModeAlgorithm.hpp"
 
+#include "../space/Space.hpp"
+#include "Algorithm.hpp"
+#include "../../helpers/Monitor.hpp"
+#include "../../desktop/view/Window.hpp"
+
 using namespace Layout;
 
 std::expected<void, std::string> IModeAlgorithm::layoutMsg(const std::string_view& sv) {
@@ -8,4 +13,21 @@ std::expected<void, std::string> IModeAlgorithm::layoutMsg(const std::string_vie
 
 std::optional<Vector2D> IModeAlgorithm::predictSizeForNewTarget() {
     return std::nullopt;
+}
+
+std::optional<Vector2D> IModeAlgorithm::focalPointForDir(SP<ITarget> t, Math::eDirection dir) {
+    Vector2D   focalPoint;
+
+    const auto WINDOWIDEALBB =
+        t->fullscreenMode() != FSMODE_NONE ? m_parent->space()->workspace()->m_monitor->logicalBox() : t->window()->getWindowIdealBoundingBoxIgnoreReserved();
+
+    switch (dir) {
+        case Math::DIRECTION_UP: focalPoint = WINDOWIDEALBB.pos() + Vector2D{WINDOWIDEALBB.size().x / 2.0, -1.0}; break;
+        case Math::DIRECTION_DOWN: focalPoint = WINDOWIDEALBB.pos() + Vector2D{WINDOWIDEALBB.size().x / 2.0, WINDOWIDEALBB.size().y + 1.0}; break;
+        case Math::DIRECTION_LEFT: focalPoint = WINDOWIDEALBB.pos() + Vector2D{-1.0, WINDOWIDEALBB.size().y / 2.0}; break;
+        case Math::DIRECTION_RIGHT: focalPoint = WINDOWIDEALBB.pos() + Vector2D{WINDOWIDEALBB.size().x + 1.0, WINDOWIDEALBB.size().y / 2.0}; break;
+        default: return std::nullopt;
+    }
+
+    return focalPoint;
 }

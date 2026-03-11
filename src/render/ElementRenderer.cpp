4caf431fc13da2ce71266265d5cac1accd9a37d6
UjinT34 <ujint34@mail.ru>
2026-03-11 16:58:42 +0300

    fix clipbox reset

--- src/render/ElementRenderer.cpp
+++ src/render/ElementRenderer.cpp
@@ -406,6 +406,7 @@ void IElementRenderer::drawTex(WP<CTexPassElement> element, const CRegion& damag
         if (useMirrorProjection)
             g_pHyprRenderer->setProjectionType(RPT_MONITOR);
         g_pHyprRenderer->m_renderData.surface.reset();
+        g_pHyprRenderer->m_renderData.clipBox = {};
     }};
 
     if (element->m_data.blur) {
@@ -438,7 +439,6 @@ void IElementRenderer::drawTex(WP<CTexPassElement> element, const CRegion& damag
             if (inverseOpaque.empty()) {
                 element->m_data.blur = false;
                 draw(element, damage);
-                m_renderData.clipBox = {};
                 return;
             }
         } else
@@ -460,8 +460,6 @@ void IElementRenderer::drawTex(WP<CTexPassElement> element, const CRegion& damag
         draw(element, damage);
     } else
         draw(element, damage);
-
-    m_renderData.clipBox = {};
 }
 
 void IElementRenderer::drawTexMatte(WP<CTextureMatteElement> element, const CRegion& damage) {

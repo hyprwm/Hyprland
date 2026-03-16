6355d06fbf2c5552cf482cd3860cc20bfb2200d2
local <>
2026-03-16 15:10:49 +0100

    tests

--- hyprtester/src/tests/main/scroll.cpp
+++ hyprtester/src/tests/main/scroll.cpp
@@ -198,6 +198,34 @@ static void testSwapcolWrapping() {
     Tests::killAllWindows();
 }
 
+static bool testWindowRule() {
+    NLog::log("{}Testing Scrolling Width", Colors::GREEN);
+
+    // inject a new rule.
+    OK(getFromSocket("/keyword windowrule[scrolling-width]:match:class kitty_scroll"));
+    OK(getFromSocket("/keyword windowrule[scrolling-width]:scrolling_width 0.1"));
+
+    if (!Tests::spawnKitty("kitty_scroll")) {
+        NLog::log("{}Failed to spawn kitty with win class `kitty_scroll`", Colors::RED);
+        return false;
+    }
+
+    if (!Tests::spawnKitty("kitty_scroll")) {
+        NLog::log("{}Failed to spawn kitty with win class `kitty_scroll`", Colors::RED);
+        return false;
+    }
+
+    EXPECT(Tests::windowCount(), 2);
+
+    // not the greatest test, but as long as res and gaps don't change, we good.
+    EXPECT_CONTAINS(getFromSocket("/activewindow"), "size: 174,1036");
+
+    NLog::log("{}Killing all windows", Colors::YELLOW);
+    Tests::killAllWindows();
+    EXPECT(Tests::windowCount(), 0);
+    return true;
+}
+
 static bool test() {
     NLog::log("{}Testing Scroll layout", Colors::GREEN);
 
@@ -216,6 +244,8 @@ static bool test() {
     // test
     NLog::log("{}Testing swapcol wrap", Colors::GREEN);
     testSwapcolWrapping();
+
+    testWindowRule();
 
     // clean up
     NLog::log("Cleaning up", Colors::YELLOW);

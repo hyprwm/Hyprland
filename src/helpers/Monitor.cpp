ffbdbe89c1e5296bba397c445d2b32f5a5ecf46f
Visal Vijay <visalvijay83@gmail.com>
2026-03-31 00:24:40 +0530

    style: remove redundant comment

--- src/helpers/Monitor.cpp
+++ src/helpers/Monitor.cpp
@@ -654,7 +654,7 @@ bool CMonitor::applyMonitorRule(Config::CMonitorRule&& pMonitorRule, bool force)
     }
 
     // Check if the rule isn't already applied
-    // Refactored: split complex condition into readable components
+
     const bool sameResolution =
         DELTALESSTHAN(m_pixelSize.x, RULE->m_resolution.x, 1) && DELTALESSTHAN(m_pixelSize.y, RULE->m_resolution.y, 1) && m_pixelSize.x > 1 && m_pixelSize.y > 1;
 

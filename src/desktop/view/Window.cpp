2e3ecf0df64ad5e953dbfe15c51db704f5f324fb
enzi <t.enzenebner@gmail.com>
2026-04-04 19:30:21 +0200

    latest merge

--- src/desktop/view/Window.cpp
+++ src/desktop/view/Window.cpp
@@ -2051,17 +2051,20 @@ void CWindow::mapWindow() {
     if (!m_ruleApplicator->noFocus().valueOrDefault() && !m_noInitialFocus && (!isX11OverrideRedirect() || (m_isX11 && m_xwaylandSurface->wantsFocus())) && !workspaceSilent &&
         (!PFORCEFOCUS || PFORCEFOCUS == m_self.lock()) && !g_pInputManager->isConstrained()) {
 
-        // this window should gain focus: if it's grouped, preserve fullscreen state.
-        const bool SAME_GROUP = m_group && m_group->has(LAST_FOCUS_WINDOW);
+        // don't steal pointer focus with X11 when buttons are held (e.g., during drags)
+        if (!(m_isX11 && g_pInputManager->hasHeldButtons())) {
+            // this window should gain focus: if it's grouped, preserve fullscreen state.
+            const bool SAME_GROUP = m_group->has(LAST_FOCUS_WINDOW);
+            if (IS_LAST_IN_FS && SAME_GROUP) {
+                Desktop::focusState()->rawWindowFocus(m_self.lock(), FOCUS_REASON_NEW_WINDOW);
+                g_pCompositor->setWindowFullscreenInternal(m_self.lock(), LAST_FS_MODE);
+            } else
+                Desktop::focusState()->fullWindowFocus(m_self.lock(), FOCUS_REASON_NEW_WINDOW);
 
-        if (IS_LAST_IN_FS && SAME_GROUP) {
-            Desktop::focusState()->rawWindowFocus(m_self.lock(), FOCUS_REASON_NEW_WINDOW);
-            g_pCompositor->setWindowFullscreenInternal(m_self.lock(), LAST_FS_MODE);
-        } else
-            Desktop::focusState()->fullWindowFocus(m_self.lock(), FOCUS_REASON_NEW_WINDOW);
+            m_activeInactiveAlpha->setValueAndWarp(*PACTIVEALPHA);
+            m_dimPercent->setValueAndWarp(m_ruleApplicator->noDim().valueOrDefault() ? 0.f : *PDIMSTRENGTH);
+        }
 
-        m_activeInactiveAlpha->setValueAndWarp(*PACTIVEALPHA);
-        m_dimPercent->setValueAndWarp(m_ruleApplicator->noDim().valueOrDefault() ? 0.f : *PDIMSTRENGTH);
     } else {
         m_activeInactiveAlpha->setValueAndWarp(*PINACTIVEALPHA);
         m_dimPercent->setValueAndWarp(0);

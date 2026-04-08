30d67433d1bd1b40be043501474b51456b355c0b
UjinT34 <ujint34@mail.ru>
2026-04-08 21:33:41 +0300

    clamp contrast input

--- src/render/shaders/glsl/gain.glsl
+++ src/render/shaders/glsl/gain.glsl
@@ -1,6 +1,7 @@
-vec3 gain(vec3 x, float k) {
+vec3 gain(vec3 src, float k) {
+    vec3 x = clamp(src, 0.0, 1.0);
     vec3 t = step(0.5, x);
     vec3 y = mix(x, 1.0 - x, t);
     vec3 a = 0.5 * pow(2.0 * y, vec3(k));
-    return clamp(mix(a, 1.0 - a, t), 0.0, 1.0);
+    return mix(a, 1.0 - a, t);
 }

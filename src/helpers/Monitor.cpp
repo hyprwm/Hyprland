8968668b99f18dbbd1c013218f43cd447b3f35a0
B2krobbery <150381094+B2krobbery@users.noreply.github.com>
2026-04-15 21:54:28 +0530

    style: shorten variable name PMONITORWORKSPACEOWNER to PMONITOR

--- src/helpers/Monitor.cpp
+++ src/helpers/Monitor.cpp
@@ -1503,22 +1503,22 @@ void CMonitor::setSpecialWorkspace(const PHLWORKSPACE& pWorkspace) {
 
     bool wasActive = false;
     //close if open elsewhere
-    const auto PMONITORWORKSPACEOWNER = pWorkspace->m_monitor.lock();
+    const auto PMONITOR = pWorkspace->m_monitor.lock();
 
-    if (PMONITORWORKSPACEOWNER && PMONITORWORKSPACEOWNER->m_activeSpecialWorkspace == pWorkspace) {
-        PMONITORWORKSPACEOWNER->m_activeSpecialWorkspace.reset();
-        g_layoutManager->recalculateMonitor(PMONITORWORKSPACEOWNER);
-        g_pHyprRenderer->damageMonitor(PMONITORWORKSPACEOWNER);
-        g_pEventManager->postEvent(SHyprIPCEvent{"activespecial", "," + PMONITORWORKSPACEOWNER->m_name});
-        g_pEventManager->postEvent(SHyprIPCEvent{"activespecialv2", ",," + PMONITORWORKSPACEOWNER->m_name});
+    if (PMONITOR && PMONITOR->m_activeSpecialWorkspace == pWorkspace) {
+        PMONITOR->m_activeSpecialWorkspace.reset();
+        g_layoutManager->recalculateMonitor(PMONITOR);
+        g_pHyprRenderer->damageMonitor(PMONITOR);
+        g_pEventManager->postEvent(SHyprIPCEvent{"activespecial", "," + PMONITOR->m_name});
+        g_pEventManager->postEvent(SHyprIPCEvent{"activespecialv2", ",," + PMONITOR->m_name});
 
         // Reset layer surfaces on the old monitor when special workspace is stolen
         for (auto const& ls : g_pCompositor->m_layers) {
-            if (ls->m_monitor == PMONITORWORKSPACEOWNER)
+            if (ls->m_monitor == PMONITOR)
                 ls->m_aboveFullscreen = false;
         }
 
-        const auto PACTIVEWORKSPACE = PMONITORWORKSPACEOWNER->m_activeWorkspace;
+        const auto PACTIVEWORKSPACE = PMONITOR->m_activeWorkspace;
         g_pDesktopAnimationManager->setFullscreenFadeAnimation(PACTIVEWORKSPACE,
                                                                PACTIVEWORKSPACE && PACTIVEWORKSPACE->m_hasFullscreenWindow ? CDesktopAnimationManager::ANIMATION_TYPE_IN :
                                                                                                                              CDesktopAnimationManager::ANIMATION_TYPE_OUT);
@@ -1540,7 +1540,7 @@ void CMonitor::setSpecialWorkspace(const PHLWORKSPACE& pWorkspace) {
     if (POLDSPECIAL)
         POLDSPECIAL->m_events.activeChanged.emit();
 
-    if (PMONITORWORKSPACEOWNER != m_self)
+    if (PMONITOR != m_self)
         pWorkspace->m_events.monitorChanged.emit();
 
     if (!wasActive)

6d9a7878e9c68b5c7170129d41209d9f13fc8d3a
Nikolai Nechaev <Nikolay_Nechaev@mail.ru>
2026-04-23 18:20:12 +0900

    tests: Use RAII for clients in client tests
    Turn clients in client tests into classes. Constructors
    initialize, destructors deinitialize.
    
    Not only is it better design when entities (clients) are
    modeled as classes, keeping all the methods connected,
    this also ensures the tests perform their custom cleanup.
    
    This will be especially important when we introduce early
    termination on failure into the tests.

--- hyprtester/src/tests/clients/shortcut-inhibitor.cpp
+++ hyprtester/src/tests/clients/shortcut-inhibitor.cpp
@@ -7,6 +7,7 @@
 #include <hyprutils/os/FileDescriptor.hpp>
 #include <hyprutils/os/Process.hpp>
 
+#include <optional>
 #include <sys/poll.h>
 #include <csignal>
 #include <thread>
@@ -17,106 +18,111 @@ using namespace Hyprutils::Memory;
 
 #define SP CSharedPointer
 
-struct SClient {
-    SP<CProcess>           proc;
-    std::array<char, 1024> readBuf;
-    CFileDescriptor        readFd, writeFd;
-    struct pollfd          fds;
-};
+namespace {
 
-static int  ret = 0;
+    class CClient {
+        SP<CProcess>           proc;
+        std::array<char, 1024> readBuf;
+        CFileDescriptor        readFd, writeFd;
+        struct pollfd          fds;
 
-static bool startClient(SClient& client) {
+      public:
+        CClient();
+        ~CClient();
+    };
+}
+
+static int ret = 0;
+
+CClient::CClient() {
     Tests::killAllWindows();
-    client.proc = makeShared<CProcess>(binaryDir + "/shortcut-inhibitor", std::vector<std::string>{});
+    this->proc = makeShared<CProcess>(binaryDir + "/shortcut-inhibitor", std::vector<std::string>{});
 
-    client.proc->addEnv("WAYLAND_DISPLAY", WLDISPLAY);
+    this->proc->addEnv("WAYLAND_DISPLAY", WLDISPLAY);
 
     int pipeFds1[2], pipeFds2[2];
     if (pipe(pipeFds1) != 0 || pipe(pipeFds2) != 0) {
         NLog::log("{}Unable to open pipe to client", Colors::RED);
-        return false;
+        throw std::exception();
     }
 
-    client.writeFd = CFileDescriptor(pipeFds1[1]);
-    client.proc->setStdinFD(pipeFds1[0]);
+    this->writeFd = CFileDescriptor(pipeFds1[1]);
+    this->proc->setStdinFD(pipeFds1[0]);
 
-    client.readFd = CFileDescriptor(pipeFds2[0]);
-    client.proc->setStdoutFD(pipeFds2[1]);
+    this->readFd = CFileDescriptor(pipeFds2[0]);
+    this->proc->setStdoutFD(pipeFds2[1]);
 
     const int COUNT_BEFORE = Tests::windowCount();
-    client.proc->runAsync();
+    this->proc->runAsync();
 
     close(pipeFds1[0]);
     close(pipeFds2[1]);
 
-    client.fds = {.fd = client.readFd.get(), .events = POLLIN};
-    if (poll(&client.fds, 1, 1000) != 1 || !(client.fds.revents & POLLIN)) {
+    this->fds = {.fd = this->readFd.get(), .events = POLLIN};
+    if (poll(&this->fds, 1, 1000) != 1 || !(this->fds.revents & POLLIN)) {
         NLog::log("{}shortcut-inhibitor client failed poll", Colors::RED);
-        return false;
+        throw std::exception();
     }
 
-    client.readBuf.fill(0);
-    if (read(client.readFd.get(), client.readBuf.data(), client.readBuf.size() - 1) == -1) {
+    this->readBuf.fill(0);
+    if (read(this->readFd.get(), this->readBuf.data(), this->readBuf.size() - 1) == -1) {
         NLog::log("{}shortcut-inhibitor client read failed", Colors::RED);
-        return false;
+        throw std::exception();
     }
 
-    std::string ret = std::string{client.readBuf.data()};
+    std::string ret = std::string{this->readBuf.data()};
     if (ret.find("started") == std::string::npos) {
         NLog::log("{}Failed to start shortcut-inhibitor client, read {}", Colors::RED, ret);
-        return false;
+        throw std::exception();
     }
 
     // wait for window to appear
     int counter = 0;
-    while (Tests::processAlive(client.proc->pid()) && Tests::windowCount() == COUNT_BEFORE) {
+    while (Tests::processAlive(this->proc->pid()) && Tests::windowCount() == COUNT_BEFORE) {
         counter++;
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
         if (counter > 50) {
             NLog::log("{}shortcut-inhibitor client took too long to open", Colors::RED);
-            return false;
+            throw std::exception();
         }
     }
 
-    if (!Tests::processAlive(client.proc->pid())) {
+    if (!Tests::processAlive(this->proc->pid())) {
         NLog::log("{}shortcut-inhibitor client not alive", Colors::RED);
-        return false;
+        throw std::exception();
     }
 
-    if (getFromSocket(std::format("/dispatch hl.dsp.focus({{ window = 'pid:{}' }})", client.proc->pid())) != "ok") {
+    if (getFromSocket(std::format("/dispatch hl.dsp.focus({{ window = 'pid:{}' }})", this->proc->pid())) != "ok") {
         NLog::log("{}Failed to focus shortcut-inhibitor client", Colors::RED, ret);
-        return false;
+        throw std::exception();
     }
 
     std::string command = "on\n";
-    if (write(client.writeFd.get(), command.c_str(), command.length()) == -1) {
+    if (write(this->writeFd.get(), command.c_str(), command.length()) == -1) {
         NLog::log("{}shortcut-inhibitor client write failed", Colors::RED);
-        return false;
+        throw std::exception();
     }
 
-    client.readBuf.fill(0);
-    if (read(client.readFd.get(), client.readBuf.data(), client.readBuf.size() - 1) == -1)
-        return false;
+    this->readBuf.fill(0);
+    if (read(this->readFd.get(), this->readBuf.data(), this->readBuf.size() - 1) == -1)
+        throw std::exception();
 
-    ret = std::string{client.readBuf.data()};
+    ret = std::string{this->readBuf.data()};
     if (ret.find("inhibiting") == std::string::npos) {
         NLog::log("{}shortcut-inhibitor client didn't return inhibiting", Colors::RED);
-        return false;
+        throw std::exception();
     }
 
     NLog::log("{}Started shortcut-inhibitor client", Colors::YELLOW);
-
-    return true;
 }
 
-static void stopClient(SClient& client) {
+CClient::~CClient() {
     std::string cmd = "off\n";
-    write(client.writeFd.get(), cmd.c_str(), cmd.length());
+    write(this->writeFd.get(), cmd.c_str(), cmd.length());
 
-    kill(client.proc->pid(), SIGKILL);
-    client.proc.reset();
+    kill(this->proc->pid(), SIGKILL);
+    this->proc.reset();
 }
 
 static std::string flagFile = "/tmp/hyprtester-keybinds.txt";
@@ -139,9 +145,11 @@ static bool attemptCheckFlag(int attempts, int intervalMs) {
 }
 
 static bool test() {
-    SClient client;
-    if (!startClient(client))
-        return false;
+    std::optional<CClient> client;
+
+    try {
+        client.emplace();
+    } catch (...) { return false; }
 
     NLog::log("{}Testing keybinds", Colors::GREEN);
     //basic keybind test
@@ -168,8 +176,6 @@ static bool test() {
     //gesture bypass flag test
     OK(getFromSocket("/eval hl.plugin.test.gesture('right', 2)"));
     EXPECT_CONTAINS(getFromSocket("/activewindow"), "floating: 1");
-
-    stopClient(client);
 
     NLog::log("{}Reloading the config", Colors::YELLOW);
     OK(getFromSocket("/reload"));

7025d43cac0c15a440efbbe2ab935fc82469ea51
Niko Savola <nikosavola@taaraconnect.com>
2026-04-27 14:02:48 +0300

    Add [[nodiscard]] attributes to API methods

--- src/Compositor.hpp
+++ src/Compositor.hpp
@@ -74,58 +74,58 @@ class CCompositor {
 
     // ------------------------------------------------- //
 
-    void                   removeWindowFromVectorSafe(PHLWINDOW);
-    PHLWINDOW              vectorToWindowUnified(const Vector2D&, uint16_t properties, PHLWINDOW pIgnoreWindow = nullptr);
-    SP<CWLSurfaceResource> vectorToLayerSurface(const Vector2D&, std::vector<PHLLSREF>*, Vector2D*, PHLLS*, bool aboveLockscreen = false);
-    SP<CWLSurfaceResource> vectorToLayerPopupSurface(const Vector2D&, PHLMONITOR monitor, Vector2D*, PHLLS*);
-    SP<CWLSurfaceResource> vectorWindowToSurface(const Vector2D&, PHLWINDOW, Vector2D& sl);
-    Vector2D               vectorToSurfaceLocal(const Vector2D&, PHLWINDOW, SP<CWLSurfaceResource>);
-    PHLWINDOW              getWindowFromSurface(SP<CWLSurfaceResource>);
-    PHLWINDOW              getWindowFromHandle(uint32_t);
-    PHLWINDOW              getUrgentWindow();
-    bool                   isWindowActive(PHLWINDOW);
-    void                   changeWindowZOrder(PHLWINDOW, bool);
-    void                   cleanupFadingOut(const MONITORID& monid);
-    PHLWINDOW              getWindowInDirection(PHLWINDOW, Math::eDirection);
-    PHLWINDOW              getWindowInDirection(const CBox& box, PHLWORKSPACE pWorkspace, Math::eDirection dir, bool floatingPreference, PHLWINDOW ignoreWindow = nullptr,
-                                                bool useVectorAngles = false);
-    PHLWINDOW              getWindowCycle(PHLWINDOW cur, bool focusableOnly = false, std::optional<bool> floating = std::nullopt, bool visible = false, bool prev = false,
-                                          bool allowFullscreenBlocked = false);
-    PHLWINDOW              getWindowCycleHist(PHLWINDOWREF cur, bool focusableOnly = false, std::optional<bool> floating = std::nullopt, bool visible = false, bool next = false,
-                                              bool allowFullscreenBlocked = false);
-    bool                   isPointOnAnyMonitor(const Vector2D&);
-    bool                   isPointOnReservedArea(const Vector2D& point, const PHLMONITOR monitor = nullptr);
-    std::optional<CBox>    calculateX11WorkArea();
-    void                   updateAllWindowsAnimatedDecorationValues();
-    void                   moveWorkspaceToMonitor(PHLWORKSPACE, PHLMONITOR, bool noWarpCursor = false);
-    void                   swapActiveWorkspaces(PHLMONITOR, PHLMONITOR);
-    void                   setWindowFullscreenInternal(const PHLWINDOW PWINDOW, const eFullscreenMode MODE);
-    void                   setWindowFullscreenClient(const PHLWINDOW PWINDOW, const eFullscreenMode MODE);
-    void                   setWindowFullscreenState(const PHLWINDOW PWINDOW, const Desktop::View::SFullscreenState state);
-    void                   changeWindowFullscreenModeClient(const PHLWINDOW PWINDOW, const eFullscreenMode MODE, const bool ON);
-    PHLWINDOW              getX11Parent(PHLWINDOW);
-    void                   addToFadingOutSafe(PHLLS);
-    void                   removeFromFadingOutSafe(PHLLS);
-    void                   addToFadingOutSafe(PHLWINDOW);
-    PHLWINDOW              getWindowByRegex(const std::string&);
-    void                   warpCursorTo(const Vector2D&, bool force = false);
-    PHLLS                  getLayerSurfaceFromSurface(SP<CWLSurfaceResource>);
-    Vector2D               parseWindowVectorArgsRelative(const std::string&, const Vector2D&);
-    void                   performUserChecks();
-    void                   moveWindowToWorkspaceSafe(PHLWINDOW pWindow, PHLWORKSPACE pWorkspace);
-    PHLWINDOW              getForceFocus();
-    void                   setPreferredScaleForSurface(SP<CWLSurfaceResource> pSurface, double scale);
-    void                   setPreferredTransformForSurface(SP<CWLSurfaceResource> pSurface, wl_output_transform transform);
-    void                   updateSuspendedStates();
-    std::optional<unsigned int>         getVTNr();
-    bool                                isVRRActiveOnAnyMonitor() const;
+    void                                 removeWindowFromVectorSafe(PHLWINDOW);
+    [[nodiscard]] PHLWINDOW              vectorToWindowUnified(const Vector2D&, uint16_t properties, PHLWINDOW pIgnoreWindow = nullptr);
+    [[nodiscard]] SP<CWLSurfaceResource> vectorToLayerSurface(const Vector2D&, std::vector<PHLLSREF>*, Vector2D*, PHLLS*, bool aboveLockscreen = false);
+    [[nodiscard]] SP<CWLSurfaceResource> vectorToLayerPopupSurface(const Vector2D&, PHLMONITOR monitor, Vector2D*, PHLLS*);
+    [[nodiscard]] SP<CWLSurfaceResource> vectorWindowToSurface(const Vector2D&, PHLWINDOW, Vector2D& sl);
+    [[nodiscard]] Vector2D               vectorToSurfaceLocal(const Vector2D&, PHLWINDOW, SP<CWLSurfaceResource>);
+    [[nodiscard]] PHLWINDOW              getWindowFromSurface(SP<CWLSurfaceResource>);
+    [[nodiscard]] PHLWINDOW              getWindowFromHandle(uint32_t);
+    [[nodiscard]] PHLWINDOW              getUrgentWindow();
+    [[nodiscard]] bool                   isWindowActive(PHLWINDOW);
+    void                                 changeWindowZOrder(PHLWINDOW, bool);
+    void                                 cleanupFadingOut(const MONITORID& monid);
+    [[nodiscard]] PHLWINDOW              getWindowInDirection(PHLWINDOW, Math::eDirection);
+    [[nodiscard]] PHLWINDOW getWindowInDirection(const CBox& box, PHLWORKSPACE pWorkspace, Math::eDirection dir, bool floatingPreference, PHLWINDOW ignoreWindow = nullptr,
+                                                 bool useVectorAngles = false);
+    [[nodiscard]] PHLWINDOW getWindowCycle(PHLWINDOW cur, bool focusableOnly = false, std::optional<bool> floating = std::nullopt, bool visible = false, bool prev = false,
+                                           bool allowFullscreenBlocked = false);
+    [[nodiscard]] PHLWINDOW getWindowCycleHist(PHLWINDOWREF cur, bool focusableOnly = false, std::optional<bool> floating = std::nullopt, bool visible = false, bool next = false,
+                                               bool allowFullscreenBlocked = false);
+    [[nodiscard]] bool      isPointOnAnyMonitor(const Vector2D&);
+    [[nodiscard]] bool      isPointOnReservedArea(const Vector2D& point, const PHLMONITOR monitor = nullptr);
+    [[nodiscard]] std::optional<CBox>                 calculateX11WorkArea();
+    void                                              updateAllWindowsAnimatedDecorationValues();
+    void                                              moveWorkspaceToMonitor(PHLWORKSPACE, PHLMONITOR, bool noWarpCursor = false);
+    void                                              swapActiveWorkspaces(PHLMONITOR, PHLMONITOR);
+    void                                              setWindowFullscreenInternal(const PHLWINDOW PWINDOW, const eFullscreenMode MODE);
+    void                                              setWindowFullscreenClient(const PHLWINDOW PWINDOW, const eFullscreenMode MODE);
+    void                                              setWindowFullscreenState(const PHLWINDOW PWINDOW, const Desktop::View::SFullscreenState state);
+    void                                              changeWindowFullscreenModeClient(const PHLWINDOW PWINDOW, const eFullscreenMode MODE, const bool ON);
+    [[nodiscard]] PHLWINDOW                           getX11Parent(PHLWINDOW);
+    void                                              addToFadingOutSafe(PHLLS);
+    void                                              removeFromFadingOutSafe(PHLLS);
+    void                                              addToFadingOutSafe(PHLWINDOW);
+    [[nodiscard]] PHLWINDOW                           getWindowByRegex(const std::string&);
+    void                                              warpCursorTo(const Vector2D&, bool force = false);
+    [[nodiscard]] PHLLS                               getLayerSurfaceFromSurface(SP<CWLSurfaceResource>);
+    [[nodiscard]] Vector2D                            parseWindowVectorArgsRelative(const std::string&, const Vector2D&);
+    void                                              performUserChecks();
+    void                                              moveWindowToWorkspaceSafe(PHLWINDOW pWindow, PHLWORKSPACE pWorkspace);
+    [[nodiscard]] PHLWINDOW                           getForceFocus();
+    void                                              setPreferredScaleForSurface(SP<CWLSurfaceResource> pSurface, double scale);
+    void                                              setPreferredTransformForSurface(SP<CWLSurfaceResource> pSurface, wl_output_transform transform);
+    void                                              updateSuspendedStates();
+    [[nodiscard]] std::optional<unsigned int>         getVTNr();
+    [[nodiscard]] bool                                isVRRActiveOnAnyMonitor() const;
 
-    NColorManagement::PImageDescription getPreferredImageDescription();
-    NColorManagement::PImageDescription getHDRImageDescription();
-    bool                                shouldChangePreferredImageDescription();
+    [[nodiscard]] NColorManagement::PImageDescription getPreferredImageDescription();
+    [[nodiscard]] NColorManagement::PImageDescription getHDRImageDescription();
+    bool                                              shouldChangePreferredImageDescription();
 
-    bool                                supportsDrmSyncobjTimeline() const;
-    std::string                         m_explicitConfigPath;
+    [[nodiscard]] bool                                supportsDrmSyncobjTimeline() const;
+    std::string                                       m_explicitConfigPath;
 
   private:
     void                           initAllSignals();

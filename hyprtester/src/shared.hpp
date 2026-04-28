b6e7198bdd3f78ae965000f4438d74d934b8218c
Tom Benham <tom.benham13@gmail.com>
2026-04-29 00:13:38 +0200

    clang-format

--- hyprtester/src/shared.hpp
+++ hyprtester/src/shared.hpp
@@ -160,7 +160,7 @@ class CTestCase {
                   std::string{haystack});                                                                                                                                          \
         MARK_TEST_FAILED_SILENT();                                                                                                                                                 \
     } else {                                                                                                                                                                       \
-        LOG_OK("{} contains {}.", #haystack, #needle);                                                                                                                            \
+        LOG_OK("{} contains {}.", #haystack, #needle);                                                                                                                             \
     }
 
 #define EXPECT_NOT_CONTAINS(haystack, needle)                                                                                                                                      \

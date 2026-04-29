23d9c4b11348dcab96e842061050dcf92247d369
imperishableSecret <liferacer2310@gmail.com>
2026-04-29 23:49:45 +0530

    unstack namespaces

--- src/config/lua/objects/LuaGroup.hpp
+++ src/config/lua/objects/LuaGroup.hpp
@@ -5,14 +5,10 @@
 
 #include "../../../desktop/view/Group.hpp"
 
-namespace Config {
-    namespace Lua {
-        namespace Objects {
-            class CLuaGroup {
-              public:
-                static void setup(lua_State* L);
-                static void push(lua_State* L, SP<Desktop::View::CGroup> group);
-            };
-        };
+namespace Config::Lua::Objects {
+    class CLuaGroup {
+      public:
+        static void setup(lua_State* L);
+        static void push(lua_State* L, SP<Desktop::View::CGroup> group);
     };
 };

7607bb3657c9269bf3c0f8e1706dbda2a3317049
erstarr <253168930+erstarr@users.noreply.github.com>
2026-05-13 21:17:54 +0200

    note a bug location

--- src/Compositor.cpp
+++ src/Compositor.cpp
@@ -1543,7 +1543,7 @@ PHLWINDOW CCompositor::getWindowInDirection(const CBox& box, PHLWORKSPACE pWorks
                             intersectLength = std::max(0.0, std::min(POSA.y + SIZEA.y, POSB.y + SIZEB.y) - std::max(POSA.y, POSB.y));
                         break;
                     case Math::DIRECTION_RIGHT:
-                        if (isAdjacent(POSB.x, POSB.x + SIZEB.x, POSA.x, POSA.x + SIZEA.x))
+                        if (isAdjacent(POSB.x, POSB.x + SIZEB.x, POSA.x, POSA.x + SIZEA.x)) // TODO ERSTARR: This is the failing point for keybind focus broken
                             intersectLength = std::max(0.0, std::min(POSA.y + SIZEA.y, POSB.y + SIZEB.y) - std::max(POSA.y, POSB.y));
                         break;
                     case Math::DIRECTION_UP:

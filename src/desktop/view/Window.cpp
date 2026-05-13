2d21fa3e5c9bc051930cd92cf26bb08722acb107
Vaxry <43317083+vaxerski@users.noreply.github.com>
2026-05-13 20:34:44 +0100

    input: properly make input-blocked windows inaccessible (#14517)
    * input: properly make input-blocked windows inaccessible
    
    * FUCKER FYUCXDDDDDDDDDDDDDDDDDD

--- src/desktop/view/Window.cpp
+++ src/desktop/view/Window.cpp
@@ -741,8 +741,10 @@ void CWindow::setInputBlocked(eWindowInputBlockReason reason, bool blocked) {
     else
         m_inputBlockReasons &= ~MASK;
 
-    if (blocked && Desktop::focusState()->window() == m_self)
-        Desktop::focusState()->window().reset();
+    const auto IS_BLOCKED = isInputBlocked();
+
+    if (IS_BLOCKED && Desktop::focusState()->window() == m_self)
+        Desktop::focusState()->fullWindowFocus(nullptr, eFocusReason::FOCUS_REASON_SWITCH_TO_WINDOW_SOFT);
 }
 
 bool CWindow::isInputBlocked() const {

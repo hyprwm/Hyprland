25fb0fbb7854d48d41a404a06cb6bb88910f6e3f
Vaxry <vaxry@vaxry.net>
2026-05-14 16:08:03 +0100

    test

---
+++ hyprtester/src/tests/main/state.cpp
@@ -0,0 +1,18 @@
+#include "../../shared.hpp"
+#include "../../hyprctlCompat.hpp"
+#include "../shared.hpp"
+#include "tests.hpp"
+
+TEST_CASE(state) {
+    NLog::log("{}Testing Fallback State", Colors::YELLOW);
+
+    OK(getFromSocket("/eval hl.monitor({ output = 'HEADLESS-1', disabled = true })"));
+    OK(getFromSocket("/eval hl.monitor({ output = 'HEADLESS-2', disabled = true })"));
+    OK(getFromSocket("/eval hl.monitor({ output = 'HEADLESS-3', disabled = true })"));
+    OK(getFromSocket("/eval hl.monitor({ output = 'HEADLESS-4', disabled = true })"));
+
+    {
+        auto str = getFromSocket("/monitors");
+        ASSERT_CONTAINS(str, "FALLBACK");
+    }
+}

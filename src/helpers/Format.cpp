12ad63b6ae3a71ff7a99e20ef143c052319ea685
Tom Englund <tomenglund26@gmail.com>
2026-05-14 16:15:15 +0200

    formatutils: use alphaStripped formats from hyprgraphics
    use alphaStripped formats from hyprgraphics if we can instead of the
    manual switch case that will eventually miss newly added formats.

--- src/helpers/Format.cpp
+++ src/helpers/Format.cpp
@@ -5,6 +5,9 @@
 #include "../macros.hpp"
 #include <xf86drm.h>
 #include <drm_fourcc.h>
+#include <hyprgraphics/egl/Egl.hpp>
+
+using namespace Hyprgraphics::Egl;
 
 SHMFormat NFormatUtils::drmToShm(DRMFormat drm) {
     switch (drm) {
@@ -69,6 +72,13 @@ std::string NFormatUtils::drmModifierName(uint64_t mod) {
 }
 
 DRMFormat NFormatUtils::alphaFormat(DRMFormat prevFormat) {
+    const auto format = getPixelFormatFromDRM(prevFormat);
+    if (format) {
+        if (format->alphaStripped != DRM_FORMAT_INVALID)
+            return format->alphaStripped;
+    }
+
+    Log::logger->log(Log::DEBUG, "NFormatUtils::alphaFormat format not found falling back to manual switch case handling");
     switch (prevFormat) {
         case DRM_FORMAT_XRGB8888: return DRM_FORMAT_ARGB8888;
         case DRM_FORMAT_XBGR8888: return DRM_FORMAT_ABGR8888;

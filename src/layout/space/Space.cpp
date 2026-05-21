f433e9e093c0b54e2cfbce008305842ffc36320d
erstarr <253168930+erstarr@users.noreply.github.com>
2026-05-21 02:10:47 +0200

    format

--- src/layout/space/Space.cpp
+++ src/layout/space/Space.cpp
@@ -159,8 +159,7 @@ eFullscreenRequestResult CSpace::setFullscreen(SP<ITarget> t, eFullscreenMode cu
 
     const auto REQUEST_RESULT = m_algorithm ? m_algorithm->requestFullscreen(t, currentEffectiveMode, mode) : FULLSCREEN_REQUEST_DEFAULT;
 
-    t->setLayoutManagedFullscreen(REQUEST_RESULT == FULLSCREEN_REQUEST_HANDLED_BY_LAYOUT &&
-                                  (mode == FSMODE_FULLSCREEN || mode == FSMODE_MAXIMIZED));
+    t->setLayoutManagedFullscreen(REQUEST_RESULT == FULLSCREEN_REQUEST_HANDLED_BY_LAYOUT && (mode == FSMODE_FULLSCREEN || mode == FSMODE_MAXIMIZED));
     if (REQUEST_RESULT != FULLSCREEN_REQUEST_HANDLED_BY_LAYOUT)
         t->setFullscreenMode(mode);
 

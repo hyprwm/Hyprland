ee141d71a54d6793a8ba3ac7dd8946578fc93e8e
Pppp1116 <pcaiadoguerreiro@gmail.com>
2026-05-31 17:48:13 +0100

    refactor: enhance pointer focus handling and improve resource management

--- src/managers/input/InputManager.hpp
+++ src/managers/input/InputManager.hpp
@@ -222,6 +222,7 @@ class CInputManager {
         CHyprSignalListener newVirtualKeyboard;
         CHyprSignalListener newVirtualMouse;
         CHyprSignalListener setCursor;
+        CHyprSignalListener pointerFocusChange;
         CHyprSignalListener overrideChanged;
     } m_listeners;
 

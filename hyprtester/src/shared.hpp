3a05ece36b763938a120c24441e056613ad375cf
Tom Benham <38216488+tomben13@users.noreply.github.com>
2026-06-08 14:16:02 +0200

    desktop/window: fix unwanted reswallow (#12993)
    * swallowing: fix unwanted reswallow when process opens a secondary window
    
    * swallowing: improved naming coherency
    
    * clang-format
    
    * swallowing: fix segfault when killing the swallowed window
    
    * tests/swallow: added swallow tests
    
    * hyprtester: small logging fix
    
    * clang-format

--- hyprtester/src/shared.hpp
+++ hyprtester/src/shared.hpp
@@ -160,7 +160,7 @@ class CTestCase {
                   std::string{haystack});                                                                                                                                          \
         MARK_TEST_FAILED_SILENT();                                                                                                                                                 \
     } else {                                                                                                                                                                       \
-        LOG_OK("{} contains {}.", #haystack, ASSERTED);                                                                                                                            \
+        LOG_OK("{} contains {}.", #haystack, #needle);                                                                                                                             \
     }
 
 #define EXPECT_NOT_CONTAINS(haystack, needle)                                                                                                                                      \

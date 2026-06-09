d94848453a03b930a7b7324e275c675d5ba362cb
erstarr <253168930+erstarr@users.noreply.github.com>
2026-06-09 04:10:14 +0200

    Create Fullscreen Controller, Create default and scrolling Fullscreen handlers
    Initially implement some simple getter-setter methods for the controller

---
+++ src/layout/algorithm/tiled/scrolling/ScrollingFullscreenHandler.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "managers/fullscreen/handler/FullscreenHandler.hpp"
+
+
+
+
+
+namespace Fullscreen {
+
+
+    class CScrollingFullscreenHandler : public IFullscreenHandler {
+        
+        // Scrolling layout's custom FS handler
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    };
+}

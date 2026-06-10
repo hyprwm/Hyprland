b819d393607fb2a40d051b565fe5afb814d435f1
Dregu <dregu@dreg.us>
2026-06-10 09:18:02 +0300

    tests

--- hyprtester/src/tests/main/hyprctl.cpp
+++ hyprtester/src/tests/main/hyprctl.cpp
@@ -171,3 +171,8 @@ TEST_CASE(hyprctlJsonErrors) {
     jqProc.runSync();
     EXPECT(jqProc.exitCode(), 0);
 }
+
+TEST_CASE(hyprctlREPL) {
+    EXPECT(getCommandStdOut("hyprctl repl 'print(type(hl))'"), "table");
+    EXPECT(getCommandStdOut("hyprctl eval 'print(type(hl))'"), "ok");
+}

8e3669d28cf30e606c7db3c623aba1f9d0142d71
erstarr <253168930+erstarr@users.noreply.github.com>
2026-06-11 05:09:40 +0200

    Draw the full framework for Controller and Default FS Handler.

--- src/layout/algorithm/tiled/scrolling/ScrollingFullscreenHandler.hpp
+++ src/layout/algorithm/tiled/scrolling/ScrollingFullscreenHandler.hpp
@@ -1,12 +1,13 @@
 #pragma once
 
-#include "managers/fullscreen/handler/FullscreenHandler.hpp"
+#include "../../../../managers/fullscreen/handler/FullscreenHandler.hpp"
+#include "../../../../managers/fullscreen/FullscreenController.hpp"
 
 
 
 
 
-namespace Fullscreen {
+namespace Fullscreen::ScrollingFullscreenHandler {
 
 
     class CScrollingFullscreenHandler : public IFullscreenHandler {
@@ -15,16 +16,18 @@ namespace Fullscreen {
 
 
 
+        // all scrolling related helpers, 2 lists of scrolling FS targets (make em unordered sets while you're at it)
 
 
+        private:
 
+        const eFullscreenHandler FULLSCREEN_HANDLER_NAME = FULLSCREEN_HANDLER_SCROLLING; // better name pls.
 
+        // Tracks FSed windows (internal OR client)
+        // Scrolling layout permits multiple FS windows in a workspace.
+        std::unordered_map<WP<Desktop::View::CWindow>, SFullscreenMode> m_fullscreenWindows;
 
-
-
-
-
-
+        // For custom layout targets, keep track of them in their own FS handlers
 
 
     };

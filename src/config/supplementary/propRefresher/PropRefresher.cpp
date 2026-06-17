2d190ba79aeec55174bda98f6202634c97370bc6
erstarr <253168930+erstarr@users.noreply.github.com>
2026-06-17 15:55:31 +0200

    config/propRefresher: add capability to execute scheduled prop refresh immediately with lua helper function, add event for prop refresh (#14990)
    * Revert https://github.com/hyprwm/Hyprland/pull/14594
    
    * add `executeScheduledRefreshImmediately()`, `refreshProp()`
    
    * add exec_scheduled_prop_refresh_immediately helper function
    
    * add event
    
    add return value for exec_scheduled_prop_refresh_immediately
    
    * format
    
    * test

--- src/config/supplementary/propRefresher/PropRefresher.cpp
+++ src/config/supplementary/propRefresher/PropRefresher.cpp
@@ -15,6 +15,7 @@
 
 #include "../../shared/monitor/MonitorRuleManager.hpp"
 #include "../../shared/inotify/ConfigWatcher.hpp"
+#include "../../../event/EventBus.hpp"
 
 using namespace Config;
 using namespace Config::Supplementary;
@@ -25,111 +26,130 @@ UP<CPropRefresher>& Supplementary::refresher() {
 }
 
 void CPropRefresher::scheduleRefresh(PropRefreshBits prop) {
-    static auto PZOOMFACTOR = CConfigValue<Config::FLOAT>("cursor:zoom_factor");
 
     m_propsTripped |= prop;
 
     if (!m_scheduled && g_pEventLoopManager) {
-        g_pEventLoopManager->doLater([this, weak = WP<CPropRefresher>{refresher()}] {
+        m_scheduledRefreshSeq = g_pEventLoopManager->doLater([this, weak = WP<CPropRefresher>{refresher()}] {
             if (!weak)
                 return;
-
-            if (m_propsTripped & REFRESH_INPUT_DEVICES) {
-                g_pInputManager->setKeyboardLayout();     // update kb layout
-                g_pInputManager->setPointerConfigs();     // update mouse cfgs
-                g_pInputManager->setTouchDeviceConfigs(); // update touch device cfgs
-                g_pInputManager->setTabletConfigs();      // update tablets
-                g_pInputManager->setTabletToolConfigs();  // update tablettools
-            }
-
-            if (m_propsTripped & REFRESH_SCREEN_SHADER) {
-                g_pHyprRenderer->m_reloadScreenShader = true;
-                for (auto const& m : State::monitorState()->monitors()) {
-                    if (!m)
-                        continue;
-
-                    m->m_forceFullFrames = 2;
-                    m->scheduleFrame();
-                }
-            }
-
-            if (m_propsTripped & REFRESH_BLUR_FB) {
-                for (auto const& m : State::monitorState()->monitors()) {
-                    if (!m)
-                        continue;
-
-                    m->m_blurFBDirty     = true;
-                    m->m_forceFullFrames = 2;
-                    m->scheduleFrame();
-                }
-            }
-
-            if (m_propsTripped & REFRESH_WINDOW_STATES) {
-                Desktop::Rule::ruleEngine()->updateAllRules();
-
-                for (const auto& ws : State::workspaceState()->workspaces()) {
-                    if (!ws)
-                        continue;
-
-                    ws->updateWindows();
-                    ws->updateWindowData();
-                    ws->updateWindowDecos();
-                }
-
-                g_pCompositor->updateAllWindowsAnimatedDecorationValues();
-
-                for (auto const& m : State::monitorState()->monitors()) {
-                    if (!m)
-                        continue;
-
-                    m->m_forceFullFrames = 2;
-                    g_pHyprRenderer->damageMonitor(m);
-                    m->scheduleFrame();
-                }
-            }
-
-            if (m_propsTripped & REFRESH_MONITOR_STATES) {
-                Config::monitorRuleMgr()->scheduleReload();
-                Config::monitorRuleMgr()->ensureVRR();
-
-                for (const auto& m : State::monitorState()->monitors()) {
-                    if (!m)
-                        continue;
-
-                    g_layoutManager->recalculateMonitor(m, Layout::CLayoutManager::RECALCULATE_MONITOR_REASON_PROP_REFRESH);
-                }
-
-                State::workspacePlacementController()->ensurePersistentWorkspacesPresent(
-                    nullptr, [](PHLWORKSPACE ws, PHLMONITOR mon, bool noWarp) { g_pCompositor->moveWorkspaceToMonitor(ws, mon, noWarp); });
-            }
-
-            if (m_propsTripped & REFRESH_LAYOUTS) {
-                Layout::Supplementary::algoMatcher()->updateWorkspaceLayouts();
-
-                for (auto const& m : State::monitorState()->monitors()) {
-                    g_layoutManager->recalculateMonitor(m, Layout::CLayoutManager::RECALCULATE_MONITOR_REASON_PROP_REFRESH);
-                    g_pHyprRenderer->damageMonitor(m);
-                }
-            }
-
-            if (m_propsTripped & REFRESH_CURSOR_ZOOMS) {
-                for (auto const& m : State::monitorState()->monitors()) {
-                    *(m->m_cursorZoom) = *PZOOMFACTOR;
-                    if (m->m_activeWorkspace)
-                        m->m_activeWorkspace->m_space->recalculate();
-                }
-            }
-
-            if (m_propsTripped & REFRESH_CONFIG_WATCHER)
-                Config::watcher()->update();
-
-            if (m_propsTripped & REFRESH_GRADIENTS_GROUPBAR)
-                refreshGroupBarGradients();
-
-            m_scheduled    = false;
-            m_propsTripped = 0;
+            refreshProp(true);
         });
 
         m_scheduled = true;
     }
+}
+
+int CPropRefresher::executeScheduledRefreshImmediately() {
+
+    if (!m_scheduled || m_scheduledRefreshSeq == 0)
+        return 1;
+
+    g_pEventLoopManager->removeDoLater(m_scheduledRefreshSeq);
+    // m_scheduledRefreshSeq must be reset back to 0 during refreshProp() call
+    refreshProp(false);
+    return 0;
+}
+
+void CPropRefresher::refreshProp(const bool execdAsScheduled) {
+
+    static auto PZOOMFACTOR = CConfigValue<Config::FLOAT>("cursor:zoom_factor");
+
+    if (m_propsTripped & REFRESH_INPUT_DEVICES) {
+        g_pInputManager->setKeyboardLayout();     // update kb layout
+        g_pInputManager->setPointerConfigs();     // update mouse cfgs
+        g_pInputManager->setTouchDeviceConfigs(); // update touch device cfgs
+        g_pInputManager->setTabletConfigs();      // update tablets
+        g_pInputManager->setTabletToolConfigs();  // update tablettools
+    }
+
+    if (m_propsTripped & REFRESH_SCREEN_SHADER) {
+        g_pHyprRenderer->m_reloadScreenShader = true;
+        for (auto const& m : State::monitorState()->monitors()) {
+            if (!m)
+                continue;
+
+            m->m_forceFullFrames = 2;
+            m->scheduleFrame();
+        }
+    }
+
+    if (m_propsTripped & REFRESH_BLUR_FB) {
+        for (auto const& m : State::monitorState()->monitors()) {
+            if (!m)
+                continue;
+
+            m->m_blurFBDirty     = true;
+            m->m_forceFullFrames = 2;
+            m->scheduleFrame();
+        }
+    }
+
+    if (m_propsTripped & REFRESH_WINDOW_STATES) {
+        Desktop::Rule::ruleEngine()->updateAllRules();
+
+        for (const auto& ws : State::workspaceState()->workspaces()) {
+            if (!ws)
+                continue;
+
+            ws->updateWindows();
+            ws->updateWindowData();
+            ws->updateWindowDecos();
+        }
+
+        g_pCompositor->updateAllWindowsAnimatedDecorationValues();
+
+        for (auto const& m : State::monitorState()->monitors()) {
+            if (!m)
+                continue;
+
+            m->m_forceFullFrames = 2;
+            g_pHyprRenderer->damageMonitor(m);
+            m->scheduleFrame();
+        }
+    }
+
+    if (m_propsTripped & REFRESH_MONITOR_STATES) {
+        Config::monitorRuleMgr()->scheduleReload();
+        Config::monitorRuleMgr()->ensureVRR();
+
+        for (const auto& m : State::monitorState()->monitors()) {
+            if (!m)
+                continue;
+
+            g_layoutManager->recalculateMonitor(m);
+        }
+
+        State::workspacePlacementController()->ensurePersistentWorkspacesPresent(
+            nullptr, [](PHLWORKSPACE ws, PHLMONITOR mon, bool noWarp) { g_pCompositor->moveWorkspaceToMonitor(ws, mon, noWarp); });
+    }
+
+    if (m_propsTripped & REFRESH_LAYOUTS) {
+        Layout::Supplementary::algoMatcher()->updateWorkspaceLayouts();
+
+        for (auto const& m : State::monitorState()->monitors()) {
+            g_layoutManager->recalculateMonitor(m);
+            g_pHyprRenderer->damageMonitor(m);
+        }
+    }
+
+    if (m_propsTripped & REFRESH_CURSOR_ZOOMS) {
+        for (auto const& m : State::monitorState()->monitors()) {
+            *(m->m_cursorZoom) = *PZOOMFACTOR;
+            if (m->m_activeWorkspace)
+                m->m_activeWorkspace->m_space->recalculate();
+        }
+    }
+
+    if (m_propsTripped & REFRESH_CONFIG_WATCHER)
+        Config::watcher()->update();
+
+    if (m_propsTripped & REFRESH_GRADIENTS_GROUPBAR)
+        refreshGroupBarGradients();
+
+    m_scheduled           = false;
+    m_scheduledRefreshSeq = 0;
+    m_propsTripped        = 0;
+
+    Event::bus()->m_events.config.props_refreshed.emit(execdAsScheduled);
 }

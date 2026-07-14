79bc0ca16e7cca40c247a9200634d150fa8193d1
feelamee <106318766+feelamee@users.noreply.github.com>
2026-07-14 15:32:22 +0000

    groups: add groupbar disable_when_only option (#15009)
    * groups: add groupbar disable_when_only option
    
    * groups: improve `groups` test
    
    `groups_disable_when_only` test fails because it checks windows sizes after
    `disable_when_only` option changed. Changing option currently don't
    trigger window size update.
    
    Same problem with `groups` test, but it worked because it or:
    - didn't checked window size after changing option.
    - called `hl.dsp.group.toggle()`, which trigger size update
    - at the begining set `enable = 1` while it `== 1` by default
    Now it fails too :D
    
    Seems it works in usual Hyprland usage because:
    - `CHyprGroupBarDecoration::draw` calls
    - `g_pDecorationPositioner->repositionDeco(this)`,
      which will invalidate cache and update sizes.
    
    * groups: fix `disable_when_only` and `groups` tests
    
    REFRESH_LAYOUTS is not required 'cause REFRESH_WINDOW_STATES will
    trigger update of window position too.
    
    * groups: fix groupbar drawing

--- src/render/decorations/CHyprGroupBarDecoration.cpp
+++ src/render/decorations/CHyprGroupBarDecoration.cpp
@@ -25,8 +25,8 @@ static SP<ITexture> m_tGradientLockedInactive;
 constexpr int       BAR_TEXT_PAD = 2;
 
 CHyprGroupBarDecoration::CHyprGroupBarDecoration(PHLWINDOW pWindow) : IHyprWindowDecoration(pWindow), m_window(pWindow) {
-    static auto PGRADIENTS = CConfigValue<Config::INTEGER>("group:groupbar:enabled");
-    static auto PENABLED   = CConfigValue<Config::INTEGER>("group:groupbar:gradients");
+    static auto PENABLED   = CConfigValue<Config::INTEGER>("group:groupbar:enabled");
+    static auto PGRADIENTS = CConfigValue<Config::INTEGER>("group:groupbar:gradients");
 
     if (*PENABLED && *PGRADIENTS)
         refreshGroupBarGradients();
@@ -101,8 +101,10 @@ void CHyprGroupBarDecoration::draw(PHLMONITOR pMonitor, float const& a) {
 
     const bool VISIBLE = visible();
 
-    if (VISIBLE != m_bLastVisibilityStatus)
+    if (!m_bLastVisibilityStatus.has_value() || VISIBLE != *m_bLastVisibilityStatus) {
         g_pDecorationPositioner->repositionDeco(this);
+        m_bLastVisibilityStatus = VISIBLE;
+    }
 
     if (!VISIBLE)
         return;
@@ -353,8 +355,8 @@ static SP<ITexture> renderGradient(Config::CGradientValueData* grad) {
 }
 
 void refreshGroupBarGradients() {
-    static auto PGRADIENTS = CConfigValue<Config::INTEGER>("group:groupbar:enabled");
-    static auto PENABLED   = CConfigValue<Config::INTEGER>("group:groupbar:gradients");
+    static auto PENABLED   = CConfigValue<Config::BOOL>("group:groupbar:enabled");
+    static auto PGRADIENTS = CConfigValue<Config::BOOL>("group:groupbar:gradients");
 
     static auto PGROUPCOLACTIVE         = CConfigValue<Config::IComplexConfigValue>("group:groupbar:col.active");
     static auto PGROUPCOLINACTIVE       = CConfigValue<Config::IComplexConfigValue>("group:groupbar:col.inactive");
@@ -535,6 +537,7 @@ CBox CHyprGroupBarDecoration::assignedBoxGlobal() {
 }
 
 bool CHyprGroupBarDecoration::visible() {
-    static auto PENABLED = CConfigValue<Config::INTEGER>("group:groupbar:enabled");
-    return *PENABLED && m_window->m_ruleApplicator->decorate().valueOrDefault();
+    static auto PENABLED = CConfigValue<Config::BOOL>("group:groupbar:enabled");
+    static auto PDISABLE = CConfigValue<Config::BOOL>("group:groupbar:disable_when_only");
+    return *PENABLED && (!*PDISABLE || m_dwGroupMembers.size() > 1) && m_window->m_ruleApplicator->decorate().valueOrDefault();
 }

ae10ced2b03ebe17bf38a51e3e6c431ebefb8454
Max Asnaashari <max@masnax.org>
2026-07-17 08:30:22 -0700

    hyprtester/plugins: Add keybind_modmask plugin
    This is different than keybind/keybind2 in that it takes a list of mods,
    and applies the mods to the xkb state as well, which the other two do not.

--- hyprtester/plugin/src/main.cpp
+++ hyprtester/plugin/src/main.cpp
@@ -422,6 +422,28 @@ static SDispatchResult keybind2(std::string in) {
     return {};
 }
 
+static SDispatchResult keybindModmask(std::string in) {
+    CVarList2 data(std::move(in));
+    // 0 = release, 1 = press
+    bool press;
+    // See src/devices/IKeyboard.hpp : eKeyboardModifiers for modifier bitmasks
+    // 0 = none, eKeyboardModifiers is shifted to start at 1
+    uint32_t modifierMask;
+    // keycode
+    uint32_t key;
+    try {
+        press        = std::stoul(std::string{data[0]}) == 1;
+        modifierMask = std::stoul(std::string{data[1]});
+        key          = std::stoul(std::string{data[2]}) - 8; // xkb offset
+    } catch (...) { return {.success = false, .error = "invalid input"}; }
+
+    g_pInputManager->m_lastMods = modifierMask;
+    g_keyboard->setMods(modifierMask, 0, 0, 0);
+    g_keyboard->sendKey(key, press);
+
+    return {};
+}
+
 static SDispatchResult setMods(std::string in) {
     CVarList2 data(std::move(in));
     try {
@@ -684,6 +706,13 @@ static int luaKeybind2(lua_State* L) {
     return luaResult(L, ::keybind2(std::format("{},{},{}", press, modifier, key)));
 }
 
+static int luaKeybindMask(lua_State* L) {
+    const auto press        = (int)luaL_checkinteger(L, 1);
+    const auto modifierMask = (int)luaL_checkinteger(L, 2);
+    const auto key          = (int)luaL_checkinteger(L, 3);
+    return luaResult(L, ::keybindModmask(std::format("{},{},{}", press, modifierMask, key)));
+}
+
 static int luaSetMods(lua_State* L) {
     const auto kbIndex   = (int)luaL_checkinteger(L, 1);
     const auto depressed = (int)luaL_checkinteger(L, 2);
@@ -758,6 +787,7 @@ APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
     addLuaFn("click", ::luaClick);
     addLuaFn("keybind", ::luaKeybind);
     addLuaFn("keybind2", ::luaKeybind2);
+    addLuaFn("keybind_modmask", ::luaKeybindMask);
     addLuaFn("set_mods", ::luaSetMods);
     addLuaFn("nullfocus", ::luaNullfocus);
     addLuaFn("clear_surface_focus", ::luaClearSurfaceFocus);

3f21cf1b853b7b534341c2830dad90181af82118
NotPppp1116 <264624490+NotPppp1116@users.noreply.github.com>
2026-07-18 11:09:20 +0100

    monitor: honor disabled direct scanout window rules

--- src/output/Monitor.cpp
+++ src/output/Monitor.cpp
@@ -2028,12 +2028,13 @@ uint16_t CMonitor::isDSBlocked(bool full) {
     if (*PDIRECTSCANOUT == 2) {
         const auto FSWINDOW = Fullscreen::controller()->getFullscreenWindow(m_self.lock());
         const bool forceDS  = FSWINDOW && FSWINDOW->m_ruleApplicator->directScanout().valueOr(false);
+        const bool blockDS  = FSWINDOW && FSWINDOW->m_ruleApplicator->directScanout().hasValue() && !forceDS;
 
         if (!PWORKSPACE || !FSWINDOW || Fullscreen::controller()->getFullscreenModes(FSWINDOW).internal != Fullscreen::FSMODE_FULLSCREEN) {
             reasons |= DS_BLOCK_WINDOWED;
             if (!full)
                 return reasons;
-        } else if (!forceDS && FSWINDOW->getContentType() != CONTENT_TYPE_GAME) {
+        } else if (blockDS || (!forceDS && FSWINDOW->getContentType() != CONTENT_TYPE_GAME)) {
             reasons |= DS_BLOCK_CONTENT;
             if (!full)
                 return reasons;

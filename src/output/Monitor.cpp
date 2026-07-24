566ff8fa7208f7027cbc21b0b208d121ee80eb38
Tom Englund <tomenglund26@gmail.com>
2026-07-24 14:51:20 +0200

    monitor: fix rotate and mirror regression (#15587)
    * monitor: fix transform logical pixels
    
    seems to have vanished in old refactor, without setting m_size the
    logical pixels are wrong if you start in non rotation, change config and
    apply rotation. m_size still refers to no transformed position.
    
    * monitor: fix added unavailable mirror workspace
    
    applyMonitorRule moved away workspaces and m_activeWorkspace before this with setMirror(),
     but onConnect then reassigned one. if guard it.

--- src/output/Monitor.cpp
+++ src/output/Monitor.cpp
@@ -317,7 +317,8 @@ void CMonitor::onConnect(bool noRule) {
 
     Log::logger->log(Log::DEBUG, "Added new monitor with name {} at {:j0} with size {:j0}, pointer {:x}", m_name, m_position, m_pixelSize, rc<uintptr_t>(m_output.get()));
 
-    setupDefaultWS(monitorRule);
+    if (!isMirror())
+        setupDefaultWS(monitorRule);
 
     for (auto const& ws : State::workspaceState()->workspacesCopy()) {
         if (!valid(ws))
@@ -696,6 +697,7 @@ bool CMonitor::applyMonitorRuleSoft(Config::CMonitorRule&& pMonitorRule) {
     }
 
     Vector2D xfmd     = m_transform % 2 == 1 ? Vector2D{m_pixelSize.y, m_pixelSize.x} : m_pixelSize;
+    m_size            = (xfmd / m_scale).round();
     m_transformedSize = xfmd;
 
     if (m_createdByUser) {

b3aa455ceba60a04e5a1c6fc93e02dac43eda269
Lucas Ritzdorf <42657792+LRitzdorf@users.noreply.github.com>
2026-07-29 07:08:53 -0600

    layout: allow constraining floating windows to be entirely or partially on-screen (#15492)
    * floating: support fitting windows only partially on-screen
    
    * floating: add config for whether existing floating windows are kept on-screen
    
    * floating: apply "floating windows on-screen" config when moving into layout
    
    * floating: add config for whether new floating windows are forced on-screen
    
    * tests: (new_)?float_force_onscreen behavior

--- src/config/values/ConfigValues.cpp
+++ src/config/values/ConfigValues.cpp
@@ -517,6 +517,8 @@ std::vector<SP<IValue>> Values::getConfigValues() {
         MS<Bool>("misc:screencopy_force_8b", "forces 8 bit screencopy", true),
         MS<Bool>("misc:disable_scale_notification", "disables notification popup when a monitor fails to set a suitable scale", false),
         MS<Bool>("misc:size_limits_tiled", "whether to apply minsize and maxsize rules to tiled windows", false),
+        MS<Int>("misc:new_float_force_onscreen", "whether new floating windows must be placed fully/partially on-screen", 2),
+        MS<Int>("misc:float_force_onscreen", "whether existing floating windows must remain fully/partially on-screen", 0),
 
         /*
          * binds:

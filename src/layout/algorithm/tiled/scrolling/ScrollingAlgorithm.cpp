91c9f77d88a3a6d311126d98a6c330579daeac63
erstarr <253168930+erstarr@users.noreply.github.com>
2026-08-04 22:53:42 +0200

    mirror mirror can you tell me who forgot to revert that change after it was redundant?

--- src/layout/algorithm/tiled/scrolling/ScrollingAlgorithm.cpp
+++ src/layout/algorithm/tiled/scrolling/ScrollingAlgorithm.cpp
@@ -548,11 +548,8 @@ void SScrollingData::recalculate(bool forceInstant) {
                 TDATA->layoutBox = controller->calculateTargetBox(i, j, USABLE, WORKAREA.pos(), *PFSONONE);
 
             if (TDATA->target) {
-                if (targetIsCoveringFullscreen) {
-                    // Scrolling onto or have a new FS window
-                    algorithm->m_scrollingFullscreenHandler->updateTargetRulesAndDecos(TARGET);
+                if (targetIsCoveringFullscreen)
                     Fullscreen::controller()->m_windowPosSettingQueued = true;
-                }
                 // must set pos of the highest level target (i.e. if target a part of a group, must set that group's pos which will set the pos of all member targets)
                 TDATA->target->setPositionGlobal(targetBoxWithGaps(TDATA->layoutBox, i, j, COL_HAS_FS_TARGET && TARGET_FS_MODE == Fullscreen::FSMODE_FULLSCREEN),
                                                  TARGET_FS_MODE != Fullscreen::FSMODE_NONE ?
--- src/layout/algorithm/tiled/scrolling/ScrollingFullscreenHandler.cpp
+++ src/layout/algorithm/tiled/scrolling/ScrollingFullscreenHandler.cpp
@@ -576,10 +576,21 @@ void CScrollingFullscreenHandler::sScrollingDataRecalculateHelper(const SP<Layou
     // TODO Decouple FS logic from SScrollingData::recalculate() to avoid having to schedule a prop refresh: it has to be here and it's a mess because recalculate() handled scrolling
     // onto/away from FS windows and this process doesn't call the controller's FS setters which are normally responsible for handling window rule checks.
 
+    // Scrolling onto or have a new FS window
+    if ((TARGET_WORKSPACE_HAS_FS && CURRENT_FS_TDATA && CURRENT_FS_TDATA->target && CURRENT_FS_TDATA->target->window() &&
+         CURRENT_FS_TDATA->target->window() != m_fullscreenWindowHidingState.lastTiledLayoutManagedFsWindow)) {
+
+        // If window group, get the current window
+        const auto LAST_FS_WINDOW_TARGET = CURRENT_FS_TDATA->target->window()->m_target;
+
+        updateTargetRulesAndDecos(LAST_FS_WINDOW_TARGET);
+
+    }
     // Scrolling away from an FS window
-    if (m_fullscreenWindowHidingState.lastTiledLayoutManagedFsWindow && !TARGET_WORKSPACE_HAS_FS) {
-        const auto LAST_FS_TARGET = m_fullscreenWindowHidingState.lastTiledLayoutManagedFsWindow->m_target;
-        updateTargetRulesAndDecos(LAST_FS_TARGET);
+    else if (m_fullscreenWindowHidingState.lastTiledLayoutManagedFsWindow && !TARGET_WORKSPACE_HAS_FS) {
+        const auto LAST_FS_WINDOW = m_fullscreenWindowHidingState.lastTiledLayoutManagedFsWindow.lock();
+
+        updateTargetRulesAndDecos(LAST_FS_WINDOW->m_target);
     }
 
     /* Setting DS and VRR */

8238afd45bb0179d3c0bc3998d630fa3ee51d36e
erstarr <253168930+erstarr@users.noreply.github.com>
2026-08-05 04:35:09 +0200

    format

--- src/layout/target/WindowTarget.cpp
+++ src/layout/target/WindowTarget.cpp
@@ -233,7 +233,7 @@ void CWindowTarget::updatePos(uint8_t flags) {
     if (*PCLAMP_TILED) {
         Vector2D minSize = m_window->m_ruleApplicator->minSize().valueOr(Vector2D{MIN_WINDOW_SIZE, MIN_WINDOW_SIZE});
         Vector2D maxSize = m_window->m_ruleApplicator->maxSize().valueOr(Vector2D{INFINITY, INFINITY});
-        calcSize = calcSize.clamp(minSize, maxSize);
+        calcSize         = calcSize.clamp(minSize, maxSize);
 
         calcPos += (availableSpace - calcSize) / 2.0;
 

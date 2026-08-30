768782777109a7b199b4dd030b585a995167954a
erstarr <253168930+erstarr@users.noreply.github.com>
2026-08-30 21:26:17 +0200

    Ensure op Precedence

--- src/layout/algorithm/tiled/scrolling/ScrollingAlgorithm.cpp
+++ src/layout/algorithm/tiled/scrolling/ScrollingAlgorithm.cpp
@@ -703,7 +703,7 @@ void CScrollingAlgorithm::focusOnInput(SP<ITarget> target, eInputMode input) {
     }
 
     // if click, but target is not under cursor, ignore
-    if (input & INPUT_MODE_CLICK && !Pointer::mgr()->getCursorBoxGlobal().overlaps(target->position()))
+    if ((input & INPUT_MODE_CLICK) && !Pointer::mgr()->getCursorBoxGlobal().overlaps(target->position()))
         return;
 
     // if we moved via non-kb, and it's fully visible, ignore

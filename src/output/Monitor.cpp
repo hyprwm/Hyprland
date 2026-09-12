584baca3284e7ca38a83d76d479fcdb82c8ccfae
Shiphan <140245703+Shiphan@users.noreply.github.com>
2026-09-12 16:29:03 +0800

    input: allow pointer constraints to work on subsurface (#15435)

--- src/output/Monitor.cpp
+++ src/output/Monitor.cpp
@@ -1473,6 +1473,8 @@ void CMonitor::setSpecialWorkspace(const PHLWORKSPACE& pWorkspace, bool noFocus)
 
     g_pHyprRenderer->damageMonitor(m_self.lock());
 
+    g_pInputManager->unconstrainMouse();
+
     if (!pWorkspace) {
         // remove special if exists
         if (m_activeSpecialWorkspace) {

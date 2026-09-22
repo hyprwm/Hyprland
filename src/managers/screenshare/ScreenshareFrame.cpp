9f0b864092653d9307ec1dda4e53fe18440fc2b2
Vaxry <vaxry@vaxry.net>
2026-09-22 09:27:07 +0200

    screencopy: revert "schedule new frames on damage (#16333)"
    This reverts commit 16a228056588909fa0a08b37ce01b6919edd438f.

--- src/managers/screenshare/ScreenshareFrame.cpp
+++ src/managers/screenshare/ScreenshareFrame.cpp
@@ -126,7 +126,7 @@ eScreenshareError CScreenshareFrame::share(SP<IHLBuffer> buffer, const CRegion& 
     }
 
     // schedule a frame so that when a screenshare starts it isn't black until the output is updated
-    if (m_isFirst || !clientDamage.empty()) {
+    if (m_isFirst) {
         const auto PMONITOR = m_session->monitor();
         if (PMONITOR)
             PMONITOR->scheduleFrame(Aquamarine::IOutput::AQ_SCHEDULE_NEEDS_FRAME);

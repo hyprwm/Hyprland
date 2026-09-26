c3dcabc15ba7b71bd1bcd880e9f543909dd9372d
Scott Jones <scottajones@gmail.com>
2026-09-26 10:27:47 -0400

    render: match software renderer names case-insensitively

--- tests/helpers/StringUtils.cpp
+++ tests/helpers/StringUtils.cpp
@@ -2,6 +2,22 @@
 
 #include <gtest/gtest.h>
 
+TEST(Helpers, stringUtilsContainsCaseInsensitive) {
+    EXPECT_TRUE(StringUtils::containsCaseInsensitive("LLVMpipe (LLVM 20.1.8, 128 bits)", "llvmpipe"));
+    EXPECT_TRUE(StringUtils::containsCaseInsensitive("Mesa software rasterizer", "Software Rasterizer"));
+    EXPECT_TRUE(StringUtils::containsCaseInsensitive("aaAb", "AAB"));
+    EXPECT_TRUE(StringUtils::containsCaseInsensitive("renderer", ""));
+    EXPECT_FALSE(StringUtils::containsCaseInsensitive("AMD Radeon RX 7900", "llvmpipe"));
+    EXPECT_FALSE(StringUtils::containsCaseInsensitive("softpip", "softpipe"));
+    EXPECT_FALSE(StringUtils::containsCaseInsensitive("", "softpipe"));
+    EXPECT_FALSE(StringUtils::containsCaseInsensitive("@", "`"));
+    EXPECT_FALSE(StringUtils::containsCaseInsensitive("[", "{"));
+    EXPECT_FALSE(StringUtils::containsCaseInsensitive("\xC0", "\xE0"));
+    EXPECT_TRUE(StringUtils::containsCaseInsensitive("\xFF!Ab", "\xFF!aB"));
+    EXPECT_TRUE(StringUtils::containsCaseInsensitive(std::string_view{"xA\0By", 5}.substr(1, 3), std::string_view{"a\0b", 3}));
+    EXPECT_FALSE(StringUtils::containsCaseInsensitive(std::string_view{"softpipe", 4}, "pipe"));
+}
+
 TEST(Helpers, stringUtilsTruthy) {
     EXPECT_TRUE(StringUtils::truthy("true"));
     EXPECT_TRUE(StringUtils::truthy("trueee"));
